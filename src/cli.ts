03177bb5a64c53d5f7876762af6e24a697f19d44
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-02 09:41:21 -0800

    chore: call global jscodeshift from cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
+import { spawn } from "child_process";
+
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore: package.json will be imported from dist folders
-import { version } from "../package.json";
+import { version } from "../package.json"; // eslint-disable-line
 
 const [, , ...args] = process.argv;
 
 if (args[0] === "--version") {
   console.log(`v${version}`);
+} else {
+  spawn("jscodeshift", args, { stdio: "inherit" });
 }
