baafe94821deda8af42594e148b5a389332863ed
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-03-02 10:35:40 -0800

    Write a basic wrapper over jscodeshift (#4)

---
+++ .changeset/tall-games-knock.md
@@ -0,0 +1,5 @@
+---
+"aws-sdk-js-codemod": patch
+---
+
+Add a basic wrapper over jscodeshift
---
+++ src/cli.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import { spawn } from "child_process";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore: package.json will be imported from dist folders
+import { version } from "../package.json"; // eslint-disable-line
+
+const [, , ...args] = process.argv;
+const jscodeshiftPath = "./node_modules/.bin/jscodeshift";
+
+export const run = async (args): Promise<void> => {
+  if (args[0] === "--version") {
+    process.stdout.write(`aws-sdk-js-codemod: ${version}\n\n`);
+    spawn(jscodeshiftPath, ["--version"], { stdio: "inherit" });
+  } else {
+    spawn(jscodeshiftPath, args, { stdio: "inherit" });
+  }
+};
+
+run(args);
