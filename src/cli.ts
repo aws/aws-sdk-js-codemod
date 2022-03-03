8b2c0c0adaa70647d913d0d864715b5e838c564f
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-02 17:05:44 -0800

    Read transforms from folder

--- src/cli.ts
+++ src/cli.ts
@@ -1,21 +1,25 @@
 #!/usr/bin/env node
 
 import { spawn } from "child_process";
+import { readdirSync } from "fs";
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore: package.json will be imported from dist folders
 import { version } from "../package.json"; // eslint-disable-line
+import { AwsSdkJsCodemodTransform } from "./transforms/types";
 
-const transforms = ["v2-to-v3"].map((folderPath) => {
-  const { name, desc, options } = require(`./${folderPath}`); // eslint-disable-line
-  return { name, desc, options };
-});
+const transforms = readdirSync("transforms", { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+  .map((dirent) => dirent.name)
+  .map((dirName) => {
+    const { name, description, options } = require(`./transforms/${dirName}/transform`); // eslint-disable-line
+    return { name, description, options } as AwsSdkJsCodemodTransform;
+  });
 
 export const run = async (args): Promise<void> => {
   if (args[0] === "--version") {
     process.stdout.write(`aws-sdk-js-codemod: ${version}\n\n`);
   } else if (args[0] === "--help" || args[0] === "-h") {
-    process.stdout.write(`aws-sdk-js-codemod is a light wrapper over jscodeshift.\n`);
     process.stdout.write(`aws-sdk-js-codemod is a light wrapper over jscodeshift.\n`);
   }
   spawn("npm", ["exec", "jscodeshift", "--", ...args], {
