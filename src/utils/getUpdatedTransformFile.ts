df84ac74b5ba0e3ef35eee5d98bff4ac7c63c3a5
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-02 19:29:57 -0800

    Fix resolve for updated transform file

--- src/utils/getUpdatedTransformFile.ts
+++ src/utils/getUpdatedTransformFile.ts
@@ -1,4 +1,4 @@
 import { resolve } from "path";
 
 export const getUpdatedTransformFile = (transformFolder: string) =>
-  resolve(__dirname, "transforms", transformFolder, "transformer.js");
+  resolve(__dirname, "..", "transforms", transformFolder, "transformer.js");
