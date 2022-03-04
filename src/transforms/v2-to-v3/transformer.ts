e3b18ca7a1aa8d413ddef1615fa7a378e51d214c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-04 08:37:49 -0800

    Pass v2DefaultImportName instead of Identifier

--- src/transforms/v2-to-v3/transformer.ts
+++ src/transforms/v2-to-v3/transformer.ts
@@ -14,15 +14,15 @@ export default function transformer(file: FileInfo, api: API) {
   const j = api.jscodeshift;
   const source = j(file.source);
 
-  const v2DefaultImport = getV2DefaultImport(j, source);
-  const v2ClientNames = getV2ClientNames(j, source, v2DefaultImport);
+  const { name: v2DefaultImportName } = getV2DefaultImport(j, source);
+  const v2ClientNames = getV2ClientNames(j, source, v2DefaultImportName);
 
   for (const v2ClientName of v2ClientNames) {
     const v3ClientName = getV3ClientName(v2ClientName);
     const v3ClientPackageName = getV3ClientPackageName(v2ClientName);
     addV3ClientImport(j, source, { v3ClientName, v3ClientPackageName });
     replaceClientCreation(j, source, {
-      importObj: v2DefaultImport,
+      v2DefaultImportName,
       v2ClientName,
       v3ClientName,
     });
