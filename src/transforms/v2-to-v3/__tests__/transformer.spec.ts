10327d891cc08cd042b1636e26d61d665868b611
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-04 09:08:16 -0800

    Add desribe block for tests

--- src/transforms/v2-to-v3/__tests__/transformer.spec.ts
+++ src/transforms/v2-to-v3/__tests__/transformer.spec.ts
@@ -1,4 +1,6 @@
 // @ts-expects-error Could not find a declaration file for module
 import { defineTest } from "jscodeshift/dist/testUtils";
 
-defineTest(__dirname, "./transformer", null, "basic", { parser: "ts" });
+describe("v2-to-v3", () => {
+  defineTest(__dirname, "./transformer", null, "basic", { parser: "ts" });
+});
