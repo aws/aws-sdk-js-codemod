1112f3f7d3b8eeb94b9eb2a6c3a354b04f7f8cc0
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-03-06 12:35:16 -0800

    Update unit tests to follow team jest standards (#25)

--- src/transforms/v2-to-v3/__tests__/transformer.spec.ts
+++
@@ -1,4 +0,0 @@
-// @ts-expects-error Could not find a declaration file for module
-import { defineTest } from "jscodeshift/dist/testUtils";
-
-defineTest(__dirname, "./transformer", null, "basic", { parser: "ts" });
---
+++ src/transforms/v2-to-v3/transformer.spec.ts
@@ -0,0 +1,20 @@
+import { readdirSync, readFileSync } from "fs";
+import { runInlineTest } from "jscodeshift/dist/testUtils";
+import { join } from "path";
+
+import transformer from "./transformer";
+
+describe("v2-to-v3", () => {
+  const fixtureDir = join(__dirname, "__fixtures__");
+  const testFilePrefixes = readdirSync(fixtureDir)
+    .filter((fileName) => fileName.endsWith(".input.ts"))
+    .map((fileName) => fileName.replace(".input.ts", ""));
+
+  it.each(testFilePrefixes)(`transforms correctly using "%s" data`, (testFilePrefix) => {
+    const path = join(fixtureDir, testFilePrefix + `.input.ts`);
+    const source = readFileSync(path, "utf8");
+    const input = { path, source };
+    const expectedOutput = readFileSync(join(fixtureDir, testFilePrefix + `.output.ts`), "utf8");
+    runInlineTest(transformer, null, input, expectedOutput);
+  });
+});
