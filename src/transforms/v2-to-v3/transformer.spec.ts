7d0028814b96144e52adc6eb2fa08af9a0a2884a
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-06 12:20:31 -0800

    Run test file next to source file

--- src/transforms/v2-to-v3/transformer.spec.ts
+++ src/transforms/v2-to-v3/transformer.spec.ts
@@ -2,15 +2,15 @@ import { readdirSync, readFileSync } from "fs";
 import { runInlineTest } from "jscodeshift/dist/testUtils";
 import { join } from "path";
 
-import transformer from "../transformer";
+import transformer from "./transformer";
 
 describe("v2-to-v3", () => {
-  const testFilePrefixes = readdirSync(join(__dirname, "..", "__testfixtures__"))
+  const testFilePrefixes = readdirSync(join(__dirname, "__testfixtures__"))
     .filter((fileName) => fileName.endsWith(".input.ts"))
     .map((fileName) => fileName.replace(".input.ts", ""));
 
   it.each(testFilePrefixes)("%s", async (testFilePrefix) => {
-    const fixtureDir = join(__dirname, "..", "__testfixtures__");
+    const fixtureDir = join(__dirname, "__testfixtures__");
     const inputPath = join(fixtureDir, testFilePrefix + `.input.ts`);
     const source = readFileSync(inputPath, "utf8");
     const expectedOutput = readFileSync(join(fixtureDir, testFilePrefix + `.output.ts`), "utf8");
