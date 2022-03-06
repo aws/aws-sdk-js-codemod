9ff31d86d4faf87dfc79c61963a187c20d0d7d9c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-06 12:24:22 -0800

    Refactor variables used in transformer tests

--- src/transforms/v2-to-v3/transformer.spec.ts
+++ src/transforms/v2-to-v3/transformer.spec.ts
@@ -5,23 +5,16 @@ import { join } from "path";
 import transformer from "./transformer";
 
 describe("v2-to-v3", () => {
-  const testFilePrefixes = readdirSync(join(__dirname, "__testfixtures__"))
+  const fixtureDir = join(__dirname, "__testfixtures__");
+  const testFilePrefixes = readdirSync(fixtureDir)
     .filter((fileName) => fileName.endsWith(".input.ts"))
     .map((fileName) => fileName.replace(".input.ts", ""));
 
-  it.each(testFilePrefixes)("%s", async (testFilePrefix) => {
-    const fixtureDir = join(__dirname, "__testfixtures__");
-    const inputPath = join(fixtureDir, testFilePrefix + `.input.ts`);
-    const source = readFileSync(inputPath, "utf8");
+  it.each(testFilePrefixes)(`transforms correctly using "%s" data`, async (testFilePrefix) => {
+    const path = join(fixtureDir, testFilePrefix + `.input.ts`);
+    const source = readFileSync(path, "utf8");
+    const input = { path, source };
     const expectedOutput = readFileSync(join(fixtureDir, testFilePrefix + `.output.ts`), "utf8");
-    runInlineTest(
-      transformer,
-      null,
-      {
-        path: inputPath,
-        source,
-      },
-      expectedOutput
-    );
+    runInlineTest(transformer, null, input, expectedOutput);
   });
 });
