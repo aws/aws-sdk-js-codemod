94881573126ef5d2adb0b8be1fd621ef5140a420
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-06 12:19:00 -0800

    Replace runTest with runInlineTest

--- src/transforms/v2-to-v3/__tests__/transformer.spec.ts
+++ src/transforms/v2-to-v3/__tests__/transformer.spec.ts
@@ -1,13 +1,27 @@
-import { readdirSync } from "fs";
-import { runTest } from "jscodeshift/dist/testUtils";
+import { readdirSync, readFileSync } from "fs";
+import { runInlineTest } from "jscodeshift/dist/testUtils";
 import { join } from "path";
+
+import transformer from "../transformer";
 
 describe("v2-to-v3", () => {
   const testFilePrefixes = readdirSync(join(__dirname, "..", "__testfixtures__"))
     .filter((fileName) => fileName.endsWith(".input.ts"))
     .map((fileName) => fileName.replace(".input.ts", ""));
 
-  it.each(testFilePrefixes)("%s", (testFilePrefix) => {
-    runTest(__dirname, "./transformer", null, testFilePrefix, { parser: "ts" });
+  it.each(testFilePrefixes)("%s", async (testFilePrefix) => {
+    const fixtureDir = join(__dirname, "..", "__testfixtures__");
+    const inputPath = join(fixtureDir, testFilePrefix + `.input.ts`);
+    const source = readFileSync(inputPath, "utf8");
+    const expectedOutput = readFileSync(join(fixtureDir, testFilePrefix + `.output.ts`), "utf8");
+    runInlineTest(
+      transformer,
+      null,
+      {
+        path: inputPath,
+        source,
+      },
+      expectedOutput
+    );
   });
 });
