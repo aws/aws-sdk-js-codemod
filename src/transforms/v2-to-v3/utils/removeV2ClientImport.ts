f715b570b858ec5847df42591b74cad3a6100ea4
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-07 12:32:07 -0800

    Remove v2 client import irrespective of specifier type

--- src/transforms/v2-to-v3/utils/removeV2ClientImport.ts
+++ src/transforms/v2-to-v3/utils/removeV2ClientImport.ts
@@ -10,17 +10,15 @@ export const removeV2ClientImport = (
     .find(j.ImportDeclaration, {
       specifiers: [
         {
-          type: "ImportDefaultSpecifier",
           local: { name: v2ClientName },
         },
       ],
       source: { value: importSourceName },
     })
     .forEach((declerationPath) => {
-      // Remove default import from ImportDeclaration.
+      // Remove import from ImportDeclaration.
       declerationPath.value.specifiers = declerationPath.value.specifiers.filter(
-        (specifier) =>
-          specifier.type !== "ImportDefaultSpecifier" && specifier.local.name !== v2ClientName
+        (specifier) => specifier.local.name !== v2ClientName
       );
       // Remove ImportDeclaration if there are no other imports.
       if (declerationPath.value.specifiers.length === 0) {
