4b39630d768a13bd4c1cd9a86f0d9e1fcc5b050c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-07 13:18:18 -0800

    Add listTagsForResource call in api-promise

--- src/transforms/v2-to-v3/__fixtures__/api-promise.input.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-promise.input.ts
@@ -1,10 +1,15 @@
 import AWS from "aws-sdk";
 
-const region = "us-west-2";
-const client = new AWS.DynamoDB({ region });
+const client = new AWS.DynamoDB();
 
 client
-  .listTables({})
+  .listTables()
+  .promise()
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err, err.stack));
+
+client
+  .listTagsOfResource({ ResourceArn: "STRING_VALUE" })
   .promise()
   .then((data) => console.log(data))
   .catch((err) => console.log(err, err.stack));
