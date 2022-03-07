673ba9cdadbca594ea3542ac2adade114a02d302
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-06 22:52:38 -0800

    Fix tests for api-promise-variable-declerator

--- src/transforms/v2-to-v3/__fixtures__/api-promise-variable-declerator.input.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-promise-variable-declerator.input.ts
@@ -3,6 +3,7 @@ import AWS from "aws-sdk";
 const client = new AWS.DynamoDB();
 
 const listTablesPromise = client.listTables().promise();
-listTablesPromise.then((data) => console.log(data))
+listTablesPromise
+  .then((data) => console.log(data))
   .catch((err) => console.log(err, err.stack));
 
