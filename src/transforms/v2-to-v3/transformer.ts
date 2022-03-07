9a6cfa40f2d83809ee7a5a29163c2800b18f50eb
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-06 20:43:18 -0800

    WIP to remove .promise() call

--- src/transforms/v2-to-v3/transformer.ts
+++ src/transforms/v2-to-v3/transformer.ts
@@ -1,4 +1,4 @@
-import { API, FileInfo } from "jscodeshift";
+import { API, FileInfo, Identifier } from "jscodeshift";
 
 import {
   addV3ClientImport,
@@ -24,6 +24,44 @@ export default function transformer(file: FileInfo, api: API) {
   for (const [v2ClientName, v3ClientMetadata] of Object.entries(clientMetadata).reverse()) {
     const { v3ClientName, v3ClientPackageName } = v3ClientMetadata;
     addV3ClientImport(j, source, { v3ClientName, v3ClientPackageName });
+
+    // ToDo: Move to utils file
+    source
+      .find(j.VariableDeclarator, {
+        id: { type: "Identifier" },
+        init: {
+          type: "NewExpression",
+          callee: {
+            object: { type: "Identifier", name: v2DefaultImportName },
+            property: { type: "Identifier", name: v2ClientName },
+          },
+        },
+      })
+      .forEach((nodePath) => {
+        const name = (nodePath.value.id as Identifier).name;
+        source
+          .find(j.CallExpression, {
+            callee: {
+              object: {
+                type: "CallExpression",
+                callee: {
+                  type: "MemberExpression",
+                  object: {
+                    type: "Identifier",
+                    name,
+                  },
+                },
+              },
+              property: { type: "Identifier", name: "promise" },
+            },
+          })
+          .forEach((callExpressionPath) => {
+            callExpressionPath.parentPath.value.object = (
+              callExpressionPath.value.callee as any
+            ).object;
+          });
+      });
+
     replaceClientCreation(j, source, {
       v2DefaultImportName,
       v2ClientName,
