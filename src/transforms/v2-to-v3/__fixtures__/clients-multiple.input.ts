afc5de5d5a680515434cc660808f792a8d11bbe8
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-07 07:41:17 -0800

    Remove test clients-multiple

--- src/transforms/v2-to-v3/__fixtures__/clients-multiple.input.ts
+++
@@ -1,6 +0,0 @@
-import AWS from "aws-sdk";
-
-const region = "us-west-2";
-
-const dynamodbClient = new AWS.DynamoDB({ region });
-const s3Client = new AWS.S3({ region });
--- src/transforms/v2-to-v3/__fixtures__/clients-multiple.output.ts
+++
@@ -1,7 +0,0 @@
-import { DynamoDB } from "@aws-sdk/client-dynamodb";
-import { S3 } from "@aws-sdk/client-s3";
-
-const region = "us-west-2";
-
-const dynamodbClient = new DynamoDB({ region });
-const s3Client = new S3({ region });
