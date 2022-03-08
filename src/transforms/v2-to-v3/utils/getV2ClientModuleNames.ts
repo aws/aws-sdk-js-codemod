2929c76c224acdef92c0c561df3da21fb4e75e4e
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-07 19:32:15 -0800

    fix typo in import

--- src/transforms/v2-to-v3/utils/getV2ClientModuleNames.ts
+++ src/transforms/v2-to-v3/utils/getV2ClientModuleNames.ts
@@ -2,7 +2,7 @@ import { Collection, JSCodeshift } from "jscodeshift";
 
 import { containsRequire } from "./containsRequire";
 import { getV2ClientImportNames } from "./getV2ClientImportNames";
-import { getV2ClientRequireNames } from "./getV2ClientREquireNames";
+import { getV2ClientRequireNames } from "./getV2ClientRequireNames";
 
 export const getV2ClientModuleNames = (j: JSCodeshift, source: Collection<any>): string[] =>
   containsRequire(j, source)
