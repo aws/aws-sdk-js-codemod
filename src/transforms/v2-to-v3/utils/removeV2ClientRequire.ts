cef55a878ce77c04dfcdf45394ee4a77d9df98c8
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-03-07 17:26:41 -0800

    Rename import to module/require (#70)

--- src/transforms/v2-to-v3/utils/removeV2ClientRequire.ts
+++ src/transforms/v2-to-v3/utils/removeV2ClientRequire.ts
@@ -7,8 +7,8 @@ export const removeV2ClientRequire = (
   source: Collection<any>,
   v2ClientName: string
 ) => {
-  const importSourceName = `aws-sdk/clients/${v2ClientName.toLowerCase()}`;
-  getRequireVariableDeclaration(j, source, importSourceName)
+  const requireLiteralName = `aws-sdk/clients/${v2ClientName.toLowerCase()}`;
+  getRequireVariableDeclaration(j, source, requireLiteralName)
     .filter(
       (nodePath) =>
         ((nodePath.value.declarations[0] as VariableDeclarator).id as Identifier).name ===
