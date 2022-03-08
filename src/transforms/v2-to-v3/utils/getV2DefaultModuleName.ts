dd83c821a8c519a1eaec228d958550074891bb2d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-03-07 19:19:49 -0800

    Split getV2DefaultModuleName into import/require components (#71)

--- src/transforms/v2-to-v3/utils/getV2DefaultModuleName.ts
+++ src/transforms/v2-to-v3/utils/getV2DefaultModuleName.ts
@@ -1,40 +1,13 @@
-import { Collection, Identifier, JSCodeshift } from "jscodeshift";
+import { Collection, JSCodeshift } from "jscodeshift";
+
+import { containsRequire } from "./containsRequire";
+import { getV2DefaultImportName } from "./getV2DefaultImportName";
+import { getV2DefaultRequireName } from "./getV2DefaultRequireName";
 
 export const getV2DefaultModuleName = (
   j: JSCodeshift,
   source: Collection<any>
-): string | undefined => {
-  let v2DefaultModuleName = undefined;
-
-  // Set specifier name in v2DefaultImportName if it is imported in the source.
-  source
-    .find(j.ImportDeclaration, {
-      source: { value: "aws-sdk" },
-    })
-    .forEach((declerationPath) => {
-      declerationPath.value.specifiers.forEach((specifier) => {
-        if (
-          specifier.type === "ImportDefaultSpecifier" ||
-          specifier.type === "ImportNamespaceSpecifier"
-        ) {
-          v2DefaultModuleName = specifier.local.name;
-        }
-      });
-    });
-
-  // Set specifier name in v2DefaultImportName if it is required in the source.
-  source
-    .find(j.VariableDeclarator, {
-      id: { type: "Identifier" },
-      init: {
-        type: "CallExpression",
-        callee: { type: "Identifier", name: "require" },
-        arguments: [{ type: "Literal", value: "aws-sdk" }],
-      },
-    })
-    .forEach((declerationPath) => {
-      v2DefaultModuleName = (declerationPath.value.id as Identifier).name;
-    });
-
-  return v2DefaultModuleName;
-};
+): string | undefined =>
+  containsRequire(j, source)
+    ? getV2DefaultRequireName(j, source)
+    : getV2DefaultImportName(j, source);
