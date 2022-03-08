51b88ae3e638197f314318571b406646f30a2369
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-07 16:26:29 -0800

    Revert "Temporarily limit transformer test to global-require"
    This reverts commit 434b8b97527edc719896d96fb150401ae162722b.

--- src/transforms/v2-to-v3/transformer.spec.ts
+++ src/transforms/v2-to-v3/transformer.spec.ts
@@ -10,7 +10,7 @@ describe("v2-to-v3", () => {
     .filter((fileName) => fileName.endsWith(".input.ts"))
     .map((fileName) => fileName.replace(".input.ts", ""));
 
-  it.each(["global-require"])(`transforms correctly using "%s" data`, (testFilePrefix) => {
+  it.each(testFilePrefixes)(`transforms correctly using "%s" data`, (testFilePrefix) => {
     const path = join(fixtureDir, testFilePrefix + `.input.ts`);
     const source = readFileSync(path, "utf8");
     const input = { path, source };
