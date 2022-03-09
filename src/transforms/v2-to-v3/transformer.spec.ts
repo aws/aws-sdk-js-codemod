04dbc231e9df01489238bd7e186e57ef7d37a704
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-08 23:25:22 -0800

    Add note for tsonly tests

--- src/transforms/v2-to-v3/transformer.spec.ts
+++ src/transforms/v2-to-v3/transformer.spec.ts
@@ -17,6 +17,8 @@ describe("v2-to-v3", () => {
     const outputCode = readFileSync(outputPath, "utf8");
     const input = { path: inputPath, source: inputCode };
 
+    // Some tests are tsonly as they fail with babel parser
+    // Refs: https://github.com/facebook/jscodeshift/issues/488
     it.each([{ parser: "ts" }, ...(testFilePrefix.startsWith("tsonly-") ? [] : [{}])])(
       "with testOptions: %o",
       (testOptions) => {
