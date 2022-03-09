812ab49729584dff16f189aae1424f7ee2312fe4
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-03-09 15:06:31 -0800

    Add test with no AWS SDK for JavaScript (v2) (#98)

---
+++ src/transforms/v2-to-v3/__fixtures__/no-aws-sdk-v2.output.ts
@@ -0,0 +1,16 @@
+// Alexa SDKs
+import * as AlexaSDK from "alexa-sdk";
+import * as Alexa from "ask-sdk";
+import * as AlexaCore from "ask-sdk-core";
+import * as DdbAdapter from "ask-sdk-dynamodb-persistence-adapter";
+import * as Runtime from "ask-sdk-runtime";
+import * as S3Adapter from "ask-sdk-s3-persistence-adapter";
+import * as ExpressAdapter from "ask-sdk-express-adapter";
+import * as AlexaSmApi from "ask-smapi-sdk";
+
+// Amplify SDKs
+import * as AwsAmplify from "aws-amplify";
+import * as AwsAmplifyReact from "@aws-amplify/ui-react";
+
+// AWS SDK for JavaScript (v3)
+import * as S3 from "@aws-sdk/client-s3";
