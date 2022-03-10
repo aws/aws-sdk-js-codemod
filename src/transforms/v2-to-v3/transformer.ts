99b13901053bd288b1e9b415abb81788af4561d7
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-09 17:31:10 -0800

    Rename v2ClientModuleNames -> v2ServiceModuleNames

--- src/transforms/v2-to-v3/transformer.ts
+++ src/transforms/v2-to-v3/transformer.ts
@@ -17,12 +17,12 @@ export default function transformer(file: FileInfo, api: API) {
   const source = j(file.source);
 
   const v2DefaultModuleName = getV2DefaultModuleName(j, source);
-  const v2ClientModuleNames = getV2ServiceModuleNames(j, source);
-  if (!v2DefaultModuleName && v2ClientModuleNames.length === 0) {
+  const v2ServiceModuleNames = getV2ServiceModuleNames(j, source);
+  if (!v2DefaultModuleName && v2ServiceModuleNames.length === 0) {
     return source.toSource();
   }
 
-  const v2ClientNames = getV2ClientNames(j, source, { v2DefaultModuleName, v2ClientModuleNames });
+  const v2ClientNames = getV2ClientNames(j, source, { v2DefaultModuleName, v2ServiceModuleNames });
   const clientMetadata = getClientMetadata(v2ClientNames);
 
   for (const [v2ClientName, v3ClientMetadata] of Object.entries(clientMetadata).reverse()) {
