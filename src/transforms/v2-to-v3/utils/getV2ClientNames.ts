ca7d7c4c2e4e46aae816317fa1d9bbd555605427
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-09 20:11:09 -0800

    Add utility getV2ClientNamesFromTypeId

--- src/transforms/v2-to-v3/utils/getV2ClientNames.ts
+++ src/transforms/v2-to-v3/utils/getV2ClientNames.ts
@@ -2,6 +2,7 @@ import { Collection, JSCodeshift } from "jscodeshift";
 
 import { getMergedArrayWithoutDuplicates } from "./getMergedArrayWithoutDuplicates";
 import { getV2ClientNamesFromNewExpr } from "./getV2ClientNamesFromNewExpr";
+import { getV2ClientNamesFromTypeId } from "./getV2ClientNamesFromTypeId";
 
 export interface GetV2ClientNamesOptions {
   v2DefaultModuleName: string;
@@ -14,6 +15,10 @@ export const getV2ClientNames = (
   { v2DefaultModuleName, v2ServiceModuleNames }: GetV2ClientNamesOptions
 ): string[] => {
   const v2ClientNamesFromNewExpr = getV2ClientNamesFromNewExpr(j, source, v2DefaultModuleName);
+  const v2ClientNamesFromTypeId = getV2ClientNamesFromTypeId(j, source, v2DefaultModuleName);
 
-  return getMergedArrayWithoutDuplicates(v2ClientNamesFromNewExpr, v2ServiceModuleNames);
+  return getMergedArrayWithoutDuplicates(
+    getMergedArrayWithoutDuplicates(v2ClientNamesFromNewExpr, v2ClientNamesFromTypeId),
+    v2ServiceModuleNames
+  );
 };
