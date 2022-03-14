33278b36388b431309410d58033c803b7b7ab8af
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-03-13 21:29:06 -0700

    Call update on config to update region

--- src/transforms/v2-to-v3/__fixtures__/config-region.input.ts
+++ src/transforms/v2-to-v3/__fixtures__/config-region.input.ts
@@ -1,5 +1,5 @@
 import AWS from "aws-sdk";
 
-AWS.config.region = "us-west-2";
+AWS.config.update({ region: "us-west-2" });
 const dynamodbClient = new AWS.DynamoDB();
 const s3Client = new AWS.S3();
