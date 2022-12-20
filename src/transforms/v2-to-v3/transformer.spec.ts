65eac5bb3947325f676748d7d09e28bdfbdb3beb
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-20 00:57:16 +0000

    test: do not pass parser when running tests

--- src/transforms/v2-to-v3/transformer.spec.ts
+++ src/transforms/v2-to-v3/transformer.spec.ts
@@ -12,20 +12,13 @@ describe("v2-to-v3", () => {
     .filter((fileName) => fileName.endsWith(".input.ts"))
     .map((fileName) => fileName.replace(".input.ts", ""));
 
-  describe.each(testFilePrefixes)(`transforms correctly using "%s" data`, (testFilePrefix) => {
+  it.each(testFilePrefixes)(`transforms correctly using "%s" data`, (testFilePrefix) => {
     const inputPath = join(fixtureDir, testFilePrefix + `.input.ts`);
     const outputPath = join(fixtureDir, testFilePrefix + `.output.ts`);
     const inputCode = readFileSync(inputPath, "utf8");
     const outputCode = readFileSync(outputPath, "utf8");
     const input = { path: inputPath, source: inputCode };
 
-    // Some tests are tsonly as they fail with babel parser
-    // Refs: https://github.com/facebook/jscodeshift/issues/488
-    it.each([{ parser: "ts" }, ...(testFilePrefix.startsWith("tsonly-") ? [] : [{}])])(
-      "with testOptions: %o",
-      (testOptions) => {
-        runInlineTest(transformer, null, input, outputCode, testOptions);
-      }
-    );
+    runInlineTest(transformer, null, input, outputCode);
   });
 });
