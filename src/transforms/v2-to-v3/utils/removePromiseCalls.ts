46b277c383fc9572a19bfaa109746ec2aaf77f0f
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-21 02:14:43 +0000

    Revert "fix: check for ThisExpression wth clientIdName"
    This reverts commit 1eb92969cb2a97c4447f103361b097c2c345313b.

--- src/transforms/v2-to-v3/utils/removePromiseCalls.ts
+++ src/transforms/v2-to-v3/utils/removePromiseCalls.ts
@@ -13,11 +13,10 @@ export const removePromiseCalls = (
   source: Collection<unknown>,
   { v2DefaultModuleName, v2ClientName }: RemovePromiseCallsOptions
 ): void => {
-  const v2ClientIdNames = ["client"];
-  // const v2ClientIdNames = getV2ClientIdNames(j, source, {
-  //   v2DefaultModuleName,
-  //   v2ClientName,
-  // });
+  const v2ClientIdNames = getV2ClientIdNames(j, source, {
+    v2DefaultModuleName,
+    v2ClientName,
+  });
 
   for (const v2ClientIdName of v2ClientIdNames) {
     source
@@ -29,14 +28,8 @@ export const removePromiseCalls = (
             callee: {
               type: "MemberExpression",
               object: {
-                type: "MemberExpression",
-                object: {
-                  type: "ThisExpression",
-                },
-                property: {
-                  type: "Identifier",
-                  name: v2ClientIdName,
-                },
+                type: "Identifier",
+                name: v2ClientIdName,
               },
             },
           },
