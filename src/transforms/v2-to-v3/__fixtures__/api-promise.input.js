7639c9ec425a73cdd06a27ab22f4ad5564187627
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-21 17:36:58 +0000

    test: add comments on api-promise

--- src/transforms/v2-to-v3/__fixtures__/api-promise.input.js
+++ src/transforms/v2-to-v3/__fixtures__/api-promise.input.js
@@ -2,12 +2,14 @@ import AWS from "aws-sdk";
 
 const client = new AWS.DynamoDB();
 
+// Promise without params
 client
   .listTables()
   .promise()
   .then((data) => console.log(data))
   .catch((err) => console.log(err, err.stack));
 
+// Promise with params
 client
   .listTagsOfResource({ ResourceArn: "STRING_VALUE" })
   .promise()
--- src/transforms/v2-to-v3/__fixtures__/api-promise.output.js
+++ src/transforms/v2-to-v3/__fixtures__/api-promise.output.js
@@ -2,11 +2,13 @@ import { DynamoDB } from "@aws-sdk/client-dynamodb";
 
 const client = new DynamoDB();
 
+// Promise without params
 client
   .listTables()
   .then((data) => console.log(data))
   .catch((err) => console.log(err, err.stack));
 
+// Promise with params
 client
   .listTagsOfResource({ ResourceArn: "STRING_VALUE" })
   .then((data) => console.log(data))
