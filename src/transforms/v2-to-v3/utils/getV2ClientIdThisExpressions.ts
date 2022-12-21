debc91d59e6be314fb24df97f30848ad1ca0bc97
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-21 03:22:03 +0000

    chore: add implementation for getV2ClientIdThisExpressions

--- src/transforms/v2-to-v3/utils/getV2ClientIdThisExpressions.ts
+++ src/transforms/v2-to-v3/utils/getV2ClientIdThisExpressions.ts
@@ -1,4 +1,4 @@
-import { Collection, Identifier, JSCodeshift, ThisExpression } from "jscodeshift";
+import { Collection, Identifier, JSCodeshift, MemberExpression, ThisExpression } from "jscodeshift";
 
 export interface ThisMemberExpression {
   type: "MemberExpression";
@@ -6,22 +6,28 @@ export interface ThisMemberExpression {
   property: Identifier;
 }
 
+const thisMemberExpression = { type: "MemberExpression", object: { type: "ThisExpression" } };
+
 export const getV2ClientIdThisExpressions = (
   j: JSCodeshift,
   source: Collection<unknown>,
-  v2ClientIdNames: Identifier[]
-): ThisMemberExpression[] => {
-  // ToDo: return MemberExpression based on v2ClientIdNames
-  return [
-    {
-      type: "MemberExpression",
-      object: {
-        type: "ThisExpression",
-      },
-      property: {
-        type: "Identifier",
-        name: "client",
-      },
-    },
-  ];
-};
+  v2ClientIdentifiers: Identifier[]
+): ThisMemberExpression[] =>
+  v2ClientIdentifiers.flatMap((v2ClientIdentifier) =>
+    source
+      .find(j.AssignmentExpression, {
+        left: thisMemberExpression as MemberExpression,
+        right: v2ClientIdentifier,
+      })
+      .nodes()
+      .map(
+        (assignmentExpression) =>
+          ({
+            ...thisMemberExpression,
+            property: {
+              type: "Identifier",
+              name: ((assignmentExpression.left as MemberExpression).property as Identifier).name,
+            },
+          } as ThisMemberExpression)
+      )
+  );
