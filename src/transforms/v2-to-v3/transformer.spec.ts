11b1012a4e8e7e7125a07fee272e7f088f8a7863
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2022-12-21 22:41:25 -0800

    test: support running transformer tests from subdirectories (#199)

--- src/transforms/v2-to-v3/transformer.spec.ts
+++ src/transforms/v2-to-v3/transformer.spec.ts
@@ -12,20 +12,47 @@ describe("v2-to-v3", () => {
 
   const inputFileRegex = /(.*).input.[jt]sx?$/;
   const fixtureDir = join(__dirname, "__fixtures__");
-  const testFiles: [string, string][] = readdirSync(fixtureDir)
-    .filter((fileName) => inputFileRegex.test(fileName))
-    .map((fileName) => [
-      (fileName.match(inputFileRegex) as RegExpMatchArray)[1],
-      fileName.split(".").pop() as string,
-    ]);
+  const fixtureSubDirs = readdirSync(fixtureDir, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
 
-  it.concurrent.each(testFiles)(`transforms: %s.%s`, async (filePrefix, fileExtension) => {
-    const inputPath = join(fixtureDir, [filePrefix, "input", fileExtension].join("."));
-    const outputPath = join(fixtureDir, [filePrefix, "output", fileExtension].join("."));
+  const getTestFileMetadata = (dirPath: string) =>
+    readdirSync(dirPath)
+      .filter((fileName) => inputFileRegex.test(fileName))
+      .map(
+        (fileName) =>
+          [
+            (fileName.match(inputFileRegex) as RegExpMatchArray)[1],
+            fileName.split(".").pop() as string,
+          ] as const
+      );
+
+  const getTestMetadata = async (dirPath: string, filePrefix: string, fileExtension: string) => {
+    const inputPath = join(dirPath, [filePrefix, "input", fileExtension].join("."));
+    const outputPath = join(dirPath, [filePrefix, "output", fileExtension].join("."));
     const inputCode = await readFile(inputPath, "utf8");
     const outputCode = await readFile(outputPath, "utf8");
 
     const input = { path: inputPath, source: inputCode };
-    runInlineTest(transformer, null, input, outputCode);
+    return { input, outputCode };
+  };
+
+  it.concurrent.each(getTestFileMetadata(fixtureDir))(
+    `transforms: %s.%s`,
+    async (filePrefix, fileExtension) => {
+      const { input, outputCode } = await getTestMetadata(fixtureDir, filePrefix, fileExtension);
+      runInlineTest(transformer, null, input, outputCode);
+    }
+  );
+
+  describe.each(fixtureSubDirs)("%s", (subDir) => {
+    const subDirPath = join(fixtureDir, subDir);
+    it.concurrent.each(getTestFileMetadata(subDirPath))(
+      `transforms: %s.%s`,
+      async (filePrefix, fileExtension) => {
+        const { input, outputCode } = await getTestMetadata(subDirPath, filePrefix, fileExtension);
+        runInlineTest(transformer, null, input, outputCode);
+      }
+    );
   });
 });
