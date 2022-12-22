0e1293e0e426555d3de8212c4d572ac3c2b7cd02
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-22 23:23:32 +0000

    chore(scripts): split generateNewClientTests

--- scripts/generateNewClientTests/getGlobalImportOutputContent.ts
+++ scripts/generateNewClientTests/getGlobalImportOutputContent.ts
@@ -2,21 +2,8 @@ import {
   CLIENT_NAMES,
   CLIENT_NAMES_MAP,
   CLIENT_PACKAGE_NAMES_MAP,
-} from "../src/transforms/v2-to-v3/utils/config";
+} from "../../src/transforms/v2-to-v3/utils/config";
 import { getClientNamesSortedByPackageName } from "./getClientNamesSortedByPackageName";
-
-export const getGlobalImportInputContent = (codegenComment: string) => {
-  let globalImportInputContent = `${codegenComment}\n`;
-
-  globalImportInputContent += `import AWS from "aws-sdk";\n\n`;
-
-  // Add new AWS.V2Client() for each client.
-  for (const clientName of CLIENT_NAMES) {
-    globalImportInputContent += `new AWS.${clientName}();\n`;
-  }
-
-  return globalImportInputContent;
-};
 
 export const getGlobalImportOutputContent = (codegenComment: string) => {
   let globalImportOutputContent = `${codegenComment}\n`;
