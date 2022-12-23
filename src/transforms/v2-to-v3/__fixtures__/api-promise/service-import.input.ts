4cc15746eba48ebecedb3cd72926dfca600cf4d5
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-23 06:07:27 +0000

    test: api-promise with client as Class Member in TypeScript

--- src/transforms/v2-to-v3/__fixtures__/api-promise/service-import.input.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-promise/service-import.input.ts
@@ -1,3 +1,16 @@
 import DynamoDB from "aws-sdk/clients/dynamodb";
 
 export const listTables = (client: DynamoDB) => client.listTables().promise();
+
+// Client as class member
+class ClientClassMember {
+  private client: DynamoDB;
+  
+  constructor(client: DynamoDB) {
+    this.client = client;
+  }
+
+  async listTables() {
+    return await this.client.listTables().promise();
+  }
+}
