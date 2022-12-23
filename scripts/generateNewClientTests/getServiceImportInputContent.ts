b0c9e804337210d1bec4cc7da35c621dd8632848
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-23 00:49:09 +0000

    chore: remove redundant getV2ServiceImportsCode

--- scripts/generateNewClientTests/getServiceImportInputContent.ts
+++ scripts/generateNewClientTests/getServiceImportInputContent.ts
@@ -1,10 +1,12 @@
+import { CLIENT_NAMES } from "../../src/transforms/v2-to-v3/utils/config";
 import { getV2ClientsNewExpressionCode } from "./getV2ClientsNewExpressionCode";
-import { getV2ServiceImportsCode } from "./getV2ServiceImportsCode";
 
 export const getServiceImportInputContent = (codegenComment: string) => {
   let serviceImportInputContent = `${codegenComment}\n`;
 
-  serviceImportInputContent += getV2ServiceImportsCode();
+  for (const clientName of CLIENT_NAMES) {
+    serviceImportInputContent += `import ${clientName} from "aws-sdk/clients/${clientName.toLowerCase()}";\n`;
+  }
   serviceImportInputContent += `\n`;
   serviceImportInputContent += getV2ClientsNewExpressionCode();
 
