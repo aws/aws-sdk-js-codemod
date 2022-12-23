d1ac66d87e43acfdb9007e5dc000e0139b51b7c9
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-23 00:46:33 +0000

    chore(scripts): add extra newline in global-require.output

--- scripts/generateNewClientTests/getGlobalRequireOutputContent.ts
+++ scripts/generateNewClientTests/getGlobalRequireOutputContent.ts
@@ -2,7 +2,7 @@ import { getV3ClientsNewExpressionCode } from "./getV3ClientsNewExpressionCode";
 import { getV3PackageRequireCode } from "./getV3PackageRequireCode";
 
 export const getGlobalRequireOutputContent = (codegenComment: string) => {
-  let globalImportOutputContent = `${codegenComment}\n`;
+  let globalImportOutputContent = `${codegenComment}\n\n`;
 
   globalImportOutputContent += getV3PackageRequireCode();
   globalImportOutputContent += getV3ClientsNewExpressionCode();
