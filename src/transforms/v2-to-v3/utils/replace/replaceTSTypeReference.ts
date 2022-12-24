74ebe9942d9d885814b0ffbcfc5b129822b8c5a9
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-24 15:32:16 -0800

    Update TS Type Reference for Input/Output

--- src/transforms/v2-to-v3/utils/replace/replaceTSTypeReference.ts
+++ src/transforms/v2-to-v3/utils/replace/replaceTSTypeReference.ts
@@ -1,10 +1,24 @@
-import { Collection, JSCodeshift } from "jscodeshift";
+import { Collection, JSCodeshift, TSTypeReference } from "jscodeshift";
 
 export interface ReplaceTypeReferenceOptions {
   v2ClientName: string;
   v3ClientName: string;
   v2DefaultModuleName: string;
 }
+
+const getTsTypeWithClientName = (j: JSCodeshift, node: TSTypeReference, v3ClientName: string) => {
+  node.typeName = j.identifier(v3ClientName);
+  return node;
+};
+
+const getTsTypeWithInputOutput = (j: JSCodeshift, node: TSTypeReference, v2IoName: string) => {
+  if (v2IoName.endsWith("Input")) {
+    node.typeName = j.identifier(v2IoName.replace(/Input$/, "CommandInput"));
+  } else if (v2IoName.endsWith("Output")) {
+    node.typeName = j.identifier(v2IoName.replace(/Output$/, "CommandOutput"));
+  }
+  return node;
+};
 
 // Replace v2 client type reference with v3 client type reference.
 export const replaceTSTypeReference = (
@@ -20,20 +34,28 @@ export const replaceTSTypeReference = (
         right: { type: "Identifier", name: v2ClientName },
       },
     })
-    .replaceWith((tsTypeRef) => {
-      const { node } = tsTypeRef;
-      node.typeName = j.identifier(v3ClientName);
-      return node;
-    });
+    .replaceWith((tsTypeRef) => getTsTypeWithClientName(j, tsTypeRef.node, v3ClientName));
 
-  // Replace type reference created with client module.
+  // Replace type reference to client input/output created with default module.
+  source
+    .find(j.TSTypeReference, {
+      typeName: {
+        left: {
+          left: { type: "Identifier", name: v2DefaultModuleName },
+          right: { type: "Identifier", name: v2ClientName },
+        },
+      },
+    })
+    .replaceWith((tsTypeRef) =>
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore Property 'right' does not exist.
+      getTsTypeWithInputOutput(j, tsTypeRef.node, tsTypeRef.node.typeName.right.name)
+    );
+
+  // Replace type reference to client created with client module.
   source
     .find(j.TSTypeReference, {
       typeName: { type: "Identifier", name: v2ClientName },
     })
-    .replaceWith((nodePath) => {
-      const { node } = nodePath;
-      node.typeName = j.identifier(v3ClientName);
-      return node;
-    });
+    .replaceWith((tsTypeRef) => getTsTypeWithClientName(j, tsTypeRef.node, v3ClientName));
 };
