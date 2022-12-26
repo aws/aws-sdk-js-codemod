6e68cd6eb799291257c9d38cae4aa9ae448e4266
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-26 00:27:59 -0800

    test: add api-input-output-type/service-named-import

---
+++ src/transforms/v2-to-v3/__fixtures__/api-input-output-type/service-named-import.input.ts
@@ -0,0 +1,11 @@
+import DynamoDB, {
+  ListTablesInput,
+  ListTablesOutput,
+}  from "aws-sdk/clients/dynamodb";
+
+const client = new DynamoDB({ region: "us-west-2" });
+
+const listTablesInput: ListTablesInput = { Limit: 10 };
+const listTablesOutput: ListTablesOutput = await client
+  .listTables(listTablesInput)
+  .promise();
