70d92e2a3518bfd6d7db45f6e6d986ae82593c4e
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-25 23:57:08 -0800

    chore: add utility addV3ClientModuleRequire

--- src/transforms/v2-to-v3/utils/add/addV3ClientRequires.ts
+++ src/transforms/v2-to-v3/utils/add/addV3ClientRequires.ts
@@ -1,16 +1,9 @@
-import {
-  Collection,
-  Identifier,
-  JSCodeshift,
-  ObjectPattern,
-  Property,
-  VariableDeclarator,
-} from "jscodeshift";
+import { Collection, Identifier, JSCodeshift } from "jscodeshift";
 
 import { PACKAGE_NAME } from "../config";
 import { getRequireVariableDeclaration, getV2ServiceModulePath, getV3ClientTypes } from "../get";
+import { addV3ClientModuleRequire } from "./addV3ClientModuleRequire";
 import { AddV3ClientModulesOptions } from "./addV3ClientModules";
-
 export const addV3ClientRequires = (
   j: JSCodeshift,
   source: Collection<unknown>,
@@ -29,25 +22,11 @@ export const addV3ClientRequires = (
     value: v3ClientNameIdentifier,
   });
 
-  const existingRequire = getRequireVariableDeclaration(j, source, v3ClientPackageName);
+  const existingRequires = getRequireVariableDeclaration(j, source, v3ClientPackageName);
 
   // Require declaration already exists.
-  if (existingRequire && existingRequire.nodes().length > 0) {
-    existingRequire
-      .nodes()[0]
-      .declarations.filter((declaration) => declaration.type === "VariableDeclarator")
-      .forEach((variableDeclarator) => {
-        // Append to existing require if property not present.
-        if (
-          !((variableDeclarator as VariableDeclarator).id as ObjectPattern).properties.find(
-            (property) => ((property as Property).key as Identifier).name === v3ClientName
-          )
-        ) {
-          ((variableDeclarator as VariableDeclarator).id as ObjectPattern).properties.push(
-            v3ClientNameProperty
-          );
-        }
-      });
+  if (existingRequires && existingRequires.nodes().length > 0) {
+    addV3ClientModuleRequire(j, existingRequires, v3ClientNameProperty);
   } else {
     // Insert after default require if present. If not, insert after service require.
     const v2ServiceModulePath = getV2ServiceModulePath(v2ClientName);
@@ -70,21 +49,18 @@ export const addV3ClientRequires = (
   const v3ClientTypes = getV3ClientTypes(j, source, { v2ClientName, v2DefaultModuleName });
 
   if (v3ClientTypes.length > 0) {
-    const clientRequire = getRequireVariableDeclaration(j, source, v3ClientPackageName);
+    const clientRequires = getRequireVariableDeclaration(j, source, v3ClientPackageName);
     for (const clientTsType of v3ClientTypes.sort()) {
       const clientsTypename = (clientTsType.typeName as Identifier).name;
       if (clientsTypename.endsWith("CommandInput") || clientsTypename.endsWith("CommandOutput")) {
         const clientsTypenameIdentifier = j.identifier(clientsTypename);
-        (
-          (clientRequire.nodes()[0].declarations[0] as VariableDeclarator).id as ObjectPattern
-        ).properties.push(
-          j.property.from({
-            kind: "init",
-            key: clientsTypenameIdentifier,
-            shorthand: true,
-            value: clientsTypenameIdentifier,
-          })
-        );
+        const clientsTypenameProperty = j.property.from({
+          kind: "init",
+          key: clientsTypenameIdentifier,
+          shorthand: true,
+          value: clientsTypenameIdentifier,
+        });
+        addV3ClientModuleRequire(j, clientRequires, clientsTypenameProperty);
       }
     }
   }
