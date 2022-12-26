97075dc4734d02ec1b0e803d7950b24bd382acd2
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-26 00:45:54 -0800

    Revert "test: add api-input-output-type/service-named-import"
    This reverts commit 6e68cd6eb799291257c9d38cae4aa9ae448e4266.

--- src/transforms/v2-to-v3/__fixtures__/api-input-output-type/service-named-import.output.ts
+++
@@ -1,7 +0,0 @@
-import { DynamoDB, ListTablesCommandInput, ListTablesCommandOutput } from "@aws-sdk/client-dynamodb";
-
-const client = new DynamoDB({ region: "us-west-2" });
-
-const listTablesInput: ListTablesCommandInput = { Limit: 10 };
-const listTablesOutput: ListTablesCommandOutput = await client
-  .listTables(listTablesInput);
