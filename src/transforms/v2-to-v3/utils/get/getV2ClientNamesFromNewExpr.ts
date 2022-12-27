2813b65df7702fd17f9e0d3d946b34145a688cd2
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-27 20:15:49 +0000

    Call getV2ClientNewExpression from getV2ClientNamesFromNewExpr

--- src/transforms/v2-to-v3/utils/get/getV2ClientNamesFromNewExpr.ts
+++ src/transforms/v2-to-v3/utils/get/getV2ClientNamesFromNewExpr.ts
@@ -1,18 +1,14 @@
 import { Collection, Identifier, JSCodeshift, MemberExpression } from "jscodeshift";
 
+import { getV2ClientNewExpression } from "../get";
+
 export const getV2ClientNamesFromNewExpr = (
   j: JSCodeshift,
   source: Collection<unknown>,
   v2DefaultModuleName: string
 ): string[] =>
   source
-    .find(j.NewExpression, {
-      callee: {
-        type: "MemberExpression",
-        object: { type: "Identifier", name: v2DefaultModuleName },
-        property: { type: "Identifier" },
-      },
-    })
+    .find(j.NewExpression, getV2ClientNewExpression({ v2DefaultModuleName }))
     .nodes()
     .map(
       (newExpression) => ((newExpression.callee as MemberExpression).property as Identifier).name
