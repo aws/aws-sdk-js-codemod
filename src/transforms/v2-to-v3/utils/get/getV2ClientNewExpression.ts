7c1059a302741b50d720d407953a399ecbb14b12
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-27 19:56:39 +0000

    chore: add utility getV2ClientNewExpression

---
+++ src/transforms/v2-to-v3/utils/get/getV2ClientNewExpression.ts
@@ -0,0 +1,33 @@
+import { NewExpression } from "jscodeshift";
+
+export interface ClientNewExpressionOptions {
+  v2DefaultModuleName?: string;
+  v2ClientName?: string;
+  isDocumentClient?: boolean;
+}
+
+export const getV2ClientNewExpression = ({
+  v2DefaultModuleName,
+  v2ClientName,
+}: ClientNewExpressionOptions): NewExpression => {
+  if (!v2DefaultModuleName && !v2ClientName) {
+    throw new Error(
+      `At least one of the following options must be provided: v2DefaultModuleName, v2ClientName`
+    );
+  }
+
+  if (v2DefaultModuleName) {
+    return {
+      type: "NewExpression",
+      callee: {
+        object: { type: "Identifier", name: v2DefaultModuleName },
+        property: { type: "Identifier", ...(v2ClientName && { name: v2ClientName }) },
+      },
+    } as NewExpression;
+  } else {
+    return {
+      type: "NewExpression",
+      callee: { type: "Identifier", name: v2ClientName },
+    } as NewExpression;
+  }
+};
