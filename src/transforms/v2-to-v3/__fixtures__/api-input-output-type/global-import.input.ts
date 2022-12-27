18957cddbafee09d462a07538b91b910fe9faefd
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-26 21:08:20 -0800

    test: Request/Response in api-input-output-type/global-import

--- src/transforms/v2-to-v3/__fixtures__/api-input-output-type/global-import.input.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-input-output-type/global-import.input.ts
@@ -1,9 +1,13 @@
 import AWS from "aws-sdk";
 
-const client = new AWS.DynamoDB({ region: "us-west-2" });
-
+const ddbClient = new AWS.DynamoDB({ region: "us-west-2" });
 const listTablesInput: AWS.DynamoDB.ListTablesInput = { Limit: 10 };
-const listTablesOutput: AWS.DynamoDB.ListTablesOutput = await client
+const listTablesOutput: AWS.DynamoDB.ListTablesOutput = await ddbClient
   .listTables(listTablesInput)
   .promise();
 
+const stsClient = new AWS.STS({ region: "us-west-2" });
+const getCallerIdentityInput: AWS.STS.GetCallerIdentityRequest = {};
+const getCallerIdentityOutput: AWS.STS.GetCallerIdentityResponse = await stsClient
+  .getCallerIdentity(getCallerIdentityInput)
+  .promise();
