b00f833704cffb5b7dc470975b3397e728236c1a
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-27 18:55:29 +0000

    chore: get Document Client from New Expression

--- src/transforms/v2-to-v3/utils/get/getV2ClientNamesFromNewExpr.ts
+++ src/transforms/v2-to-v3/utils/get/getV2ClientNamesFromNewExpr.ts
@@ -1,19 +1,44 @@
-import { Collection, Identifier, JSCodeshift, MemberExpression } from "jscodeshift";
+import { Collection, Identifier, JSCodeshift, MemberExpression, NewExpression } from "jscodeshift";
+
+const getClientNewExpression = (v2DefaultModuleName: string) =>
+  ({
+    callee: {
+      type: "MemberExpression",
+      object: { type: "Identifier", name: v2DefaultModuleName },
+      property: { type: "Identifier" },
+    },
+  } as NewExpression);
+
+const getDocumentClientNewExpression = (v2DefaultModuleName: string) =>
+  ({
+    callee: {
+      type: "MemberExpression",
+      object: {
+        type: "MemberExpression",
+        object: { type: "Identifier", name: v2DefaultModuleName },
+        property: { type: "Identifier", name: "DynamoDB" },
+      },
+      property: { type: "Identifier" },
+    },
+  } as NewExpression);
+
+const getClientNameFromNewExpr = (
+  j: JSCodeshift,
+  source: Collection<unknown>,
+  newExpression: NewExpression
+): string[] =>
+  source
+    .find(j.NewExpression, newExpression)
+    .nodes()
+    .map(
+      (newExpression) => ((newExpression.callee as MemberExpression).property as Identifier).name
+    );
 
 export const getV2ClientNamesFromNewExpr = (
   j: JSCodeshift,
   source: Collection<unknown>,
   v2DefaultModuleName: string
-): string[] =>
-  source
-    .find(j.NewExpression, {
-      callee: {
-        type: "MemberExpression",
-        object: { type: "Identifier", name: v2DefaultModuleName },
-        property: { type: "Identifier" },
-      },
-    })
-    .nodes()
-    .map(
-      (newExpression) => ((newExpression.callee as MemberExpression).property as Identifier).name
-    );
+): string[] => [
+  ...getClientNameFromNewExpr(j, source, getClientNewExpression(v2DefaultModuleName)),
+  ...getClientNameFromNewExpr(j, source, getDocumentClientNewExpression(v2DefaultModuleName)),
+];
