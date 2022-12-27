47dd63ce8a0696d2d0ae2586b07f4ca6f2b100ab
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-26 21:13:42 -0800

    test: add Request/Response in api-input-output-type/global-require

--- src/transforms/v2-to-v3/__fixtures__/api-input-output-type/global-require.output.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-input-output-type/global-require.output.ts
@@ -4,8 +4,18 @@ const {
   ListTablesCommandOutput
 } = require("@aws-sdk/client-dynamodb");
 
-const client = new DynamoDB({ region: "us-west-2" });
+const {
+  STS,
+  GetCallerIdentityCommandInput,
+  GetCallerIdentityCommandOutput
+} = require("@aws-sdk/client-sts");
 
+const ddbClient = new DynamoDB({ region: "us-west-2" });
 const listTablesInput: ListTablesCommandInput = { Limit: 10 };
-const listTablesOutput: ListTablesCommandOutput = await client
+const listTablesOutput: ListTablesCommandOutput = await ddbClient
   .listTables(listTablesInput);
+
+const stsClient = new STS({ region: "us-west-2" });
+const getCallerIdentityInput: GetCallerIdentityCommandInput = {};
+const getCallerIdentityOutput: GetCallerIdentityCommandOutput = await stsClient
+  .getCallerIdentity(getCallerIdentityInput);
