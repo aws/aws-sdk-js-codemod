32a08e4e27c27ddede8a13b0f9ffc2d375cc8049
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-26 21:11:22 -0800

    chore: add Request/Response to Input/Output suffix list

--- src/transforms/v2-to-v3/utils/config/constants.ts
+++ src/transforms/v2-to-v3/utils/config/constants.ts
@@ -1,5 +1,4 @@
 export const PACKAGE_NAME = "aws-sdk";
 
-// ToDo: Add Request and Response suffixes in future version.
-export const V2_CLIENT_INPUT_SUFFIX_LIST = ["Input"];
-export const V2_CLIENT_OUTPUT_SUFFIX_LIST = ["Output"];
+export const V2_CLIENT_INPUT_SUFFIX_LIST = ["Input", "Request"];
+export const V2_CLIENT_OUTPUT_SUFFIX_LIST = ["Output", "Response"];
