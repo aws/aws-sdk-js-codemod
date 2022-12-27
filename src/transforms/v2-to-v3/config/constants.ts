cee7b1151c819461c90493d1df01f7a6f2a60bb2
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-27 19:02:02 +0000

    Add DynamoDB and DocumentClient as constants

--- src/transforms/v2-to-v3/config/constants.ts
+++ src/transforms/v2-to-v3/config/constants.ts
@@ -1,4 +1,7 @@
 export const PACKAGE_NAME = "aws-sdk";
 
+export const DYNAMODB_CLIENT_NAME = "DynamoDB";
+export const DYNAMODB_DOCUMENT_CLIENT_NAME = "DocumentClient";
+
 export const V2_CLIENT_INPUT_SUFFIX_LIST = ["Input", "Request"];
 export const V2_CLIENT_OUTPUT_SUFFIX_LIST = ["Output", "Response"];
