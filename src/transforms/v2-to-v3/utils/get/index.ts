1ea536add56711aea0dd3928c5b3076cdcb07358
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-27 20:13:24 +0000

    Export getV2ClientNewExpression

--- src/transforms/v2-to-v3/utils/get/index.ts
+++ src/transforms/v2-to-v3/utils/get/index.ts
@@ -4,6 +4,7 @@ export * from "./getV2ClientIdentifiers";
 export * from "./getV2ClientIdThisExpressions";
 export * from "./getV2ClientNames";
 export * from "./getV2ClientNamesFromDefault";
+export * from "./getV2ClientNewExpression";
 export * from "./getV2ClientTypeNames";
 export * from "./getV2DefaultModuleName";
 export * from "./getV2ServiceModulePath";
