6336496d77efea2db10271d3e3b35789ab02d388
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-26 16:50:38 -0800

    chore: add constants for v2 Client Input/Output suffix

--- src/transforms/v2-to-v3/utils/config/constants.ts
+++ src/transforms/v2-to-v3/utils/config/constants.ts
@@ -1,1 +1,5 @@
 export const PACKAGE_NAME = "aws-sdk";
+
+// ToDo: Add Request and Response suffixes in future version.
+export const V2_CLIENT_INPUT_SUFFIX_LIST = ["Input"];
+export const V2_CLIENT_OUTPUT_SUFFIX_LIST = ["Output"];
