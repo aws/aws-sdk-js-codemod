a6dc7ef2d1e80292b9832a21c24b6640b6381228
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-28 22:12:55 +0000

    Update indentation in api-promise/service-require

--- src/transforms/v2-to-v3/__fixtures__/api-promise/service-require.output.js
+++ src/transforms/v2-to-v3/__fixtures__/api-promise/service-require.output.js
@@ -1,4 +1,6 @@
-const { DynamoDB: AWS_DynamoDB } = require("@aws-sdk/client-dynamodb");
+const {
+  DynamoDB: AWS_DynamoDB
+} = require("@aws-sdk/client-dynamodb");
 
 const client = new AWS_DynamoDB();
 
