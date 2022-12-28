ba2781dc45dcba6312982ebe64b784c12838309f
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-27 20:40:39 -0800

    Make v2GlobalName optional in addV3ClientModules

--- src/transforms/v2-to-v3/utils/add/addV3ClientModules.ts
+++ src/transforms/v2-to-v3/utils/add/addV3ClientModules.ts
@@ -6,9 +6,9 @@ import { addV3ClientRequires } from "./addV3ClientRequires";
 
 export interface AddV3ClientModulesOptions {
   v2ClientName: string;
+  v2GlobalName?: string;
   v3ClientName: string;
   v3ClientPackageName: string;
-  v2GlobalName: string;
 }
 
 export const addV3ClientModules = (
