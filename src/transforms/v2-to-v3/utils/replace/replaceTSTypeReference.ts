ec60a035410b00a40d447623b5f141b14465e092
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-28 00:28:39 +0000

    Call getV2ClientTSTypeRef from replaceTSTypeReference

--- src/transforms/v2-to-v3/utils/replace/replaceTSTypeReference.ts
+++ src/transforms/v2-to-v3/utils/replace/replaceTSTypeReference.ts
@@ -1,13 +1,6 @@
-import {
-  ASTPath,
-  Collection,
-  Identifier,
-  JSCodeshift,
-  TSQualifiedName,
-  TSTypeReference,
-} from "jscodeshift";
+import { Collection, Identifier, JSCodeshift, TSQualifiedName, TSTypeReference } from "jscodeshift";
 
-import { getV2ClientTypeNames, getV3ClientTypeName } from "../get";
+import { getV2ClientTSTypeRef, getV2ClientTypeNames, getV3ClientTypeName } from "../get";
 import { isV2ClientInputOutputType } from "../isV2ClientInputOutputType";
 
 export interface ReplaceTypeReferenceOptions {
@@ -29,38 +22,23 @@ export const replaceTSTypeReference = (
 ): void => {
   // Replace type reference to client created with default module.
   source
-    .find(j.TSTypeReference, {
-      typeName: {
-        left: { type: "Identifier", name: v2DefaultModuleName },
-        right: { type: "Identifier", name: v2ClientName },
-      },
-    })
+    .find(j.TSTypeReference, getV2ClientTSTypeRef({ v2ClientName, v2DefaultModuleName }))
     .replaceWith((v2ClientType) =>
       j.tsTypeReference(j.identifier(v3ClientName), v2ClientType.node.typeParameters)
     );
 
   // Replace type reference to client input/output created with default module.
   source
-    .find(j.TSTypeReference, {
-      typeName: {
-        type: "TSQualifiedName",
-        left: {
-          left: { type: "Identifier", name: v2DefaultModuleName },
-          right: { type: "Identifier", name: v2ClientName },
-        },
-        right: { type: "Identifier" },
-      },
-    })
+    .find(
+      j.TSTypeReference,
+      getV2ClientTSTypeRef({ v2ClientName, v2DefaultModuleName, isType: true })
+    )
     .filter((v2ClientType) => isV2ClientInputOutputType(getRightIdentifierName(v2ClientType.node)))
     .replaceWith((v2ClientType) => getV3ClientTypeName(getRightIdentifierName(v2ClientType.node)));
 
   // Replace type reference to client created with client module.
   source
-    .find(j.TSTypeReference, {
-      typeName: {
-        left: { type: "Identifier", name: v2ClientName },
-      },
-    })
+    .find(j.TSTypeReference, getV2ClientTSTypeRef({ v2ClientName }))
     .filter((v2ClientType) => isV2ClientInputOutputType(getRightIdentifierName(v2ClientType.node)))
     .replaceWith((v2ClientType) => getV3ClientTypeName(getRightIdentifierName(v2ClientType.node)));
 
@@ -69,9 +47,7 @@ export const replaceTSTypeReference = (
   for (const v2ClientTypeName of v2ClientTypeNames) {
     if (isV2ClientInputOutputType(v2ClientTypeName)) {
       source
-        .find(j.TSTypeReference, {
-          typeName: { type: "Identifier", name: v2ClientTypeName },
-        })
+        .find(j.TSTypeReference, { typeName: { type: "Identifier", name: v2ClientTypeName } })
         .filter((v2ClientType) => isV2ClientInputOutputType(getIdentifierName(v2ClientType.node)))
         .replaceWith((v2ClientType) => getV3ClientTypeName(getIdentifierName(v2ClientType.node)));
     }
