9c1f4133b7d7e7f879b7f35e3641568c623c378c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-28 22:11:43 +0000

    Add utility getV3ClientRequireProperty with keyName/valueName

--- src/transforms/v2-to-v3/utils/add/addV3ClientModuleRequire.ts
+++ src/transforms/v2-to-v3/utils/add/addV3ClientModuleRequire.ts
@@ -8,10 +8,12 @@ import {
   VariableDeclarator,
 } from "jscodeshift";
 
+import { getV3ClientRequireProperty, V3ClientRequirePropertyOptions } from "../get";
+
 export const addV3ClientModuleRequire = (
   j: JSCodeshift,
   existingRequires: Collection<VariableDeclaration>,
-  v3ClientProperty: Property
+  { keyName, valueName }: V3ClientRequirePropertyOptions
 ) => {
   const existingRequireProperties = existingRequires
     .nodes()
@@ -24,11 +26,13 @@ export const addV3ClientModuleRequire = (
     !existingRequireProperties.find((variableDeclarator) =>
       (variableDeclarator.id as ObjectPattern).properties.find(
         (property) =>
-          ((property as Property).key as Identifier).name ===
-          (v3ClientProperty.key as Identifier).name
+          ((property as Property).key as Identifier).name === keyName &&
+          ((property as Property).value as Identifier).name === valueName
       )
     )
   ) {
-    (existingRequireProperties[0].id as ObjectPattern).properties.push(v3ClientProperty);
+    (existingRequireProperties[0].id as ObjectPattern).properties.push(
+      getV3ClientRequireProperty(j, { keyName, valueName })
+    );
   }
 };
--- src/transforms/v2-to-v3/utils/add/addV3ClientRequires.ts
+++ src/transforms/v2-to-v3/utils/add/addV3ClientRequires.ts
@@ -4,6 +4,7 @@ import { PACKAGE_NAME } from "../config";
 import {
   getRequireVariableDeclaration,
   getV2ServiceModulePath,
+  getV3ClientRequireProperty,
   getV3ClientTypeNames,
 } from "../get";
 import { addV3ClientModuleRequire } from "./addV3ClientModuleRequire";
@@ -20,14 +21,23 @@ const getClientProperty = (j: JSCodeshift, name: Identifier) =>
 export const addV3ClientRequires = (
   j: JSCodeshift,
   source: Collection<unknown>,
-  { v2ClientName, v3ClientName, v3ClientPackageName, v2GlobalName }: AddV3ClientModulesOptions
+  {
+    v2ClientName,
+    v2ClientLocalName,
+    v3ClientName,
+    v3ClientPackageName,
+    v2GlobalName,
+  }: AddV3ClientModulesOptions
 ): void => {
   const v3ClientNameProperty = getClientProperty(j, j.identifier(v3ClientName));
   const existingRequires = getRequireVariableDeclaration(j, source, v3ClientPackageName);
 
   // Require declaration already exists.
   if (existingRequires && existingRequires.nodes().length > 0) {
-    addV3ClientModuleRequire(j, existingRequires, v3ClientNameProperty);
+    addV3ClientModuleRequire(j, existingRequires, {
+      keyName: v3ClientName,
+      valueName: v2ClientLocalName,
+    });
   } else {
     // Insert after default require if present. If not, insert after service require.
     const v2ServiceModulePath = getV2ServiceModulePath(v2ClientName);
@@ -36,16 +46,19 @@ export const addV3ClientRequires = (
 
     const requireVarDecl =
       defaultRequireVarDecl.size() > 0 ? defaultRequireVarDecl : serviceRequireVarDecl;
-    requireVarDecl
-      .at(0)
-      .insertAfter(
-        j.variableDeclaration("const", [
-          j.variableDeclarator(
-            j.objectPattern([v3ClientNameProperty]),
-            j.callExpression(j.identifier("require"), [j.literal(v3ClientPackageName)])
-          ),
-        ])
-      );
+    requireVarDecl.at(0).insertAfter(
+      j.variableDeclaration("const", [
+        j.variableDeclarator(
+          j.objectPattern([
+            getV3ClientRequireProperty(j, {
+              keyName: v3ClientName,
+              valueName: v2ClientLocalName,
+            }),
+          ]),
+          j.callExpression(j.identifier("require"), [j.literal(v3ClientPackageName)])
+        ),
+      ])
+    );
   }
 
   // Add require for input/output types, if needed.
@@ -54,8 +67,10 @@ export const addV3ClientRequires = (
   if (v3ClientTypeNames.length > 0) {
     const clientRequires = getRequireVariableDeclaration(j, source, v3ClientPackageName);
     for (const v3ClientTypeName of v3ClientTypeNames.sort()) {
-      const v3ClientTypeNameProperty = getClientProperty(j, j.identifier(v3ClientTypeName));
-      addV3ClientModuleRequire(j, clientRequires, v3ClientTypeNameProperty);
+      addV3ClientModuleRequire(j, clientRequires, {
+        keyName: v3ClientTypeName,
+        valueName: v3ClientTypeName,
+      });
     }
   }
 };
