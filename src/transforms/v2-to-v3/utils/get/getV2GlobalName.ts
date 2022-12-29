6a6f31276098a461c80dfe9904ef89a94538b5bd
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-29 10:55:06 -0800

    Update getV2GlobalName to read default require/import

--- src/transforms/v2-to-v3/utils/get/getV2GlobalName.ts
+++ src/transforms/v2-to-v3/utils/get/getV2GlobalName.ts
@@ -1,16 +1,43 @@
-import { Collection, JSCodeshift } from "jscodeshift";
+import { Collection, Identifier, JSCodeshift } from "jscodeshift";
 
 import { PACKAGE_NAME } from "../config";
 import { hasRequire } from "../has";
-import { getImportLocalNameForClient } from "./getImportLocalNameForClient";
-import { getRequireLocalNameForClient } from "./getRequireLocalNameForClient";
 
 export const getV2GlobalName = (
   j: JSCodeshift,
   source: Collection<unknown>
 ): string | undefined => {
   if (hasRequire(j, source)) {
-    return getRequireLocalNameForClient(j, source, PACKAGE_NAME);
+    const requireIdentifiers = source
+      .find(j.VariableDeclarator, {
+        id: { type: "Identifier" },
+        init: {
+          type: "CallExpression",
+          callee: { type: "Identifier", name: "require" },
+          arguments: [{ value: PACKAGE_NAME }],
+        },
+      })
+      .nodes();
+    if (requireIdentifiers.length > 0) {
+      return (requireIdentifiers[0]?.id as Identifier).name;
+    }
   }
-  return getImportLocalNameForClient(j, source, PACKAGE_NAME);
+
+  const importDefaultNamespaceSpecifiers = source
+    .find(j.ImportDeclaration, {
+      type: "ImportDeclaration",
+      source: { value: PACKAGE_NAME },
+    })
+    .nodes()
+    .map((importDeclaration) => importDeclaration.specifiers)
+    .flat()
+    .filter((specifier) =>
+      ["ImportDefaultSpecifier", "ImportNamespaceSpecifier"].includes(specifier?.type as string)
+    );
+
+  if (importDefaultNamespaceSpecifiers.length > 0) {
+    return (importDefaultNamespaceSpecifiers[0]?.local as Identifier).name;
+  }
+
+  return undefined;
 };
