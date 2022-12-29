28584c359d377031e786d3a385b9c59bd06a8352
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-28 23:34:42 -0800

    Read named imports in getImportIdentifierName

--- src/transforms/v2-to-v3/utils/get/getImportIdentifierName.ts
+++ src/transforms/v2-to-v3/utils/get/getImportIdentifierName.ts
@@ -1,27 +1,43 @@
-import { Collection, Identifier, JSCodeshift } from "jscodeshift";
+import { Collection, Identifier, ImportSpecifier, JSCodeshift } from "jscodeshift";
 
+import { PACKAGE_NAME } from "../config";
 import { getV2ServiceModulePath } from "./getV2ServiceModulePath";
 
-export const getImportIdentifierName = (
-  j: JSCodeshift,
-  source: Collection<unknown>,
-  clientName: string
-): string | undefined => {
-  const sourceValue = getV2ServiceModulePath(clientName);
-  const importSpecifiers = source
+const getImportSpecifiers = (j: JSCodeshift, source: Collection<unknown>, sourceValue: string) =>
+  source
     .find(j.ImportDeclaration, {
       type: "ImportDeclaration",
       source: { value: sourceValue },
     })
     .nodes()
     .map((importDeclaration) => importDeclaration.specifiers)
-    .flat()
-    .filter((specifier) =>
-      ["ImportDefaultSpecifier", "ImportNamespaceSpecifier"].includes(specifier?.type as string)
-    );
+    .flat();
 
-  if (importSpecifiers.length > 0) {
-    return (importSpecifiers[0]?.local as Identifier).name;
+export const getImportIdentifierName = (
+  j: JSCodeshift,
+  source: Collection<unknown>,
+  clientName: string
+): string | undefined => {
+  const specifiersFromNamedImport = getImportSpecifiers(j, source, PACKAGE_NAME).filter(
+    (specifier) => specifier?.type === "ImportSpecifier"
+  ) as ImportSpecifier[];
+
+  const clientImportSpecifier = specifiersFromNamedImport.find(
+    (specifier) => specifier?.imported.name === clientName
+  );
+  if (clientImportSpecifier) {
+    return (clientImportSpecifier.local as Identifier).name;
   }
+
+  const deepImportPath = getV2ServiceModulePath(clientName);
+  const specifiersFromDeepImport = getImportSpecifiers(j, source, deepImportPath).filter(
+    (specifier) =>
+      ["ImportDefaultSpecifier", "ImportNamespaceSpecifier"].includes(specifier?.type as string)
+  );
+
+  if (specifiersFromDeepImport.length > 0) {
+    return (specifiersFromDeepImport[0]?.local as Identifier).name;
+  }
+
   return undefined;
 };
