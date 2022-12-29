c407c729ccb5739aeeedb02b3778197a22cd2176
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-28 23:15:16 -0800

    Temporarily run only new-client/service-import tests

--- src/transforms/v2-to-v3/transformer.spec.ts
+++ src/transforms/v2-to-v3/transformer.spec.ts
@@ -37,9 +37,9 @@ describe("v2-to-v3", () => {
     return { input, outputCode };
   };
 
-  describe.each(fixtureSubDirs)("%s", (subDir) => {
+  describe.each(["new-client"])("%s", (subDir) => {
     const subDirPath = join(fixtureDir, subDir);
-    it.concurrent.each(getTestFileMetadata(subDirPath))(
+    it.concurrent.each([["service-import", "js"]])(
       `transforms: %s.%s`,
       async (filePrefix, fileExtension) => {
         const { input, outputCode } = await getTestMetadata(subDirPath, filePrefix, fileExtension);
