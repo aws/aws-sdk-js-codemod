8dcbbf9a1fa4cba387dbd4c71519244cfd29b840
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-29 20:27:12 -0800

    Remove redundant output type from getV2ClientNamesRecord

--- src/transforms/v2-to-v3/utils/get/getV2ClientNamesRecord.ts
+++ src/transforms/v2-to-v3/utils/get/getV2ClientNamesRecord.ts
@@ -4,10 +4,7 @@ import { hasRequire } from "../has";
 import { getV2ClientNamesRecordFromImport } from "./getV2ClientNamesRecordFromImport";
 import { getV2ClientNamesRecordFromRequire } from "./getV2ClientNamesRecordFromRequire";
 
-export const getV2ClientNamesRecord = (
-  j: JSCodeshift,
-  source: Collection<unknown>
-): Record<string, string> =>
+export const getV2ClientNamesRecord = (j: JSCodeshift, source: Collection<unknown>) =>
   hasRequire(j, source)
     ? getV2ClientNamesRecordFromRequire(j, source)
     : getV2ClientNamesRecordFromImport(j, source);
