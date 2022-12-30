bd7b222460ab7b74f2fc91b754a59a0c54a1fe9c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-12-30 12:54:42 -0800

    Add utility getRequiresWithArgument

--- src/transforms/v2-to-v3/utils/add/addV3ClientRequires.ts
+++ src/transforms/v2-to-v3/utils/add/addV3ClientRequires.ts
@@ -1,4 +1,10 @@
-import { CallExpression, Collection, JSCodeshift, VariableDeclarator } from "jscodeshift";
+import {
+  CallExpression,
+  Collection,
+  JSCodeshift,
+  VariableDeclaration,
+  VariableDeclarator,
+} from "jscodeshift";
 
 import { PACKAGE_NAME } from "../config";
 import {
@@ -11,42 +17,48 @@ import { hasPropertyWithName } from "../has";
 import { ClientCodemodOptions } from "../types";
 import { addV3ClientModuleRequire } from "./addV3ClientModuleRequire";
 
+const getRequiresWithArgument = (
+  existingRequires: Collection<VariableDeclaration>,
+  argumentValue: string
+) =>
+  existingRequires.filter((varDeclaration) =>
+    varDeclaration.value.declarations.some((varDeclarator) => {
+      const init = (varDeclarator as VariableDeclarator).init as CallExpression;
+      const argument = init?.arguments[0];
+      if (argument.type !== "Literal" && argument.type !== "StringLiteral") return false;
+      return argument.value === argumentValue;
+    })
+  );
+
 export const addV3ClientRequires = (
   j: JSCodeshift,
   source: Collection<unknown>,
   { v2GlobalName, clientMetadataRecord }: ClientCodemodOptions
 ): void => {
   const existingRequires = getRequireVariableDeclaration(j, source);
+  const globalRequires = getRequiresWithArgument(existingRequires, PACKAGE_NAME);
 
   for (const [v2ClientName, v3ClientMetadata] of Object.entries(clientMetadataRecord)) {
     const { v2ClientLocalName, v3ClientName, v3ClientPackageName } = v3ClientMetadata;
 
-    const existingRequiresForClient = existingRequires.filter((varDeclaration) => {
-      return varDeclaration.value.declarations.some((varDeclarator) => {
-        const init = (varDeclarator as VariableDeclarator).init as CallExpression;
-        const argument = init?.arguments[0];
-        if (argument.type !== "Literal") return false;
-        return argument.value === v3ClientPackageName;
-      });
-    });
+    const clientRequires = getRequiresWithArgument(existingRequires, v3ClientPackageName);
 
     // Require declaration already exists.
-    if (existingRequiresForClient && existingRequiresForClient.nodes().length > 0) {
-      addV3ClientModuleRequire(j, existingRequiresForClient, {
+    if (clientRequires && clientRequires.nodes().length > 0) {
+      addV3ClientModuleRequire(j, clientRequires, {
         keyName: v3ClientName,
         valueName: v2ClientLocalName,
       });
     } else {
       // Insert after require for global SDK if present. If not, insert after service require.
       const v2ServiceModulePath = getV2ServiceModulePath(v2ClientName);
-      const globalRequireVarDecl = getRequireVariableDeclaration(j, source, PACKAGE_NAME).filter(
-        (varDeclaration) =>
-          hasPropertyWithName(varDeclaration, {
-            identifierName: v2GlobalName,
-            objectPropertyName: v2ClientLocalName,
-          })
+      const globalRequireVarDecl = globalRequires.filter((varDeclaration) =>
+        hasPropertyWithName(varDeclaration, {
+          identifierName: v2GlobalName,
+          objectPropertyName: v2ClientLocalName,
+        })
       );
-      const serviceRequireVarDecl = getRequireVariableDeclaration(j, source, v2ServiceModulePath);
+      const serviceRequireVarDecl = getRequiresWithArgument(existingRequires, v2ServiceModulePath);
 
       const requireVarDecl =
         globalRequireVarDecl.size() > 0 ? globalRequireVarDecl : serviceRequireVarDecl;
