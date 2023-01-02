68223791ccec0345ec1984b1140c8cbbb462290b
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-02 10:23:46 -0800

    Check for v2GlobalName in getV2RequireDeclarator

--- src/transforms/v2-to-v3/modules/getV2RequireDeclarator.ts
+++ src/transforms/v2-to-v3/modules/getV2RequireDeclarator.ts
@@ -15,13 +15,15 @@ export const getV2RequireDeclarator = (
   source: Collection<unknown>,
   { v2ClientName, v2ClientLocalName, v2GlobalName }: GetV2BaseDeclaratorOptions
 ) => {
-  const v2GlobalNameIdentifier = { type: "Identifier", name: v2GlobalName } as Identifier;
-  // prettier-ignore
-  const v2GlobalNameIdentifierDeclarator =
-    getRequireVariableDeclarator(j, source, v2GlobalNameIdentifier, PACKAGE_NAME);
+  if (v2GlobalName) {
+    const v2GlobalNameIdentifier = { type: "Identifier", name: v2GlobalName } as Identifier;
+    // prettier-ignore
+    const v2GlobalNameIdentifierDeclarator =
+      getRequireVariableDeclarator(j, source, v2GlobalNameIdentifier, PACKAGE_NAME);
 
-  if (v2GlobalNameIdentifierDeclarator && v2GlobalNameIdentifierDeclarator.nodes().length > 0) {
-    return v2GlobalNameIdentifierDeclarator;
+    if (v2GlobalNameIdentifierDeclarator && v2GlobalNameIdentifierDeclarator.nodes().length > 0) {
+      return v2GlobalNameIdentifierDeclarator;
+    }
   }
 
   const v2ClientLocalNameObject = {
