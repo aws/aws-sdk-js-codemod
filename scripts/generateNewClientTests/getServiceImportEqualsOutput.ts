fb298b049325a7ad1a85e119cec7e97efaf897ae
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-03 11:50:05 -0800

    Update order of clients in ServiceImportEqualsOutput script

--- scripts/generateNewClientTests/getServiceImportEqualsOutput.ts
+++ scripts/generateNewClientTests/getServiceImportEqualsOutput.ts
@@ -1,3 +1,26 @@
-import { getGlobalImportEqualsOutput } from "./getGlobalImportEqualsOutput";
+import { CLIENT_NAMES_MAP, CLIENT_PACKAGE_NAMES_MAP } from "../../src/transforms/v2-to-v3/config";
+import { getV3ClientDefaultLocalName } from "../../src/transforms/v2-to-v3/utils";
+import { CLIENTS_TO_TEST } from "./config";
+import { getV3ClientsNewExpressionCode } from "./getV3ClientsNewExpressionCode";
 
-export const getServiceImportEqualsOutput = getGlobalImportEqualsOutput;
+export const getServiceImportEqualsOutput = (codegenComment: string) => {
+  let serviceImportEqualsOutputContent = `${codegenComment};\n`;
+
+  for (const v2ClientName of CLIENTS_TO_TEST) {
+    const v3ClientDefaultLocalName = getV3ClientDefaultLocalName(v2ClientName);
+    const v3ClientPackageName = `@aws-sdk/${CLIENT_PACKAGE_NAMES_MAP[v2ClientName]}`;
+    serviceImportEqualsOutputContent += `import ${v3ClientDefaultLocalName} = require("${v3ClientPackageName}");\n\n`;
+
+    const v3ClientName = CLIENT_NAMES_MAP[v2ClientName];
+    const v3ObjectPattern =
+      v3ClientName === v2ClientName ? v3ClientName : `${v3ClientName}: ${v2ClientName}`;
+    serviceImportEqualsOutputContent +=
+      `const {\n` +
+      `  ${v3ObjectPattern}\n` +
+      `} = ${getV3ClientDefaultLocalName(v2ClientName)};\n\n`;
+  }
+
+  serviceImportEqualsOutputContent += getV3ClientsNewExpressionCode(CLIENTS_TO_TEST);
+
+  return serviceImportEqualsOutputContent;
+};
