b538d0cbf782cc8696d2da75a063096f811700ae
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-02 19:16:06 -0800

    Delete utility isV2ClientInputOutputType

--- src/transforms/v2-to-v3/ts-type/isV2ClientInputOutputType.ts
+++
@@ -1,6 +0,0 @@
-import { V2_CLIENT_INPUT_SUFFIX_LIST, V2_CLIENT_OUTPUT_SUFFIX_LIST } from "../config";
-
-export const isV2ClientInputOutputType = (v2ClientTypeName: string) =>
-  [...V2_CLIENT_INPUT_SUFFIX_LIST, ...V2_CLIENT_OUTPUT_SUFFIX_LIST].some((suffix) =>
-    v2ClientTypeName.endsWith(suffix)
-  );
