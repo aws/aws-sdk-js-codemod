6022a0a61674bf4ca81ff6a39770b60dc99b1a96
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-04 00:21:23 -0800

    Add utility getNewExpressionCount

---
+++ src/transforms/v2-to-v3/modules/getNewExpressionCount.ts
@@ -0,0 +1,28 @@
+import { Collection, JSCodeshift } from "jscodeshift";
+
+import { getV2ClientNewExpression } from "../utils";
+import { V3ClientModulesOptions } from "./types";
+
+export const getNewExpressionCount = (
+  j: JSCodeshift,
+  source: Collection<unknown>,
+  { v2ClientName, v2ClientLocalName, v2GlobalName }: V3ClientModulesOptions
+): number => {
+  let newExpressionCount = 0;
+
+  if (v2GlobalName) {
+    const newExpressionsFromGlobal = source.find(
+      j.NewExpression,
+      getV2ClientNewExpression({ v2ClientName, v2GlobalName })
+    );
+    newExpressionCount += newExpressionsFromGlobal.length;
+  }
+
+  const newExpressionsFromClientLocalName = source.find(
+    j.NewExpression,
+    getV2ClientNewExpression({ v2ClientLocalName })
+  );
+  newExpressionCount += newExpressionsFromClientLocalName.length;
+
+  return newExpressionCount;
+};
