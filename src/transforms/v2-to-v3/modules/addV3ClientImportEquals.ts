f65f2b8e78627ad540220e01db78e34ee9909056
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-04 14:28:22 -0800

    Store getV2ImportEqualsDeclaration output in a variable

--- src/transforms/v2-to-v3/modules/addV3ClientImportEquals.ts
+++ src/transforms/v2-to-v3/modules/addV3ClientImportEquals.ts
@@ -35,26 +35,31 @@ export const addV3ClientImportEquals = (
   }
 
   // Insert after global, or service import equals.
-  getV2ImportEqualsDeclaration(j, source, { v2ClientName, v2ClientLocalName, v2GlobalName })
-    .at(0)
-    .insertAfter(
-      j.variableDeclaration("const", [
-        j.variableDeclarator(
-          j.objectPattern([
-            j.objectProperty.from({
-              key: j.identifier(v3ClientName),
-              value: j.identifier(v2ClientLocalName),
-              shorthand: true,
-            }),
-          ]),
-          j.identifier(v3ClientDefaultLocalName)
-        ),
-      ])
+  const v2ImportEqualsDeclaration = getV2ImportEqualsDeclaration(j, source, {
+    v2ClientName,
+    v2ClientLocalName,
+    v2GlobalName,
+  }).at(0);
+
+  v2ImportEqualsDeclaration.insertAfter(
+    j.variableDeclaration("const", [
+      j.variableDeclarator(
+        j.objectPattern([
+          j.objectProperty.from({
+            key: j.identifier(v3ClientName),
+            value: j.identifier(v2ClientLocalName),
+            shorthand: true,
+          }),
+        ]),
+        j.identifier(v3ClientDefaultLocalName)
+      ),
+    ])
+  );
+
+  v2ImportEqualsDeclaration.insertAfter(
+    j.tsImportEqualsDeclaration(
+      j.identifier(v3ClientDefaultLocalName),
+      j.tsExternalModuleReference(j.stringLiteral(v3ClientPackageName))
     )
-    .insertAfter(
-      j.tsImportEqualsDeclaration(
-        j.identifier(v3ClientDefaultLocalName),
-        j.tsExternalModuleReference(j.stringLiteral(v3ClientPackageName))
-      )
-    );
+  );
 };
