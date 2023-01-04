5eeb4e5c3499f9cb52e0639d858a4473405371a3
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-03 23:53:35 -0800

    Add v2ClientLocalName in getV2ClientTSTypeRef

--- src/transforms/v2-to-v3/utils/getV2ClientTSTypeRef.ts
+++ src/transforms/v2-to-v3/utils/getV2ClientTSTypeRef.ts
@@ -1,16 +1,24 @@
 import { Identifier, TSQualifiedName, TSTypeReference } from "jscodeshift";
 
 export interface V2ClientTsTypeRefOptions {
+  v2ClientLocalName?: string;
+  v2ClientName?: string;
   v2GlobalName?: string;
-  v2ClientName: string;
   withoutRightSection?: boolean;
 }
 
 export const getV2ClientTSTypeRef = ({
-  v2GlobalName,
+  v2ClientLocalName,
   v2ClientName,
+  v2GlobalName,
   withoutRightSection = false,
 }: V2ClientTsTypeRefOptions): TSTypeReference => {
+  if (!v2GlobalName && !v2ClientLocalName) {
+    throw new Error(
+      `At least one of the following options must be provided: v2ClientLocalName, v2GlobalName`
+    );
+  }
+
   if (v2GlobalName) {
     const idWithGlobalName = {
       type: "TSQualifiedName",
@@ -25,7 +33,7 @@ export const getV2ClientTSTypeRef = ({
     } as TSTypeReference;
   }
 
-  const idWithClientName = { type: "Identifier", name: v2ClientName } as Identifier;
+  const idWithClientName = { type: "Identifier", name: v2ClientLocalName } as Identifier;
   return {
     typeName: {
       ...(withoutRightSection ? { left: idWithClientName } : idWithClientName),
