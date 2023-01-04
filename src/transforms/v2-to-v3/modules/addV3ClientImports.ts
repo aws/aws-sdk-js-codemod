76e7410f307c17a0f525b574c9edc64382dced4f
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-04 14:18:20 -0800

    Call addV3ClientImportEquals from addV3ClientModules

--- src/transforms/v2-to-v3/modules/addV3ClientImports.ts
+++ src/transforms/v2-to-v3/modules/addV3ClientImports.ts
@@ -2,11 +2,9 @@ import { Collection, JSCodeshift } from "jscodeshift";
 
 import { getV3ClientTypeNames } from "../ts-type";
 import { addV3ClientDefaultImport } from "./addV3ClientDefaultImport";
-import { addV3ClientImportEquals } from "./addV3ClientImportEquals";
 import { addV3ClientNamedImport } from "./addV3ClientNamedImport";
 import { getClientTSTypeRefCount } from "./getClientTSTypeRefCount";
 import { getNewExpressionCount } from "./getNewExpressionCount";
-import { hasImportEquals } from "./hasImportEquals";
 import { V3ClientModulesOptions } from "./types";
 
 export const addV3ClientImports = (
@@ -14,11 +12,6 @@ export const addV3ClientImports = (
   source: Collection<unknown>,
   options: V3ClientModulesOptions
 ): void => {
-  if (hasImportEquals(j, source)) {
-    addV3ClientImportEquals(j, source, options);
-    return;
-  }
-
   const { v2ClientLocalName, v2ClientName, v2GlobalName } = options;
   const v3ClientTypeNames = getV3ClientTypeNames(j, source, {
     v2ClientLocalName,
--- src/transforms/v2-to-v3/modules/addV3ClientModules.ts
+++ src/transforms/v2-to-v3/modules/addV3ClientModules.ts
@@ -1,7 +1,9 @@
 import { Collection, JSCodeshift } from "jscodeshift";
 
+import { addV3ClientImportEquals } from "./addV3ClientImportEquals";
 import { addV3ClientImports } from "./addV3ClientImports";
 import { addV3ClientRequires } from "./addV3ClientRequires";
+import { hasImportEquals } from "./hasImportEquals";
 import { hasRequire } from "./hasRequire";
 import { V3ClientModulesOptions } from "./types";
 
@@ -12,4 +14,6 @@ export const addV3ClientModules = (
 ): void =>
   hasRequire(j, source)
     ? addV3ClientRequires(j, source, options)
+    : hasImportEquals(j, source)
+    ? addV3ClientImportEquals(j, source, options)
     : addV3ClientImports(j, source, options);
