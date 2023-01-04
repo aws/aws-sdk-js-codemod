4947b8cd42e9bcb024af4bdb4da5a27366241b4e
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-01-03 23:03:01 -0800

    Add utility getV2ImportDeclaration (#305)

--- src/transforms/v2-to-v3/modules/addV3ClientDefaultImport.ts
+++ src/transforms/v2-to-v3/modules/addV3ClientDefaultImport.ts
@@ -1,14 +1,16 @@
 import { Collection, ImportDefaultSpecifier, JSCodeshift } from "jscodeshift";
 
 import { getV3ClientDefaultLocalName } from "../utils";
+import { getV2ImportDeclaration } from "./getV2ImportDeclaration";
 import { V3ClientModulesOptions } from "./types";
 
 export const addV3ClientDefaultImport = (
   j: JSCodeshift,
   source: Collection<unknown>,
-  { v2ClientLocalName, v3ClientPackageName }: V3ClientModulesOptions
+  { v2ClientLocalName, v2ClientName, v3ClientPackageName }: V3ClientModulesOptions
 ) => {
   const localName = getV3ClientDefaultLocalName(v2ClientLocalName);
+  const defaultImportSpecifier = j.importDefaultSpecifier(j.identifier(localName));
   const existingImports = source.find(j.ImportDeclaration, {
     source: { value: v3ClientPackageName },
   });
@@ -21,7 +23,33 @@ export const addV3ClientDefaultImport = (
       (importSpecifier) => importSpecifier?.type === "ImportDefaultSpecifier"
     ) as ImportDefaultSpecifier[];
 
-  if (!existingImportDefaultSpecifiers.find((specifier) => specifier?.local?.name === localName)) {
-    existingImports.nodes()[0].specifiers?.push(j.importDefaultSpecifier(j.identifier(localName)));
+  if (existingImports.length) {
+    if (
+      !existingImportDefaultSpecifiers.find((specifier) => specifier?.local?.name === localName)
+    ) {
+      existingImports.nodes()[0].specifiers?.push(defaultImportSpecifier);
+      return;
+    }
+  }
+
+  // Insert after global import, or service import.
+  const v2ImportDeclaration = getV2ImportDeclaration(j, source, {
+    v2ClientName,
+    v2ClientLocalName,
+  });
+
+  const importDeclaration = j.importDeclaration(
+    [defaultImportSpecifier],
+    j.stringLiteral(v3ClientPackageName)
+  );
+
+  if (v2ImportDeclaration && v2ImportDeclaration.nodes().length > 0) {
+    v2ImportDeclaration.at(0).insertAfter(importDeclaration);
+  } else {
+    // Unreachable code, throw error
+    throw new Error(
+      "Base Import Declaration not found to insert new Import Declaration.\n" +
+        "Please report your use case on https://github.com/awslabs/aws-sdk-js-codemod"
+    );
   }
 };
---
+++ src/transforms/v2-to-v3/modules/getV2ImportDeclaration.ts
@@ -0,0 +1,43 @@
+import { Collection, JSCodeshift } from "jscodeshift";
+
+import { PACKAGE_NAME } from "../config";
+import { getV2ServiceModulePath } from "../utils";
+
+export interface GetV2ImportDeclarationOptions {
+  v2ClientName: string;
+  v2ClientLocalName: string;
+}
+
+export const getV2ImportDeclaration = (
+  j: JSCodeshift,
+  source: Collection<unknown>,
+  { v2ClientName, v2ClientLocalName }: GetV2ImportDeclarationOptions
+) =>
+  // Return global or service import declaration.
+  source.find(j.ImportDeclaration).filter((importDeclaration) => {
+    const sourceValue = importDeclaration.value.source.value as string;
+
+    if (
+      sourceValue === PACKAGE_NAME &&
+      importDeclaration.value.specifiers?.some(
+        (specifier) =>
+          ["ImportNamespaceSpecifier", "ImportDefaultSpecifier"].includes(specifier.type) ||
+          (specifier.type === "ImportSpecifier" && specifier.local?.name === v2ClientLocalName)
+      )
+    ) {
+      return true;
+    }
+
+    if (
+      sourceValue === getV2ServiceModulePath(v2ClientName) &&
+      importDeclaration.value.specifiers?.some(
+        (specifier) =>
+          ["ImportNamespaceSpecifier", "ImportDefaultSpecifier"].includes(specifier.type) &&
+          specifier.local?.name === v2ClientLocalName
+      )
+    ) {
+      return true;
+    }
+
+    return false;
+  });
