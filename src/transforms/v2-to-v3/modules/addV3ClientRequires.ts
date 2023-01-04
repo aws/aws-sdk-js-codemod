669ad5902f372f59580d0bd3d08d21bf144b8e04
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-01-04 00:23:49 -0800

    Add utility getNewExpressionCount (#308)

--- src/transforms/v2-to-v3/modules/addV3ClientRequires.ts
+++ src/transforms/v2-to-v3/modules/addV3ClientRequires.ts
@@ -1,9 +1,9 @@
 import { Collection, JSCodeshift } from "jscodeshift";
 
 import { getV3ClientTypeNames } from "../ts-type";
-import { getV2ClientNewExpression } from "../utils";
 import { addV3ClientDefaultRequire } from "./addV3ClientDefaultRequire";
 import { addV3ClientNamedRequire } from "./addV3ClientNamedRequire";
+import { getNewExpressionCount } from "./getNewExpressionCount";
 import { V3ClientModulesOptions } from "./types";
 
 export const addV3ClientRequires = (
@@ -11,7 +11,7 @@ export const addV3ClientRequires = (
   source: Collection<unknown>,
   options: V3ClientModulesOptions
 ): void => {
-  const { v2ClientName, v2ClientLocalName, v2GlobalName } = options;
+  const { v2ClientName, v2GlobalName } = options;
   const v3ClientTypeNames = getV3ClientTypeNames(j, source, { v2ClientName, v2GlobalName });
 
   // Add default require for types, if needed.
@@ -19,12 +19,7 @@ export const addV3ClientRequires = (
     addV3ClientDefaultRequire(j, source, options);
   }
 
-  const newExpressions = source.find(
-    j.NewExpression,
-    getV2ClientNewExpression({ v2ClientName, v2ClientLocalName, v2GlobalName })
-  );
-
-  if (newExpressions.length) {
+  if (getNewExpressionCount(j, source, options) > 0) {
     addV3ClientNamedRequire(j, source, options);
   }
 };
