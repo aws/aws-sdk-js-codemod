e64574d8187f3be4f5ec3ea060d95a281a7f0fc7
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-01-03 22:41:43 -0800

    Remove client require if clients are not created (#302)

--- src/transforms/v2-to-v3/modules/addV3ClientRequires.ts
+++ src/transforms/v2-to-v3/modules/addV3ClientRequires.ts
@@ -1,6 +1,7 @@
 import { Collection, JSCodeshift } from "jscodeshift";
 
 import { getV3ClientTypeNames } from "../ts-type";
+import { getV2ClientNewExpression } from "../utils";
 import { addV3ClientDefaultRequire } from "./addV3ClientDefaultRequire";
 import { addV3ClientNamedRequire } from "./addV3ClientNamedRequire";
 import { V3ClientModulesOptions } from "./types";
@@ -10,7 +11,7 @@ export const addV3ClientRequires = (
   source: Collection<unknown>,
   options: V3ClientModulesOptions
 ): void => {
-  const { v2ClientName, v2GlobalName } = options;
+  const { v2ClientName, v2ClientLocalName, v2GlobalName } = options;
   const v3ClientTypeNames = getV3ClientTypeNames(j, source, { v2ClientName, v2GlobalName });
 
   // Add default require for types, if needed.
@@ -18,5 +19,12 @@ export const addV3ClientRequires = (
     addV3ClientDefaultRequire(j, source, options);
   }
 
-  addV3ClientNamedRequire(j, source, options);
+  const newExpressions = source.find(
+    j.NewExpression,
+    getV2ClientNewExpression({ v2ClientName, v2ClientLocalName, v2GlobalName })
+  );
+
+  if (newExpressions.length) {
+    addV3ClientNamedRequire(j, source, options);
+  }
 };
