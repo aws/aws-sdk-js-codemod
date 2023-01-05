b40f4260766615e86028af155c6d841eaf81296d
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-05 11:29:30 -0800

    Add examples for native types and arrays

--- src/transforms/v2-to-v3/__fixtures__/api-unavailable-type/global-import.input.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-unavailable-type/global-import.input.ts
@@ -1,4 +1,14 @@
 import AWS from "aws-sdk";
 
-// string
-const arn: AWS.ACM.Arn = "arn";
+// Native types
+const stringType: AWS.S3.AccountId = "string"; // string
+const booleanType: AWS.S3.BucketKeyEnabled = true; // boolean
+const numberType: AWS.S3.ContentLength = 123; // number
+
+// Uint8Array
+const blobType: AWS.ACM.CertificateBodyBlob = new Uint8Array();
+
+// Arrays
+const stringArray: AWS.S3.AllowedHeaders = ["string1", "string2"]; // string[]
+const booleanArray: AWS.RDSDataService.BooleanArray = [true, false]; // boolean[]
+const numberArray: AWS.RDSDataService.LongArray = [123, 456]; // number[]
--- src/transforms/v2-to-v3/__fixtures__/api-unavailable-type/global-import.output.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-unavailable-type/global-import.output.ts
@@ -1,3 +1,12 @@
-// string
-const arn: string = "arn";
+// Native types
+const stringType: string = "string"; // string
+const booleanType: boolean = true; // boolean
+const numberType: number = 123; // number
 
+// Uint8Array
+const blobType: Uint8Array = new Uint8Array();
+
+// Arrays
+const stringArray: string[] = ["string1", "string2"]; // string[]
+const booleanArray: boolean[] = [true, false]; // boolean[]
+const numberArray: number[] = [123, 456]; // number[]
