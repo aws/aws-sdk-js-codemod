aa98b0f37cfdf01b92ac8517ace028e5de545db9
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-05 13:58:34 -0800

    Add code for hasReferenceToKeysInValues

--- scripts/generateClientTypesMap/hasReferenceToKeysInValues.ts
+++ scripts/generateClientTypesMap/hasReferenceToKeysInValues.ts
@@ -1,3 +1,13 @@
+/**
+ * Checks if any of the values in the clientTypesMap has a reference to any of the keys.
+ * Reference is defined as key name inside brackets, i.e. `<key>` for `key`.
+ */
 export const hasReferenceToKeysInValues = (clientTypesMap: Record<string, string>) => {
+  const keys = Object.keys(clientTypesMap);
+  for (const value of Object.values(clientTypesMap)) {
+    if (keys.some((key) => value.includes(`<${key}>`))) {
+      return true;
+    }
+  }
   return false;
 };
