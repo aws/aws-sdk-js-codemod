bd83efd71b256782400bd93a45eea66f8df72a60
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-06 01:14:30 -0800

    Remove comments past variables in api-redundant-type

--- src/transforms/v2-to-v3/__fixtures__/api-redundant-type/global-import.input.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-redundant-type/global-import.input.ts
@@ -1,47 +1,47 @@
 import AWS from "aws-sdk";
 
 // Native types
-const stringType: AWS.S3.AccountId = "string"; // string
-const booleanType: AWS.S3.BucketKeyEnabled = true; // boolean
-const numberType: AWS.S3.ContentLength = 123; // number
+const stringType: AWS.S3.AccountId = "string";
+const booleanType: AWS.S3.BucketKeyEnabled = true;
+const numberType: AWS.S3.ContentLength = 123;
 
 // Date
-const dateType: AWS.S3.CreationDate = new Date(); // Date
+const dateType: AWS.S3.CreationDate = new Date();
 
 // Uint8Array
 const blobType: AWS.RDSDataService._Blob = new Uint8Array();
 
 // Arrays
-const stringArray: AWS.S3.AllowedHeaders = ["string1", "string2"]; // string[]
-const booleanArray: AWS.RDSDataService.BooleanArray = [true, false]; // boolean[]
-const numberArray: AWS.RDSDataService.LongArray = [123, 456]; // number[]
-const blobArray: AWS.IoTFleetWise.NetworkFilesList = [new Uint8Array()]; // Uint8Array[]
-const enumArray: AWS.S3.ChecksumAlgorithmList = ["CRC32"]; // ChecksumAlgorithm[]
-const structureArray: AWS.S3.Buckets = [{ Name: "bucketName" }]; // Bucket[]
+const stringArray: AWS.S3.AllowedHeaders = ["string1", "string2"];
+const booleanArray: AWS.RDSDataService.BooleanArray = [true, false];
+const numberArray: AWS.RDSDataService.LongArray = [123, 456];
+const blobArray: AWS.IoTFleetWise.NetworkFilesList = [new Uint8Array()];
+const enumArray: AWS.S3.ChecksumAlgorithmList = ["CRC32"];
+const structureArray: AWS.S3.Buckets = [{ Name: "bucketName" }];
 
 // Maps
-const stringMap: AWS.S3.Metadata = { key: "value" }; // Record<string, string>
-const booleanMap: AWS.APIGateway.MapOfStringToBoolean = { key: true }; // Record<string, boolean>
-const numberMap: AWS.SSM.AssociationStatusAggregatedCount = { key: 123 }; // Record<string, number>
-const structureMap: AWS.APIGateway.MapOfMethodSnapshot = { key: { apiKeyRequired: true } }; // Record<string, MethodSnapshot>
+const stringMap: AWS.S3.Metadata = { key: "value" };
+const booleanMap: AWS.APIGateway.MapOfStringToBoolean = { key: true };
+const numberMap: AWS.SSM.AssociationStatusAggregatedCount = { key: 123 };
+const structureMap: AWS.APIGateway.MapOfMethodSnapshot = { key: { apiKeyRequired: true } };
 
 // Nested arrays
-const arrayNestedTwice: AWS.SageMakerGeospatial.LinearRing = [[1, 2], [3, 4]]; // number[][]
-const arrayNestedThrice: AWS.SageMakerGeospatial.LinearRings = [[[1, 2], [3, 4]], [[4, 5], [6, 7]]]; // number[][][]
+const arrayNestedTwice: AWS.SageMakerGeospatial.LinearRing = [[1, 2], [3, 4]];
+const arrayNestedThrice: AWS.SageMakerGeospatial.LinearRings = [[[1, 2], [3, 4]], [[4, 5], [6, 7]]];
 const arrayNestedFour: AWS.SageMakerGeospatial.LinearRingsList = [
   [[[1], [2]], [[3], [4]]],
   [[[5], [6]], [[7], [8]]]
-]; // number[][][][]
+];
 
 // Nested maps
-const mapNestedTwice: AWS.LexModelsV2.ConditionMap = { key: stringMap }; // Record<string, Record<string, string>>
-const mapNestedTwiceStruct: AWS.APIGateway.PathToMapOfMethodSnapshot = { key: structureMap }; // Record<string, Record<string, MethodSnapshot>>
+const mapNestedTwice: AWS.LexModelsV2.ConditionMap = { key: stringMap };
+const mapNestedTwiceStruct: AWS.APIGateway.PathToMapOfMethodSnapshot = { key: structureMap };
 
 // Nested arrays and maps
-const mapOfArrays: AWS.NetworkManager.FilterMap = { key: ["value"] }; // Record<string, string[]>
-const mapOfMapOfArrays: AWS.SecurityLake.AllDimensionsMap = { key: mapOfArrays }; // Record<string, Record<string, string[]>>
-const mapOfArrayOfMaps: AWS.DynamoDB.BatchGetResponseMap = { key: [{ key: { S:"A" }}] }; // Record<string, Record<string, AttributeValue>[]>
-const mapOfArrayOfArrays: AWS.APIGateway.MapOfKeyUsages = { key: [[1], [2]] }; // Record<string, number[][]>
-const arrayOfMaps: AWS.SSM.InventoryItemEntryList = [stringMap]; // Record<string, string>[]
-const arrayOfMapOfArrays: AWS.SSM.TargetMaps = [mapOfArrays]; // Record<string, string[]>[]
-const arrayOfMapOfMapOfArrays: AWS.SecurityLake.RegionSourceTypesAccountsList = [mapOfMapOfArrays]; // Record<string, Record<string, string[]>>[]
+const mapOfArrays: AWS.NetworkManager.FilterMap = { key: ["value"] };
+const mapOfMapOfArrays: AWS.SecurityLake.AllDimensionsMap = { key: mapOfArrays };
+const mapOfArrayOfMaps: AWS.DynamoDB.BatchGetResponseMap = { key: [{ key: { S:"A" }}] };
+const mapOfArrayOfArrays: AWS.APIGateway.MapOfKeyUsages = { key: [[1], [2]] };
+const arrayOfMaps: AWS.SSM.InventoryItemEntryList = [stringMap];
+const arrayOfMapOfArrays: AWS.SSM.TargetMaps = [mapOfArrays];
+const arrayOfMapOfMapOfArrays: AWS.SecurityLake.RegionSourceTypesAccountsList = [mapOfMapOfArrays];
--- src/transforms/v2-to-v3/__fixtures__/api-redundant-type/global-import.output.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-redundant-type/global-import.output.ts
@@ -3,47 +3,47 @@ import AWS_DynamoDB from "@aws-sdk/client-dynamodb";
 import AWS_S3 from "@aws-sdk/client-s3";
 
 // Native types
-const stringType: string = "string"; // string
-const booleanType: boolean = true; // boolean
-const numberType: number = 123; // number
+const stringType: string = "string";
+const booleanType: boolean = true;
+const numberType: number = 123;
 
 // Date
-const dateType: Date = new Date(); // Date
+const dateType: Date = new Date();
 
 // Uint8Array
 const blobType: Uint8Array = new Uint8Array();
 
 // Arrays
-const stringArray: string[] = ["string1", "string2"]; // string[]
-const booleanArray: boolean[] = [true, false]; // boolean[]
-const numberArray: number[] = [123, 456]; // number[]
-const blobArray: Uint8Array[] = [new Uint8Array()]; // Uint8Array[]
-const enumArray: AWS_S3.ChecksumAlgorithm[] = ["CRC32"]; // ChecksumAlgorithm[]
-const structureArray: AWS_S3.Bucket[] = [{ Name: "bucketName" }]; // Bucket[]
+const stringArray: string[] = ["string1", "string2"];
+const booleanArray: boolean[] = [true, false];
+const numberArray: number[] = [123, 456];
+const blobArray: Uint8Array[] = [new Uint8Array()];
+const enumArray: AWS_S3.ChecksumAlgorithm[] = ["CRC32"];
+const structureArray: AWS_S3.Bucket[] = [{ Name: "bucketName" }];
 
 // Maps
-const stringMap: Record<string, string> = { key: "value" }; // Record<string, string>
-const booleanMap: Record<string, boolean> = { key: true }; // Record<string, boolean>
-const numberMap: Record<string, number> = { key: 123 }; // Record<string, number>
-const structureMap: Record<string, AWS_APIGateway.MethodSnapshot> = { key: { apiKeyRequired: true } }; // Record<string, MethodSnapshot>
+const stringMap: Record<string, string> = { key: "value" };
+const booleanMap: Record<string, boolean> = { key: true };
+const numberMap: Record<string, number> = { key: 123 };
+const structureMap: Record<string, AWS_APIGateway.MethodSnapshot> = { key: { apiKeyRequired: true } };
 
 // Nested arrays
-const arrayNestedTwice: number[][] = [[1, 2], [3, 4]]; // number[][]
-const arrayNestedThrice: number[][][] = [[[1, 2], [3, 4]], [[4, 5], [6, 7]]]; // number[][][]
+const arrayNestedTwice: number[][] = [[1, 2], [3, 4]];
+const arrayNestedThrice: number[][][] = [[[1, 2], [3, 4]], [[4, 5], [6, 7]]];
 const arrayNestedFour: number[][][][] = [
   [[[1], [2]], [[3], [4]]],
   [[[5], [6]], [[7], [8]]]
-]; // number[][][][]
+];
 
 // Nested maps
-const mapNestedTwice: Record<string, Record<string, string>> = { key: stringMap }; // Record<string, Record<string, string>>
-const mapNestedTwiceStruct: Record<string, Record<string, AWS_APIGateway.MethodSnapshot>> = { key: structureMap }; // Record<string, Record<string, MethodSnapshot>>
+const mapNestedTwice: Record<string, Record<string, string>> = { key: stringMap };
+const mapNestedTwiceStruct: Record<string, Record<string, AWS_APIGateway.MethodSnapshot>> = { key: structureMap };
 
 // Nested arrays and maps
-const mapOfArrays: Record<string, string[]> = { key: ["value"] }; // Record<string, string[]>
-const mapOfMapOfArrays: Record<string, Record<string, string[]>> = { key: mapOfArrays }; // Record<string, Record<string, Array<string>>>
-const mapOfArrayOfMaps: Record<string, Record<string, AWS_DynamoDB.AttributeValue>[]> = { key: [{ key: { S:"A" }}] }; // Record<string, Record<string, AttributeValue>[]>
-const mapOfArrayOfArrays: Record<string, number[][]> = { key: [[1], [2]] }; // Record<string, number[][]>
-const arrayOfMaps: Record<string, string>[] = [stringMap]; // Record<string, string>[]
-const arrayOfMapOfArrays: Record<string, string[]>[] = [mapOfArrays]; // Record<string, string[]>[]
-const arrayOfMapOfMapOfArrays: Record<string, Record<string, string[]>>[] = [mapOfMapOfArrays]; // Record<string, Record<string, string[]>>[]
+const mapOfArrays: Record<string, string[]> = { key: ["value"] };
+const mapOfMapOfArrays: Record<string, Record<string, string[]>> = { key: mapOfArrays };
+const mapOfArrayOfMaps: Record<string, Record<string, AWS_DynamoDB.AttributeValue>[]> = { key: [{ key: { S:"A" }}] };
+const mapOfArrayOfArrays: Record<string, number[][]> = { key: [[1], [2]] };
+const arrayOfMaps: Record<string, string>[] = [stringMap];
+const arrayOfMapOfArrays: Record<string, string[]>[] = [mapOfArrays];
+const arrayOfMapOfMapOfArrays: Record<string, Record<string, string[]>>[] = [mapOfMapOfArrays];
