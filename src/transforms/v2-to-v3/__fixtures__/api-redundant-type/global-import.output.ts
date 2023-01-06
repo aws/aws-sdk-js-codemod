1ac580d189a8ea6d4c8b2bdd9d075388aed0b982
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-06 01:17:28 -0800

    Use Array instead of brackets

--- src/transforms/v2-to-v3/__fixtures__/api-redundant-type/global-import.output.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-redundant-type/global-import.output.ts
@@ -14,12 +14,12 @@ const dateType: Date = new Date();
 const blobType: Uint8Array = new Uint8Array();
 
 // Arrays
-const stringArray: string[] = ["string1", "string2"];
-const booleanArray: boolean[] = [true, false];
-const numberArray: number[] = [123, 456];
-const blobArray: Uint8Array[] = [new Uint8Array()];
-const enumArray: AWS_S3.ChecksumAlgorithm[] = ["CRC32"];
-const structureArray: AWS_S3.Bucket[] = [{ Name: "bucketName" }];
+const stringArray: Array<string> = ["string1", "string2"];
+const booleanArray: Array<boolean> = [true, false];
+const numberArray: Array<number> = [123, 456];
+const blobArray: Array<Uint8Array> = [new Uint8Array()];
+const enumArray: Array<AWS_S3.ChecksumAlgorithm> = ["CRC32"];
+const structureArray: Array<AWS_S3.Bucket> = [{ Name: "bucketName" }];
 
 // Maps
 const stringMap: Record<string, string> = { key: "value" };
@@ -28,9 +28,9 @@ const numberMap: Record<string, number> = { key: 123 };
 const structureMap: Record<string, AWS_APIGateway.MethodSnapshot> = { key: { apiKeyRequired: true } };
 
 // Nested arrays
-const arrayNestedTwice: number[][] = [[1, 2], [3, 4]];
-const arrayNestedThrice: number[][][] = [[[1, 2], [3, 4]], [[4, 5], [6, 7]]];
-const arrayNestedFour: number[][][][] = [
+const arrayNestedTwice: Array<Array<number>> = [[1, 2], [3, 4]];
+const arrayNestedThrice: Array<Array<Array<number>>> = [[[1, 2], [3, 4]], [[4, 5], [6, 7]]];
+const arrayNestedFour: Array<Array<Array<Array<number>>>> = [
   [[[1], [2]], [[3], [4]]],
   [[[5], [6]], [[7], [8]]]
 ];
@@ -40,10 +40,10 @@ const mapNestedTwice: Record<string, Record<string, string>> = { key: stringMap 
 const mapNestedTwiceStruct: Record<string, Record<string, AWS_APIGateway.MethodSnapshot>> = { key: structureMap };
 
 // Nested arrays and maps
-const mapOfArrays: Record<string, string[]> = { key: ["value"] };
-const mapOfMapOfArrays: Record<string, Record<string, string[]>> = { key: mapOfArrays };
-const mapOfArrayOfMaps: Record<string, Record<string, AWS_DynamoDB.AttributeValue>[]> = { key: [{ key: { S:"A" }}] };
-const mapOfArrayOfArrays: Record<string, number[][]> = { key: [[1], [2]] };
-const arrayOfMaps: Record<string, string>[] = [stringMap];
-const arrayOfMapOfArrays: Record<string, string[]>[] = [mapOfArrays];
-const arrayOfMapOfMapOfArrays: Record<string, Record<string, string[]>>[] = [mapOfMapOfArrays];
+const mapOfArrays: Record<string, Array<string>> = { key: ["value"] };
+const mapOfMapOfArrays: Record<string, Record<string, Array<string>>> = { key: mapOfArrays };
+const mapOfArrayOfMaps: Record<string, Array<Record<string, AWS_DynamoDB.AttributeValue>>> = { key: [{ key: { S:"A" }}] };
+const mapOfArrayOfArrays: Record<string, Array<Array<number>>> = { key: [[1], [2]] };
+const arrayOfMaps: Array<Record<string, string>> = [stringMap];
+const arrayOfMapOfArrays: Array<Record<string, Array<string>>> = [mapOfArrays];
+const arrayOfMapOfMapOfArrays: Array<Record<string, Record<string, Array<string>>>> = [mapOfMapOfArrays];
