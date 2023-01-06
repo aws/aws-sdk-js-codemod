85dfd37990a044180d9f3f7e7ccfccb6a43367f6
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-06 00:27:41 -0800

    Return type for number and boolean

--- src/transforms/v2-to-v3/ts-type/getTypeRefForString.ts
+++ src/transforms/v2-to-v3/ts-type/getTypeRefForString.ts
@@ -5,5 +5,17 @@ export const getTypeRefForString = (
   v3ClientDefaultLocalName: string,
   v3ClientTypeString: string
 ): TSType => {
+  if (v3ClientTypeString === "string") {
+    return j.tsStringKeyword();
+  }
+
+  if (v3ClientTypeString === "number") {
+    return j.tsNumberKeyword();
+  }
+
+  if (v3ClientTypeString === "boolean") {
+    return j.tsBooleanKeyword();
+  }
+
   return j.tsStringKeyword();
 };
