dc4e0850183f15b134fd92fc3816cc867fc47129
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-06 15:44:17 -0800

    Update script getServiceImportDeepOutput to maintain input import order

--- scripts/generateNewClientTests/getServiceImportDeepOutput.ts
+++ scripts/generateNewClientTests/getServiceImportDeepOutput.ts
@@ -1,3 +1,13 @@
-import { getServiceImportOutput } from "./getServiceImportOutput";
+import { CLIENTS_TO_TEST } from "./config";
+import { getV3ClientsNewExpressionCode } from "./getV3ClientsNewExpressionCode";
+import { getV3PackageImportsCode } from "./getV3PackageImportsCode";
 
-export const getServiceImportDeepOutput = getServiceImportOutput;
+export const getServiceImportDeepOutput = (codegenComment: string) => {
+  let serviceImportOutputContent = `${codegenComment}\n`;
+
+  serviceImportOutputContent += getV3PackageImportsCode(CLIENTS_TO_TEST);
+  serviceImportOutputContent += `\n`;
+  serviceImportOutputContent += getV3ClientsNewExpressionCode(CLIENTS_TO_TEST);
+
+  return serviceImportOutputContent;
+};
