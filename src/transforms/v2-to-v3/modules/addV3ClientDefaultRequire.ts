d36a69f2bb650b8a7c53fa8541366589ab3d0a09
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-06 20:19:15 -0800

    Call utility from addV3ClientDefaultRequire

--- src/transforms/v2-to-v3/modules/addV3ClientDefaultRequire.ts
+++ src/transforms/v2-to-v3/modules/addV3ClientDefaultRequire.ts
@@ -1,7 +1,7 @@
 import { Collection, JSCodeshift } from "jscodeshift";
 
 import { getV3ClientDefaultLocalName } from "../utils";
-import { getRequireDeclaratorsWithIdentifier } from "./getRequireDeclaratorsWithIdentifier";
+import { getRequireDeclarators } from "./getRequireDeclarators";
 import { getV2RequireDeclarator } from "./getV2RequireDeclarator";
 import { V3ClientModulesOptions } from "./types";
 
@@ -11,10 +11,7 @@ export const addV3ClientDefaultRequire = (
   { v2ClientName, v2ClientLocalName, v3ClientPackageName, v2GlobalName }: V3ClientModulesOptions
 ) => {
   const identifierName = getV3ClientDefaultLocalName(v2ClientLocalName);
-  const existingRequires = getRequireDeclaratorsWithIdentifier(j, source, {
-    identifierName,
-    sourceValue: v3ClientPackageName,
-  });
+  const existingRequires = getRequireDeclarators(j, source, v3ClientPackageName);
 
   if (
     existingRequires &&
