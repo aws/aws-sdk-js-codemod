76c88b13b8061771ca0186517206726b0a75143e
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-06 20:27:40 -0800

    Update scripts which populate new-client tests

--- scripts/generateNewClientTests/getServiceRequireInput.ts
+++ scripts/generateNewClientTests/getServiceRequireInput.ts
@@ -4,9 +4,7 @@ import { getV2ClientsNewExpressionCode } from "./getV2ClientsNewExpressionCode";
 export const getServiceRequireInput = (codegenComment: string) => {
   let serviceRequireInputContent = `${codegenComment}\n`;
 
-  for (const clientName of CLIENTS_TO_TEST) {
-    serviceRequireInputContent += `const { ${clientName} } = require("aws-sdk");\n`;
-  }
+  serviceRequireInputContent += `const { ${CLIENTS_TO_TEST.join(", ")} } = require("aws-sdk");\n`;
   serviceRequireInputContent += `\n`;
   serviceRequireInputContent += getV2ClientsNewExpressionCode(CLIENTS_TO_TEST);
 
