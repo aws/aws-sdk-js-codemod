9dff77f7f0ed83ca3f18092022de50c6b4f6663a
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-06 18:54:33 -0800

    Throw error in case of unreachable code path in addV3Client*Require

--- src/transforms/v2-to-v3/modules/addV3ClientNamedRequire.ts
+++ src/transforms/v2-to-v3/modules/addV3ClientNamedRequire.ts
@@ -87,7 +87,10 @@ export const addV3ClientNamedRequire = (
   if (v2RequireDeclarator && v2RequireDeclarator.nodes().length > 0) {
     v2RequireDeclarator.insertAfter(requireDeclarator);
   } else {
-    // Insert at the top of the file.
-    source.insertBefore(requireDeclarator);
+    // Unreachable code, throw error
+    throw new Error(
+      "Base Require Declarator not found to insert new Require Declarator.\n" +
+        "Please report your use case on https://github.com/awslabs/aws-sdk-js-codemod"
+    );
   }
 };
