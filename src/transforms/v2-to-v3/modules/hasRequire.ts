982f96b7e3419b61f7425ce30837daf0bf38c318
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-09 07:47:13 -0800

    Add check for type as StringLiteral in hasRequire

--- src/transforms/v2-to-v3/modules/hasRequire.ts
+++ src/transforms/v2-to-v3/modules/hasRequire.ts
@@ -9,7 +9,7 @@ export const hasRequire = (j: JSCodeshift, source: Collection<unknown>) =>
       const { arguments: args } = callExpression.value;
       return (
         args.length > 0 &&
-        args[0].type === "Literal" &&
+        (args[0].type === "Literal" || args[0].type === "StringLiteral") &&
         typeof args[0].value === "string" &&
         args[0].value.startsWith("aws-sdk")
       );
