80023f9de087857dedeb49fd7d73162a1af01b58
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-18 19:27:44 +0000

    Merge no params and params API calls in api-promise/service-import

--- src/transforms/v2-to-v3/__fixtures__/api-promise/service-import.input.js
+++ src/transforms/v2-to-v3/__fixtures__/api-promise/service-import.input.js
@@ -2,59 +2,55 @@ import DynamoDBClient from "aws-sdk/clients/dynamodb";
 
 const client = new DynamoDBClient();
 
-// Promise without params
-{
-  // async/await
-  try {
-    await client.listTables().promise();
-    console.log(data);
-  } catch (err) {
-    console.log(err, err.stack);
-  }
-
-  // .then() and .catch()
-  client
-    .listTables()
-    .promise()
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err, err.stack));
-
-  // Client as class member
-  class ClientClassMember {
-    constructor(clientInCtr = new DynamoDBClient()) {
-      this.clientInClass = clientInCtr;
-    }
-  
-    async listTables() {
-      return await this.clientInClass.listTables().promise();
-    }
-  }
-
-  // Variable declarator
-  const listTablesPromise = client.listTables().promise();
-
-  // Promise call on request in variable declarator
-  const listTablesRequest = client.listTables();
-  listTablesRequest
-    .promise()
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err, err.stack));
+// async/await
+try {
+  await client.listTables().promise();
+  await client.listTagsOfResource({ ResourceArn: "STRING_VALUE" }).promise();
+  console.log(data);
+} catch (err) {
+  console.log(err, err.stack);
 }
 
-// Promise with params
-{
-  // async/await
-  try {
-    await client.listTagsOfResource({ ResourceArn: "STRING_VALUE" }).promise();
-    console.log(data);
-  } catch (err) {
-    console.log(err, err.stack);
+// .then() and .catch()
+client
+  .listTables()
+  .promise()
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err, err.stack));
+client
+  .listTagsOfResource({ ResourceArn: "STRING_VALUE" })
+  .promise()
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err, err.stack));
+
+// Client as class member
+class ClientClassMember {
+  constructor(clientInCtr = new DynamoDBClient()) {
+    this.clientInClass = clientInCtr;
   }
 
-  // .then() and .catch()
-  client
-    .listTagsOfResource({ ResourceArn: "STRING_VALUE" })
-    .promise()
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err, err.stack));
+  async listTables() {
+    return await this.clientInClass.listTables().promise();
+  }
+
+  async listTagsOfResource() {
+    return await this.clientInClass.listTagsOfResource({ ResourceArn: "STRING_VALUE" }).promise();
+  }
 }
+
+// Variable declarator
+const listTablesPromise = client.listTables().promise();
+const listTagsOfResourcePromise = client.listTagsOfResource({ ResourceArn: "STRING_VALUE" }).promise();
+
+// Promise call on request in variable declarator
+const listTablesRequest = client.listTables();
+listTablesRequest
+  .promise()
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err, err.stack));
+
+const listTagsOfResourceRequest = client.listTagsOfResource({ ResourceArn: "STRING_VALUE" });
+listTagsOfResourceRequest
+  .promise()
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err, err.stack));
--- src/transforms/v2-to-v3/__fixtures__/api-promise/service-import.output.js
+++ src/transforms/v2-to-v3/__fixtures__/api-promise/service-import.output.js
@@ -2,56 +2,51 @@ import { DynamoDB as DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
 const client = new DynamoDBClient();
 
-// Promise without params
-{
-  // async/await
-  try {
-    await client.listTables();
-    console.log(data);
-  } catch (err) {
-    console.log(err, err.stack);
-  }
-
-  // .then() and .catch()
-  client
-    .listTables()
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err, err.stack));
-
-  // Client as class member
-  class ClientClassMember {
-    constructor(clientInCtr = new DynamoDBClient()) {
-      this.clientInClass = clientInCtr;
-    }
-  
-    async listTables() {
-      return await this.clientInClass.listTables();
-    }
-  }
-
-  // Variable declarator
-  const listTablesPromise = client.listTables();
-
-  // Promise call on request in variable declarator
-  const listTablesRequest = client.listTables();
-  listTablesRequest
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err, err.stack));
+// async/await
+try {
+  await client.listTables();
+  await client.listTagsOfResource({ ResourceArn: "STRING_VALUE" });
+  console.log(data);
+} catch (err) {
+  console.log(err, err.stack);
 }
 
-// Promise with params
-{
-  // async/await
-  try {
-    await client.listTagsOfResource({ ResourceArn: "STRING_VALUE" });
-    console.log(data);
-  } catch (err) {
-    console.log(err, err.stack);
+// .then() and .catch()
+client
+  .listTables()
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err, err.stack));
+client
+  .listTagsOfResource({ ResourceArn: "STRING_VALUE" })
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err, err.stack));
+
+// Client as class member
+class ClientClassMember {
+  constructor(clientInCtr = new DynamoDBClient()) {
+    this.clientInClass = clientInCtr;
   }
 
-  // .then() and .catch()
-  client
-    .listTagsOfResource({ ResourceArn: "STRING_VALUE" })
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err, err.stack));
+  async listTables() {
+    return await this.clientInClass.listTables();
+  }
+
+  async listTagsOfResource() {
+    return await this.clientInClass.listTagsOfResource({ ResourceArn: "STRING_VALUE" });
+  }
 }
+
+// Variable declarator
+const listTablesPromise = client.listTables();
+const listTagsOfResourcePromise = client.listTagsOfResource({ ResourceArn: "STRING_VALUE" });
+
+// Promise call on request in variable declarator
+const listTablesRequest = client.listTables();
+listTablesRequest
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err, err.stack));
+
+const listTagsOfResourceRequest = client.listTagsOfResource({ ResourceArn: "STRING_VALUE" });
+listTagsOfResourceRequest
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err, err.stack));
