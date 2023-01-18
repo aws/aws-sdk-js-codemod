83ae18f6f70e3e140ea81d50a54fe96f77ea1b73
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-18 19:02:56 +0000

    Add acallExpression arguments in ArrowFunction/Return while remove promise()

--- src/transforms/v2-to-v3/apis/removePromiseForCallExpression.ts
+++ src/transforms/v2-to-v3/apis/removePromiseForCallExpression.ts
@@ -2,27 +2,34 @@ import { ASTPath, CallExpression, MemberExpression } from "jscodeshift";
 
 export const removePromiseForCallExpression = (callExpression: ASTPath<CallExpression>) => {
   switch (callExpression.parentPath.value.type) {
-    case "AwaitExpression":
+    case "AwaitExpression": {
       callExpression.parentPath.value.argument = (
         callExpression.value.callee as MemberExpression
       ).object;
       break;
-    case "MemberExpression":
+    }
+    case "MemberExpression": {
       callExpression.parentPath.value.object = (
         callExpression.value.callee as MemberExpression
       ).object;
       break;
-    case "VariableDeclarator":
+    }
+    case "VariableDeclarator": {
       callExpression.parentPath.value.init = (
         callExpression.value.callee as MemberExpression
       ).object;
       break;
+    }
     case "ArrowFunctionExpression":
-    case "ReturnStatement":
-      callExpression.value.callee = (
-        (callExpression.value.callee as MemberExpression).object as CallExpression
-      ).callee;
+    case "ReturnStatement": {
+      const currentCalleeObject = (callExpression.value.callee as MemberExpression)
+        .object as CallExpression;
+      if (currentCalleeObject.arguments.length > 0) {
+        callExpression.value.arguments = currentCalleeObject.arguments;
+      }
+      callExpression.value.callee = currentCalleeObject.callee;
       break;
+    }
     default:
       throw new Error(
         `Removal of .promise() not implemented for ${callExpression.parentPath.value.type}`
