9a1eadabfe2061b3c59c41cf487f75c81f2d2715
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-02-20 14:36:33 -0800

    Add v3 waiter API in requires and import equals

--- src/transforms/v2-to-v3/modules/addV3ClientImportEquals.ts
+++ src/transforms/v2-to-v3/modules/addV3ClientImportEquals.ts
@@ -1,5 +1,6 @@
 import { Collection, JSCodeshift } from "jscodeshift";
 
+import { getClientWaiterStates, getV3ClientWaiterApiName } from "../apis";
 import { getV3ClientTypesCount } from "../ts-type";
 import { addV3ClientDefaultImportEquals } from "./addV3ClientDefaultImportEquals";
 import { addV3ClientNamedImportEquals } from "./addV3ClientNamedImportEquals";
@@ -15,8 +16,14 @@ export const addV3ClientImportEquals = (
   const v3ClientTypesCount = getV3ClientTypesCount(j, source, options);
   const newExpressionCount = getNewExpressionCount(j, source, options);
   const clientTSTypeRefCount = getClientTSTypeRefCount(j, source, options);
+  const waiterStates = getClientWaiterStates(j, source, options);
 
-  if (v3ClientTypesCount > 0) {
+  if (
+    v3ClientTypesCount > 0 ||
+    newExpressionCount > 0 ||
+    clientTSTypeRefCount > 0 ||
+    waiterStates.length > 0
+  ) {
     addV3ClientDefaultImportEquals(j, source, options);
   }
 
@@ -25,6 +32,15 @@ export const addV3ClientImportEquals = (
       ...options,
       keyName: options.v3ClientName,
       valueName: options.v2ClientLocalName,
+    });
+  }
+
+  for (const waiterState of waiterStates) {
+    const v3WaiterApiName = getV3ClientWaiterApiName(waiterState);
+    addV3ClientNamedImportEquals(j, source, {
+      ...options,
+      keyName: v3WaiterApiName,
+      valueName: v3WaiterApiName,
     });
   }
 };
