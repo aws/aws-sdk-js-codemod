487c43d6631e704034346784399fd3ad99e92384
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-02-20 15:11:28 -0800

    Replace client.waitFor with waitUntil*

--- src/transforms/v2-to-v3/apis/replaceWaiterApi.ts
+++ src/transforms/v2-to-v3/apis/replaceWaiterApi.ts
@@ -1,7 +1,8 @@
 import { Collection, JSCodeshift } from "jscodeshift";
 
+import { getClientWaiterStates } from "./getClientWaiterStates";
 import { getV2ClientIdentifiers } from "./getV2ClientIdentifiers";
-import { getV2ClientIdThisExpressions } from "./getV2ClientIdThisExpressions";
+import { getV3ClientWaiterApiName } from "./getV3ClientWaiterApiName";
 
 export interface ReplaceWaiterApiOptions {
   v2ClientName: string;
@@ -16,55 +17,24 @@ export const replaceWaiterApi = (
   options: ReplaceWaiterApiOptions
 ): void => {
   const v2ClientIdentifiers = getV2ClientIdentifiers(j, source, options);
-  const v2ClientIdThisExpressions = getV2ClientIdThisExpressions(j, source, v2ClientIdentifiers);
 
-  for (const v2ClientId of [...v2ClientIdentifiers, ...v2ClientIdThisExpressions]) {
-    // Replace .waitFor() in client API calls.
-    // source
-    //   .find(j.CallExpression, {
-    //     callee: {
-    //       type: "MemberExpression",
-    //       object: {
-    //         type: "CallExpression",
-    //         callee: {
-    //           type: "MemberExpression",
-    //           object: v2ClientId,
-    //         },
-    //       },
-    //       property: { type: "Identifier", name: "promise" },
-    //     },
-    //   })
-    //   // Use replaceWith() instead of forEach() to replace the node.
-    //   .forEach((callExpression) => {
-    //     removePromiseForCallExpression(callExpression);
-    //   });
-    // Replace .waitFor() from client API request stored in a variable.
-    // source
-    //   .find(j.VariableDeclarator, {
-    //     id: { type: "Identifier" },
-    //     init: {
-    //       type: "CallExpression",
-    //       callee: {
-    //         type: "MemberExpression",
-    //         object: v2ClientId,
-    //       },
-    //     },
-    //   })
-    //   .forEach((variableDeclarator) => {
-    //     source
-    //       .find(j.CallExpression, {
-    //         callee: {
-    //           type: "MemberExpression",
-    //           object: {
-    //             type: "Identifier",
-    //             name: (variableDeclarator.value.id as Identifier).name,
-    //           },
-    //           property: { type: "Identifier", name: "promise" },
-    //         },
-    //       })
-    //       .forEach((callExpression) => {
-    //         removePromiseForCallExpression(callExpression);
-    //       });
-    //   });
+  for (const v2ClientId of v2ClientIdentifiers) {
+    const waiterStates = getClientWaiterStates(j, source, options);
+
+    for (const waiterState of waiterStates) {
+      const v3WaiterApiName = getV3ClientWaiterApiName(waiterState);
+      source
+        .find(j.CallExpression, {
+          type: "CallExpression",
+          callee: {
+            type: "MemberExpression",
+            object: v2ClientId,
+            property: { type: "Identifier", name: "waitFor" },
+          },
+        })
+        .replaceWith((callExpression) => {
+          return j.callExpression(j.identifier(v3WaiterApiName), callExpression.node.arguments);
+        });
+    }
   }
 };
