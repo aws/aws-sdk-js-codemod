04b724cb7db7bbc58d832f341053706b4458aa20
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-02-21 12:56:17 -0800

    Add constant OBJECT_PROPERTY_TYPE_LIST

--- src/transforms/v2-to-v3/config/constants.ts
+++ src/transforms/v2-to-v3/config/constants.ts
@@ -2,3 +2,5 @@ export const PACKAGE_NAME = "aws-sdk";
 
 export const V2_CLIENT_INPUT_SUFFIX_LIST = ["Input", "Request"];
 export const V2_CLIENT_OUTPUT_SUFFIX_LIST = ["Output", "Response"];
+
+export const OBJECT_PROPERTY_TYPE_LIST = ["Property", "ObjectProperty"];
