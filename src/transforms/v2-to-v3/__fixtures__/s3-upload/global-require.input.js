f1cfd98fccc55aad4ad0b96ebf9732d2d333d35f
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-02-22 14:43:26 -0800

    Add test s3-upload/global-require

---
+++ src/transforms/v2-to-v3/__fixtures__/s3-upload/global-require.input.js
@@ -0,0 +1,9 @@
+const AWS = require("aws-sdk");
+
+const client = new AWS.S3({ region: "REGION" });
+await client.upload({
+  Body: "BODY",
+  Bucket: "Bucket",
+  ContentType: "ContentType",
+  Key: "Key",
+}).promise();
