4840ab7fe1905409cf9cb138f13f2a4e47b448b1
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-02-21 16:28:47 -0800

    Add tests for waiter configuration

--- src/transforms/v2-to-v3/__fixtures__/waiters/global-require.input.js
+++ src/transforms/v2-to-v3/__fixtures__/waiters/global-require.input.js
@@ -6,3 +6,12 @@ const client = new AWS.S3({ region: "REGION" });
 await client.waitFor("bucketNotExists", { Bucket }).promise();
 await client.createBucket({ Bucket }).promise();
 await client.waitFor("bucketExists", { Bucket }).promise();
+
+// Waiter configuration: delay
+await client.waitFor("bucketExists", { Bucket, $waiter: { delay: 2 } }).promise();
+
+// Waiter configuration: maxAttempts
+await client.waitFor("bucketExists", { Bucket, $waiter: { maxAttempts: 5 } }).promise();
+
+// Waiter configuration: delay+maxAttempts
+await client.waitFor("bucketExists", { Bucket, $waiter: { delay: 2, maxAttempts: 5 } }).promise();
--- src/transforms/v2-to-v3/__fixtures__/waiters/service-require.output.js
+++ src/transforms/v2-to-v3/__fixtures__/waiters/service-require.output.js
@@ -16,3 +16,29 @@ await waitUntilBucketExists({
   client,
   maxWaitTime: 200
 }, { Bucket });
+
+// Waiter configuration: delay
+await waitUntilBucketExists({
+  client,
+  minDelay: 2,
+  maxWaitTime: 40
+}, {
+  Bucket
+});
+
+// Waiter configuration: maxAttempts
+await waitUntilBucketExists({
+  client,
+  maxWaitTime: 100
+}, {
+  Bucket
+});
+
+// Waiter configuration: delay+maxAttempts
+await waitUntilBucketExists({
+  client,
+  minDelay: 2,
+  maxWaitTime: 20
+}, {
+  Bucket
+});
