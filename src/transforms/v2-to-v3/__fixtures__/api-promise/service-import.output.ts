77e0b9cb6d7e3aa151b8ed72014199955224815e
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-21 18:57:47 -0800

    Update test api-promise/service-import

--- src/transforms/v2-to-v3/__fixtures__/api-promise/service-import.output.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-promise/service-import.output.ts
@@ -1,5 +1,9 @@
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
 
+export const listTablesObjectProperty = async (client: DynamoDB) => ({
+  promise: client.listTables(),
+});
+
 export const listTables = async (client: DynamoDB) => client.listTables();
 export const listTagsOfResource = async (client: DynamoDB) =>
   client.listTagsOfResource({ ResourceArn: "STRING_VALUE" });
