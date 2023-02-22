ec4577cb8cfb75e9460a3b91516693edd431468c
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-02-21 19:01:27 -0800

    Support removal of .promise() with parentPath ObjectProperty (#392)

--- src/transforms/v2-to-v3/__fixtures__/api-promise/global-import.input.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-promise/global-import.input.ts
@@ -1,5 +1,9 @@
 import AWS from "aws-sdk";
 
+export const listTablesObjectProperty = async (client: AWS.DynamoDB) => ({
+  promise: client.listTables().promise(),
+});
+
 export const listTables = async (client: AWS.DynamoDB) => client.listTables().promise();
 export const listTagsOfResource = async (client: AWS.DynamoDB) =>
   client.listTagsOfResource({ ResourceArn: "STRING_VALUE" }).promise();
--- src/transforms/v2-to-v3/__fixtures__/api-promise/service-import.input.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-promise/service-import.input.ts
@@ -1,5 +1,9 @@
 import DynamoDB from "aws-sdk/clients/dynamodb";
 
+export const listTablesObjectProperty = async (client: DynamoDB) => ({
+  promise: client.listTables().promise(),
+});
+
 export const listTables = async (client: DynamoDB) => client.listTables().promise();
 export const listTagsOfResource = async (client: DynamoDB) =>
   client.listTagsOfResource({ ResourceArn: "STRING_VALUE" }).promise();
