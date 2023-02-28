f0381b5eef0d9f3662d4b5d45388ea564233e1ac
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-02-28 14:14:10 -0800

    Support returning declaration per client in getV3PackageRequiresCode (#422)

--- scripts/generateNewClientTests/getServiceRequireDeepOutput.ts
+++ scripts/generateNewClientTests/getServiceRequireDeepOutput.ts
@@ -1,17 +1,11 @@
-import { CLIENT_NAMES_MAP, CLIENT_PACKAGE_NAMES_MAP } from "../../src/transforms/v2-to-v3/config";
 import { CLIENTS_TO_TEST } from "./config";
 import { getV3ClientsNewExpressionCode } from "./getV3ClientsNewExpressionCode";
+import { getV3PackageRequiresCode } from "./getV3PackageRequiresCode";
 
 export const getServiceRequireDeepOutput = (codegenComment: string) => {
   let content = `${codegenComment}\n`;
 
-  for (const v2ClientName of CLIENTS_TO_TEST) {
-    const v3ClientName = CLIENT_NAMES_MAP[v2ClientName];
-    const v3ClientPackageName = `@aws-sdk/${CLIENT_PACKAGE_NAMES_MAP[v2ClientName]}`;
-    const v3RequireKeyValuePair =
-      v3ClientName === v2ClientName ? v3ClientName : `${v3ClientName}: ${v2ClientName}`;
-    content += `const {\n  ${v3RequireKeyValuePair}\n} = require("${v3ClientPackageName}");\n`;
-  }
+  content += getV3PackageRequiresCode(CLIENTS_TO_TEST, { declarationPerClient: true });
   content += `\n`;
   content += getV3ClientsNewExpressionCode(CLIENTS_TO_TEST);
 
