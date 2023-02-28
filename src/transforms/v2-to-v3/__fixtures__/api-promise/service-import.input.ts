6b02d9a9b70a4d833f7327c17946ea25b0a17ba0
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-28 08:00:03 -0800

    Add test api-promise/function-param

--- src/transforms/v2-to-v3/__fixtures__/api-promise/service-import.input.ts
+++
@@ -1,5 +0,0 @@
-import DynamoDB from "aws-sdk/clients/dynamodb";
-
-export const listTables = async (client: DynamoDB) => client.listTables().promise();
-export const listTagsOfResource = async (client: DynamoDB) =>
-  client.listTagsOfResource({ ResourceArn: "STRING_VALUE" }).promise();
