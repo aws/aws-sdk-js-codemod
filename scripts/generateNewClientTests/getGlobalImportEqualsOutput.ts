962e17938981f31d7aef6be093f2298d7595e031
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-02-28 15:59:53 -0800

    Rename getV3ImportEqualsCode to getV3PackageImportEqualsCode (#425)

--- scripts/generateNewClientTests/getGlobalImportEqualsOutput.ts
+++ scripts/generateNewClientTests/getGlobalImportEqualsOutput.ts
@@ -1,12 +1,12 @@
 import { CLIENTS_TO_TEST } from "./config";
 import { getClientNamesSortedByPackageName } from "./getClientNamesSortedByPackageName";
 import { getV3ClientsNewExpressionCode } from "./getV3ClientsNewExpressionCode";
-import { getV3ImportEqualsCode } from "./getV3ImportEqualsCode";
+import { getV3PackageImportEqualsCode } from "./getV3PackageImportEqualsCode";
 
 export const getGlobalImportEqualsOutput = (codegenComment: string) => {
   let content = `${codegenComment};\n`;
 
-  content += getV3ImportEqualsCode(getClientNamesSortedByPackageName(CLIENTS_TO_TEST));
+  content += getV3PackageImportEqualsCode(getClientNamesSortedByPackageName(CLIENTS_TO_TEST));
   content += getV3ClientsNewExpressionCode(CLIENTS_TO_TEST);
 
   return content;
