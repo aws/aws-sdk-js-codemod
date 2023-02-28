93f1da31b57512325fd0e78cabd6e649cb93143f
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-28 13:12:49 -0800

    Add utility getClientDeepImportPath

--- scripts/generateNewClientTests/getServiceImportDeepInput.ts
+++ scripts/generateNewClientTests/getServiceImportDeepInput.ts
@@ -1,11 +1,12 @@
 import { CLIENTS_TO_TEST } from "./config";
+import { getClientDeepImportPath } from "./getClientDeepImportPath";
 import { getV2ClientsNewExpressionCode } from "./getV2ClientsNewExpressionCode";
 
 export const getServiceImportDeepInput = (codegenComment: string) => {
   let content = `${codegenComment}\n`;
 
   for (const clientName of CLIENTS_TO_TEST) {
-    content += `import ${clientName} from "aws-sdk/clients/${clientName.toLowerCase()}";\n`;
+    content += `import ${clientName} from "${getClientDeepImportPath(clientName)}";\n`;
   }
   content += `\n`;
   content += getV2ClientsNewExpressionCode(CLIENTS_TO_TEST);
