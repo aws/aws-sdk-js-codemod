f23e7d1879d9cfde66246882f136440d0dd8fa6c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-28 11:28:44 -0800

    Update import order of clients

--- scripts/generateNewClientTests/getServiceImportDeepStarWithNameOutput.ts
+++ scripts/generateNewClientTests/getServiceImportDeepStarWithNameOutput.ts
@@ -1,12 +1,11 @@
 import { CLIENT_NAMES_MAP, CLIENT_PACKAGE_NAMES_MAP } from "../../src/transforms/v2-to-v3/config";
 import { CLIENTS_TO_TEST, LOCAL_NAME_SUFFIX } from "./config";
-import { getClientNamesSortedByPackageName } from "./getClientNamesSortedByPackageName";
 import { getV3ClientsNewExpressionCode } from "./getV3ClientsNewExpressionCode";
 
 export const getServiceImportDeepStarWithNameOutput = (codegenComment: string) => {
   let serviceImportOutputContent = `${codegenComment}\n`;
 
-  for (const v2ClientName of getClientNamesSortedByPackageName(CLIENTS_TO_TEST)) {
+  for (const v2ClientName of CLIENTS_TO_TEST) {
     const v3ClientName = CLIENT_NAMES_MAP[v2ClientName];
     const v3ClientPackageName = `@aws-sdk/${CLIENT_PACKAGE_NAMES_MAP[v2ClientName]}`;
     const v3ClientLocalName = `${v2ClientName}${LOCAL_NAME_SUFFIX}`;
