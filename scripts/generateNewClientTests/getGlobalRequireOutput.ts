eb7546f68abdc387dd9c953d97f24eada071c9b4
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-02-28 14:02:10 -0800

    Add utility getV3PackageRequiresCode (#420)

--- scripts/generateNewClientTests/getGlobalRequireOutput.ts
+++ scripts/generateNewClientTests/getGlobalRequireOutput.ts
@@ -1,25 +1,12 @@
-import { CLIENT_NAMES_MAP, CLIENT_PACKAGE_NAMES_MAP } from "../../src/transforms/v2-to-v3/config";
 import { CLIENTS_TO_TEST } from "./config";
 import { getClientNamesSortedByPackageName } from "./getClientNamesSortedByPackageName";
 import { getV3ClientsNewExpressionCode } from "./getV3ClientsNewExpressionCode";
+import { getV3PackageRequiresCode } from "./getV3PackageRequiresCode";
 
 export const getGlobalRequireOutput = (codegenComment: string) => {
   let content = `${codegenComment}\n`;
 
-  const sortedClientNames = getClientNamesSortedByPackageName(CLIENTS_TO_TEST);
-  content += `const `;
-  for (const v2ClientName of sortedClientNames) {
-    const v3ClientName = CLIENT_NAMES_MAP[v2ClientName];
-    const v3ClientPackageName = `@aws-sdk/${CLIENT_PACKAGE_NAMES_MAP[v2ClientName]}`;
-    const v3RequireKeyValuePair =
-      v3ClientName === v2ClientName ? v3ClientName : `${v3ClientName}: ${v2ClientName}`;
-    content +=
-      `{\n` +
-      `        ${v3RequireKeyValuePair}\n` +
-      `      } = require("${v3ClientPackageName}"),\n` +
-      `      `;
-  }
-  content = content.replace(/,\n {6}$/, ";\n\n");
+  content += getV3PackageRequiresCode(getClientNamesSortedByPackageName(CLIENTS_TO_TEST));
   content += getV3ClientsNewExpressionCode(CLIENTS_TO_TEST);
 
   return content;
---
+++ scripts/generateNewClientTests/getV3PackageRequiresCode.ts
@@ -0,0 +1,38 @@
+import {
+  CLIENT_NAMES,
+  CLIENT_NAMES_MAP,
+  CLIENT_PACKAGE_NAMES_MAP,
+} from "../../src/transforms/v2-to-v3/config";
+import { getClientNameWithLocalSuffix } from "./getClientNameWithLocalSuffix";
+
+export interface V3PackageRequiresCodeOptions {
+  useLocalSuffix?: boolean;
+}
+
+export const getV3PackageRequiresCode = (
+  clientsToTest: typeof CLIENT_NAMES,
+  options?: V3PackageRequiresCodeOptions
+) => {
+  let content = ``;
+  const { useLocalSuffix = false } = options || {};
+
+  content += `const `;
+  for (const v2ClientName of clientsToTest) {
+    const v3ClientName = CLIENT_NAMES_MAP[v2ClientName];
+    const v3ClientPackageName = `@aws-sdk/${CLIENT_PACKAGE_NAMES_MAP[v2ClientName]}`;
+    const v2ClientLocalName = useLocalSuffix
+      ? getClientNameWithLocalSuffix(v2ClientName)
+      : v2ClientName;
+
+    const v3RequireKeyValuePair =
+      v3ClientName === v2ClientLocalName ? v3ClientName : `${v3ClientName}: ${v2ClientLocalName}`;
+    content +=
+      `{\n` +
+      `        ${v3RequireKeyValuePair}\n` +
+      `      } = require("${v3ClientPackageName}"),\n` +
+      `      `;
+  }
+  content = content.replace(/,\n {6}$/, ";\n\n");
+
+  return content;
+};
