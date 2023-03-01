73ac0d5b0f8ab5e51b66b8fad02d406068b777a6
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-02-28 16:40:39 -0800

    Add test new-client/service-require-deep-with-name (#428)

---
+++ scripts/generateNewClientTests/getServiceRequireDeepWithNameInput.ts
@@ -0,0 +1,17 @@
+import { CLIENTS_TO_TEST } from "./config";
+import { getClientDeepImportPath } from "./getClientDeepImportPath";
+import { getClientNameWithLocalSuffix } from "./getClientNameWithLocalSuffix";
+import { getV2ClientsNewExpressionCode } from "./getV2ClientsNewExpressionCode";
+
+export const getServiceRequireDeepWithNameInput = (codegenComment: string) => {
+  let content = `${codegenComment}\n`;
+
+  for (const clientName of CLIENTS_TO_TEST) {
+    const importedName = getClientNameWithLocalSuffix(clientName);
+    content += `const ${importedName} = require("${getClientDeepImportPath(clientName)}");\n`;
+  }
+  content += `\n`;
+  content += getV2ClientsNewExpressionCode(CLIENTS_TO_TEST.map(getClientNameWithLocalSuffix));
+
+  return content;
+};
