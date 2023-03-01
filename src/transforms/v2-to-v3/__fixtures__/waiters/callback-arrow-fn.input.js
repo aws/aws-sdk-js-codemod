45f190df5e81bcd9ab5f64b96e86ff11ff565961
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-03-01 11:19:56 -0800

    Add test waiters/callback-arrow-fn (#437)

---
+++ src/transforms/v2-to-v3/__fixtures__/waiters/callback-arrow-fn.input.js
@@ -0,0 +1,9 @@
+import AWS from "aws-sdk";
+
+const Bucket = "BUCKET_NAME";
+const client = new AWS.S3({ region: "REGION" });
+
+client.waitFor("bucketExists", { Bucket }, (err, data) => {
+  if (err) console.log(err, err.stack); // an error occurred
+  else console.log(data); // successful response
+});
