d8fdc70228d902f2459a464b519e3a90431bf76d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-03-01 10:10:58 -0800

    Add await promise example in comment

--- src/transforms/v2-to-v3/__fixtures__/waiters/callback.output.js
+++ src/transforms/v2-to-v3/__fixtures__/waiters/callback.output.js
@@ -4,7 +4,7 @@ const Bucket = "BUCKET_NAME";
 const client = new AWS.S3({ region: "REGION" });
 
 // Waiters with callbacks are not supported in AWS SDK for JavaScript (v3).
-// Please convert to async-await, and re-run aws-sdk-js-codemod.
+// Please convert to `await client.waitFor(state, params).promise()`, and re-run aws-sdk-js-codemod.
 client.waitFor("bucketExists", { Bucket }, function (err, data) {
   if (err) console.log(err, err.stack); // an error occurred
   else console.log(data); // successful response
