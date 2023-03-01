860b4f1f9f44f607f86fbf981e0752803ba0f832
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-02-28 16:44:17 -0800

    Import *ImportDeep from *ImportDeepStar (#429)

--- scripts/generateNewClientTests/getServiceImportDeepStarWithNameOutput.ts
+++ scripts/generateNewClientTests/getServiceImportDeepStarWithNameOutput.ts
@@ -1,14 +1,3 @@
-import { CLIENTS_TO_TEST } from "./config";
-import { getClientNameWithLocalSuffix } from "./getClientNameWithLocalSuffix";
-import { getV3ClientsNewExpressionCode } from "./getV3ClientsNewExpressionCode";
-import { getV3PackageImportsCode } from "./getV3PackageImportsCode";
+import { getServiceImportDeepWithNameOutput } from "./getServiceImportDeepWithNameOutput";
 
-export const getServiceImportDeepStarWithNameOutput = (codegenComment: string) => {
-  let content = `${codegenComment}\n`;
-
-  content += getV3PackageImportsCode(CLIENTS_TO_TEST, { useLocalSuffix: true });
-  content += `\n`;
-  content += getV3ClientsNewExpressionCode(CLIENTS_TO_TEST.map(getClientNameWithLocalSuffix));
-
-  return content;
-};
+export const getServiceImportDeepStarWithNameOutput = getServiceImportDeepWithNameOutput;
--- scripts/generateNewClientTests/getServiceImportDeepWithNameOutput.ts
+++ scripts/generateNewClientTests/getServiceImportDeepWithNameOutput.ts
@@ -1,3 +1,14 @@
-import { getServiceImportDeepStarWithNameOutput } from "./getServiceImportDeepStarWithNameOutput";
+import { CLIENTS_TO_TEST } from "./config";
+import { getClientNameWithLocalSuffix } from "./getClientNameWithLocalSuffix";
+import { getV3ClientsNewExpressionCode } from "./getV3ClientsNewExpressionCode";
+import { getV3PackageImportsCode } from "./getV3PackageImportsCode";
 
-export const getServiceImportDeepWithNameOutput = getServiceImportDeepStarWithNameOutput;
+export const getServiceImportDeepWithNameOutput = (codegenComment: string) => {
+  let content = `${codegenComment}\n`;
+
+  content += getV3PackageImportsCode(CLIENTS_TO_TEST, { useLocalSuffix: true });
+  content += `\n`;
+  content += getV3ClientsNewExpressionCode(CLIENTS_TO_TEST.map(getClientNameWithLocalSuffix));
+
+  return content;
+};
