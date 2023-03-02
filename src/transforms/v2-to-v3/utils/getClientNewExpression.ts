f386b47888d1d37c73c853159c2ccad3c05de26c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-03-02 09:02:16 -0800

    Remove hack for DocumentClient from getV2ClientNewExpression

--- src/transforms/v2-to-v3/utils/getClientNewExpression.ts
+++ src/transforms/v2-to-v3/utils/getClientNewExpression.ts
@@ -1,8 +1,6 @@
 import { NewExpression } from "jscodeshift";
 
-import { DOCUMENT_CLIENT, DYNAMODB_DOCUMENT_CLIENT } from "../config";
-
-export interface GetClientNewExpressionOptions {
+export interface ClientNewExpressionOptions {
   v2ClientLocalName?: string;
   v2ClientName?: string;
   v2GlobalName?: string;
@@ -12,7 +10,7 @@ export const getClientNewExpression = ({
   v2ClientLocalName,
   v2ClientName,
   v2GlobalName,
-}: GetClientNewExpressionOptions): NewExpression => {
+}: ClientNewExpressionOptions): NewExpression => {
   if (!v2GlobalName && !v2ClientLocalName) {
     throw new Error(
       `One of the following options must be provided: v2ClientLocalName, v2GlobalName`
@@ -26,40 +24,11 @@ export const getClientNewExpression = ({
   }
 
   if (v2GlobalName) {
-    // Hack for AWS.DynamoDB.DocumentClient
-    if (v2ClientName === DYNAMODB_DOCUMENT_CLIENT) {
-      const [DynamoDB, DocumentClient] = v2ClientName.split(".");
-      return {
-        type: "NewExpression",
-        callee: {
-          type: "MemberExpression",
-          object: {
-            type: "MemberExpression",
-            object: { type: "Identifier", name: v2GlobalName },
-            property: { type: "Identifier", name: DynamoDB },
-          },
-          property: { type: "Identifier", name: DocumentClient },
-        },
-      } as NewExpression;
-    }
-
     return {
       type: "NewExpression",
       callee: {
         object: { type: "Identifier", name: v2GlobalName },
         property: { type: "Identifier", ...(v2ClientName && { name: v2ClientName }) },
-      },
-    } as NewExpression;
-  }
-
-  // Hack for DynamoDBLocalName.DocumentClient
-  if (v2ClientLocalName && v2ClientLocalName.endsWith(`.${DOCUMENT_CLIENT}`)) {
-    const [DynamoDBLocalName, DocumentClient] = v2ClientLocalName.split(".");
-    return {
-      type: "NewExpression",
-      callee: {
-        object: { type: "Identifier", name: DynamoDBLocalName },
-        property: { type: "Identifier", name: DocumentClient },
       },
     } as NewExpression;
   }
