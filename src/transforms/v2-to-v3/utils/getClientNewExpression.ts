a1d0a4c015b8a9dc77ee5494b0007691e573acad
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-03-02 08:27:26 -0800

    Update getV2ClientNewExpression to support DocumentClient

--- src/transforms/v2-to-v3/utils/getClientNewExpression.ts
+++ src/transforms/v2-to-v3/utils/getClientNewExpression.ts
@@ -1,5 +1,7 @@
 import { NewExpression } from "jscodeshift";
 
+import { DOCUMENT_CLIENT, DYNAMODB_DOCUMENT_CLIENT } from "../config";
+
 export interface GetClientNewExpressionOptions {
   v2ClientLocalName?: string;
   v2ClientName?: string;
@@ -24,11 +26,40 @@ export const getClientNewExpression = ({
   }
 
   if (v2GlobalName) {
+    // Hack for AWS.DynamoDB.DocumentClient
+    if (v2ClientName === DYNAMODB_DOCUMENT_CLIENT) {
+      const [DynamoDB, DocumentClient] = v2ClientName.split(".");
+      return {
+        type: "NewExpression",
+        callee: {
+          type: "MemberExpression",
+          object: {
+            type: "MemberExpression",
+            object: { type: "Identifier", name: v2GlobalName },
+            property: { type: "Identifier", name: DynamoDB },
+          },
+          property: { type: "Identifier", name: DocumentClient },
+        },
+      } as NewExpression;
+    }
+
     return {
       type: "NewExpression",
       callee: {
         object: { type: "Identifier", name: v2GlobalName },
         property: { type: "Identifier", ...(v2ClientName && { name: v2ClientName }) },
+      },
+    } as NewExpression;
+  }
+
+  // Hack for DynamoDBLocalName.DocumentClient
+  if (v2ClientLocalName && v2ClientLocalName.endsWith(`.${DOCUMENT_CLIENT}`)) {
+    const [DynamoDBLocalName, DocumentClient] = v2ClientLocalName.split(".");
+    return {
+      type: "NewExpression",
+      callee: {
+        object: { type: "Identifier", name: DynamoDBLocalName },
+        property: { type: "Identifier", name: DocumentClient },
       },
     } as NewExpression;
   }
