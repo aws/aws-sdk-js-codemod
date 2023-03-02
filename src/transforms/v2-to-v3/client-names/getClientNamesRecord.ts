59b84c9cb6da87687578806f8098553618cc9a7e
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-03-02 14:22:19 -0800

    Rename getV2ClientNamesRecordFromRequire to getClientNamesRecordFromRequire

--- src/transforms/v2-to-v3/client-names/getClientNamesRecord.ts
+++ src/transforms/v2-to-v3/client-names/getClientNamesRecord.ts
@@ -1,13 +1,13 @@
 import { Collection, JSCodeshift } from "jscodeshift";
 
 import { hasRequire } from "../modules";
+import { getClientNamesRecordFromRequire } from "./getClientNamesRecordFromRequire";
 import { getV2ClientNamesRecordFromImport } from "./getV2ClientNamesRecordFromImport";
-import { getV2ClientNamesRecordFromRequire } from "./getV2ClientNamesRecordFromRequire";
 import { getV2ClientNamesWithServiceModule } from "./getV2ClientNamesWithServiceModule";
 
 export const getClientNamesRecord = (j: JSCodeshift, source: Collection<unknown>) => {
   const v2ClientNamesWithServiceModule = getV2ClientNamesWithServiceModule(source.toSource());
   return hasRequire(j, source)
-    ? getV2ClientNamesRecordFromRequire(j, source, v2ClientNamesWithServiceModule)
+    ? getClientNamesRecordFromRequire(j, source, v2ClientNamesWithServiceModule)
     : getV2ClientNamesRecordFromImport(j, source, v2ClientNamesWithServiceModule);
 };
