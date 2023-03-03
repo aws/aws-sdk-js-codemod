e35f02f13d6b51ccfb21d0e25f0a7ad667881917
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-03-02 20:21:51 -0800

    Add test ddb-doc-client/global-import-equals

---
+++ src/transforms/v2-to-v3/__fixtures__/ddb-doc-client/global-import-equals.input.ts
@@ -0,0 +1,4 @@
+import AWS = require("aws-sdk");
+
+const documentClient = new AWS.DynamoDB.DocumentClient({ region: "us-west-2" });
+const response = await documentClient.scan({ TableName: "TABLE_NAME" }).promise();
