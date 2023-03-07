e832e0661edcb87781766cf50b812701dbe96b17
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-03-06 16:32:53 -0800

    Remove utility getClientTSTypeRef

--- src/transforms/v2-to-v3/utils/index.ts
+++ src/transforms/v2-to-v3/utils/index.ts
@@ -1,7 +1,5 @@
 export * from "./getClientDeepImportPath";
 export * from "./getClientNewExpression";
-export * from "./getClientTSTypeRef";
-export * from "./getClientTypeName";
 export * from "./getDefaultLocalName";
 export * from "./getDocClientNewExpression";
 export * from "./isTypeScriptFile";
