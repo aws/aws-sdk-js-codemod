61ff436a58a1d825edc65e027bdfebe06a9c7981
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-03-06 16:14:36 -0800

    Export getClientTypeName

--- src/transforms/v2-to-v3/utils/index.ts
+++ src/transforms/v2-to-v3/utils/index.ts
@@ -1,6 +1,7 @@
 export * from "./getClientDeepImportPath";
 export * from "./getClientNewExpression";
 export * from "./getClientTSTypeRef";
+export * from "./getClientTypeName";
 export * from "./getDefaultLocalName";
 export * from "./getDocClientNewExpression";
 export * from "./isTypeScriptFile";
