463382785f4476d552bde80d3652d9f97dfed609
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-03-15 18:01:45 -0700

    Fix test ddb-doc-client/service-require-property-deep-with-name.output

--- src/transforms/v2-to-v3/__fixtures__/ddb-doc-client/service-require-property-deep-with-name.output.js
+++ src/transforms/v2-to-v3/__fixtures__/ddb-doc-client/service-require-property-deep-with-name.output.js
@@ -2,8 +2,8 @@ const {
         DynamoDBDocument
       } = require("@aws-sdk/lib-dynamodb"),
       {
-        DynamoDB: DynamoDBClient
+        DynamoDB
       } = require("@aws-sdk/client-dynamodb");
 
-const documentClient = DynamoDBDocument.from(new DynamoDBClient({ region: "us-west-2" }));
+const documentClient = DynamoDBDocument.from(new DynamoDB({ region: "us-west-2" }));
 const response = await documentClient.scan({ TableName: "TABLE_NAME" });
