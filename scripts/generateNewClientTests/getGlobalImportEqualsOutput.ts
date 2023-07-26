41694510cfab85bed4bda1a2595262edcd14b5a3
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-07-26 13:09:57 -0700

    chore(script): remove 'use strict' from auto generated files

--- scripts/generateNewClientTests/getGlobalImportEqualsOutput.ts
+++ scripts/generateNewClientTests/getGlobalImportEqualsOutput.ts
@@ -4,7 +4,7 @@ import { getV3ClientsNewExpressionCode } from "./getV3ClientsNewExpressionCode";
 import { getV3PackageImportEqualsCode } from "./getV3PackageImportEqualsCode";
 
 export const getGlobalImportEqualsOutput = (codegenComment: string) => {
-  let content = `${codegenComment};\n`;
+  let content = `${codegenComment}\n`;
 
   content += getV3PackageImportEqualsCode(getClientNamesSortedByPackageName(CLIENTS_TO_TEST));
   content += getV3ClientsNewExpressionCode(CLIENTS_TO_TEST);
--- scripts/generateNewClientTests/getServiceImportEqualsWithNameOutput.ts
+++ scripts/generateNewClientTests/getServiceImportEqualsWithNameOutput.ts
@@ -4,7 +4,7 @@ import { getV3ClientsNewExpressionCode } from "./getV3ClientsNewExpressionCode";
 import { getV3PackageImportEqualsCode } from "./getV3PackageImportEqualsCode";
 
 export const getServiceImportEqualsWithNameOutput = (codegenComment: string) => {
-  let content = `${codegenComment};\n`;
+  let content = `${codegenComment}\n`;
 
   content += getV3PackageImportEqualsCode(CLIENTS_TO_TEST, { useLocalSuffix: true });
   content += getV3ClientsNewExpressionCode(CLIENTS_TO_TEST.map(getClientNameWithLocalSuffix));
