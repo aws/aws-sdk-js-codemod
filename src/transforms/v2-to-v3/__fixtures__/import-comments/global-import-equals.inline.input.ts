dcb56ae324d5fbd0231097bbeb65de4ed3491b6d
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-07-26 14:58:05 -0700

    test: import-comments/global-import-equals.inline

---
+++ src/transforms/v2-to-v3/__fixtures__/import-comments/global-import-equals.inline.input.ts
@@ -0,0 +1,4 @@
+// Example comment which should not be removed.
+import AWS = require("aws-sdk");
+
+const client = new AWS.DynamoDB();
---
+++ src/transforms/v2-to-v3/__fixtures__/import-comments/global-import-equals.inline.output.ts
@@ -0,0 +1,8 @@
+// Example comment which should not be removed.
+import AWS_DynamoDB = require("@aws-sdk/client-dynamodb");
+
+const {
+  DynamoDB
+} = AWS_DynamoDB;
+
+const client = new DynamoDB();
