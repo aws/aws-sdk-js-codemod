a9fb1880fb312cd6ad626b325e9fe00864c57b5b
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-07-26 14:43:45 -0700

    test: add import-comments/service-require

---
+++ src/transforms/v2-to-v3/__fixtures__/import-comments/service-require.block.input.js
@@ -0,0 +1,6 @@
+/*
+ * Example comment which should not be removed.
+ */
+const { DynamoDB } = require("aws-sdk");
+
+const client = new DynamoDB();
---
+++ src/transforms/v2-to-v3/__fixtures__/import-comments/service-require.block.output.js
@@ -0,0 +1,8 @@
+/*
+ * Example comment which should not be removed.
+ */
+const {
+ DynamoDB
+} = require("@aws-sdk/client-dynamodb");
+
+const client = new DynamoDB();
