e0afd8881491c8678c0cb16cc15ba7620e343d4b
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-07-26 13:27:20 -0700

    test: file-header-comment

---
+++ src/transforms/v2-to-v3/__fixtures__/misc/file-header-comment.input.js
@@ -0,0 +1,6 @@
+/*
+ * Example comment which should not be replaced.
+ */
+import AWS from "aws-sdk";
+
+const client = new AWS.DynamoDB();
---
+++ src/transforms/v2-to-v3/__fixtures__/misc/file-header-comment.output.js
@@ -0,0 +1,6 @@
+/*
+ * Example comment which should not be replaced.
+ */
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+
+const client = new DynamoDB();
