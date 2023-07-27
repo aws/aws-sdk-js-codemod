fb5a6e4ee9e9817b102c9f0c9a3561675f944400
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-07-27 15:05:28 -0700

    test: sort imports in s3-get-signed-url outputs

--- src/transforms/v2-to-v3/__fixtures__/s3-get-signed-url/putObject.output.js
+++ src/transforms/v2-to-v3/__fixtures__/s3-get-signed-url/putObject.output.js
@@ -1,5 +1,5 @@
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { S3, PutObjectCommand } from "@aws-sdk/client-s3";
+import { PutObjectCommand, S3 } from "@aws-sdk/client-s3";
 
 const s3 = new S3();
 const params = { Bucket: "bucket", Key: "key" };
