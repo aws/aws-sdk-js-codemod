e8cbde61b0dafa0e93eefc4585b08364455e8b52
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-07-27 14:28:06 -0700

    test: move Promise calls to separate file in s3-get-signed-url

--- src/transforms/v2-to-v3/__fixtures__/s3-get-signed-url/getObject.input.js
+++ src/transforms/v2-to-v3/__fixtures__/s3-get-signed-url/getObject.input.js
@@ -4,7 +4,6 @@ const s3 = new AWS.S3();
 const params = { Bucket: "bucket", Key: "key" };
 
 url = s3.getSignedUrl("getObject", params);
-url = await s3.getSignedUrlPromise("getObject", params);
 
 url = s3.getSignedUrl("getObject", { Bucket: "bucket", Key: "key" });
 url = s3.getSignedUrl("getObject", { Bucket: "bucket", Key: "key", Expires: 60 });
--- src/transforms/v2-to-v3/__fixtures__/s3-get-signed-url/putObject.input.js
+++ src/transforms/v2-to-v3/__fixtures__/s3-get-signed-url/putObject.input.js
@@ -4,7 +4,6 @@ const s3 = new AWS.S3();
 const params = { Bucket: "bucket", Key: "key" };
 
 url = s3.getSignedUrl("putObject", params);
-url = await s3.getSignedUrlPromise("putObject", params);
 
 url = s3.getSignedUrl("putObject", { Bucket: "bucket", Key: "key" });
 url = s3.getSignedUrl("putObject", { Bucket: "bucket", Key: "key", Expires: 60 });
