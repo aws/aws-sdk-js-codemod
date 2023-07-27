8ffcf60f24b0667c8e92129a58d2ccdc53455044
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-07-27 14:54:21 -0700

    chore: add utility getS3SignedUrlApiNames

---
+++ src/transforms/v2-to-v3/apis/getS3SignedUrlApiNames.ts
@@ -0,0 +1,38 @@
+import { Collection, JSCodeshift, Literal } from "jscodeshift";
+
+import { getClientIdentifiers } from "./getClientIdentifiers";
+
+export interface GetS3SignedUrlApiNameOptions {
+  v2ClientName: string;
+  v2ClientLocalName: string;
+  v2GlobalName?: string;
+}
+
+export const getS3SignedUrlApiNames = (
+  j: JSCodeshift,
+  source: Collection<unknown>,
+  options: GetS3SignedUrlApiNameOptions
+): string[] => {
+  if (options.v2ClientName !== "S3") return [];
+
+  const apiNames: Set<string> = new Set();
+  const clientIdentifiers = getClientIdentifiers(j, source, options);
+
+  for (const clientId of clientIdentifiers) {
+    for (const apiName of ["getSignedUrl", "getSignedUrlPromise"]) {
+      source
+        .find(j.CallExpression, {
+          callee: {
+            type: "MemberExpression",
+            object: clientId,
+            property: { type: "Identifier", name: apiName },
+          },
+        })
+        .forEach((callExpression) => {
+          apiNames.add((callExpression.value.arguments[0] as Literal).value as string);
+        });
+    }
+  }
+
+  return [...apiNames];
+};
--- src/transforms/v2-to-v3/apis/index.ts
+++ src/transforms/v2-to-v3/apis/index.ts
@@ -1,6 +1,7 @@
 export * from "./addNotSupportedComments";
 export * from "./getClientWaiterStates";
 export * from "./getV3ClientWaiterApiName";
+export * from "./getS3SignedUrlApiNames";
 export * from "./isS3GetSignedUrlApiUsed";
 export * from "./isS3UploadApiUsed";
 export * from "./removePromiseCalls";
