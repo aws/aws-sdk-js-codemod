b0993c3536e149a70263816efb6c056f6ed3a103
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-07-28 12:25:19 -0700

    chore: support transformation of getSignedUrlPromise

--- src/transforms/v2-to-v3/apis/replaceS3GetSignedUrlApi.ts
+++ src/transforms/v2-to-v3/apis/replaceS3GetSignedUrlApi.ts
@@ -30,71 +30,77 @@ export const replaceS3GetSignedUrlApi = (
   const clientIdentifiers = getClientIdentifiers(j, source, options);
 
   for (const clientId of clientIdentifiers) {
-    source
-      .find(j.CallExpression, getClientApiCallExpression(clientId, "getSignedUrl"))
-      .replaceWith((callExpression) => {
-        const args = callExpression.node.arguments;
+    for (const getSignedUrlApiName of ["getSignedUrl", "getSignedUrlPromise"]) {
+      source
+        .find(j.CallExpression, getClientApiCallExpression(clientId, getSignedUrlApiName))
+        .replaceWith((callExpression) => {
+          const args = callExpression.node.arguments;
 
-        const apiName = (args[0] as Literal).value as string;
-        const params = args[1];
+          const apiName = (args[0] as Literal).value as string;
+          const params = args[1];
 
-        const options = j.objectExpression([]);
+          const options = j.objectExpression([]);
 
-        if (params.type === "ObjectExpression") {
-          // Check if params has property 'Expires' and add it to options.
-          for (const property of params.properties) {
-            if (!OBJECT_PROPERTY_TYPE_LIST.includes(property.type)) continue;
-            const propertyKey = (property as Property | ObjectProperty).key;
-            const propertyValue = (property as Property | ObjectProperty).value;
-            if (propertyKey.type === "Identifier") {
-              const propertyKeyName = propertyKey.name;
-              if (propertyKeyName === "Expires") {
-                // Add 'expiresIn' property to options.
-                options.properties.push(
-                  j.objectProperty.from({
-                    key: j.identifier("expiresIn"),
-                    value: propertyValue,
-                    shorthand: true,
-                  })
-                );
-                // Remove 'Expires' property from params.
-                params.properties = params.properties.filter((property) => {
-                  if (!OBJECT_PROPERTY_TYPE_LIST.includes(property.type)) return true;
-                  const propertyKey = (property as Property | ObjectProperty).key;
-                  if (propertyKey.type !== "Identifier") return true;
-                  return propertyKey.name !== "Expires";
-                });
+          if (params.type === "ObjectExpression") {
+            // Check if params has property 'Expires' and add it to options.
+            for (const property of params.properties) {
+              if (!OBJECT_PROPERTY_TYPE_LIST.includes(property.type)) continue;
+              const propertyKey = (property as Property | ObjectProperty).key;
+              const propertyValue = (property as Property | ObjectProperty).value;
+              if (propertyKey.type === "Identifier") {
+                const propertyKeyName = propertyKey.name;
+                if (propertyKeyName === "Expires") {
+                  // Add 'expiresIn' property to options.
+                  options.properties.push(
+                    j.objectProperty.from({
+                      key: j.identifier("expiresIn"),
+                      value: propertyValue,
+                      shorthand: true,
+                    })
+                  );
+                  // Remove 'Expires' property from params.
+                  params.properties = params.properties.filter((property) => {
+                    if (!OBJECT_PROPERTY_TYPE_LIST.includes(property.type)) return true;
+                    const propertyKey = (property as Property | ObjectProperty).key;
+                    if (propertyKey.type !== "Identifier") return true;
+                    return propertyKey.name !== "Expires";
+                  });
+                }
               }
             }
+          } else {
+            // Add comment that expiredIn need to explicitly set.
+            options.properties.push(
+              j.objectProperty.from({
+                key: j.identifier("expiresIn"),
+                value: j.literal.from({
+                  value: "/* add value from 'Expires' from v2 call if present, else remove */",
+                }),
+                shorthand: true,
+              })
+            );
           }
-        } else {
-          // Add comment that expiredIn need to explicitly set.
-          options.properties.push(
-            j.objectProperty.from({
-              key: j.identifier("expiresIn"),
-              value: j.literal.from({
-                value: "/* add value from 'Expires' from v2 call if present, else remove */",
-              }),
-              shorthand: true,
-            })
-          );
-        }
 
-        const getSignedUrlArgs: (ClientIdentifier | NewExpression | ObjectExpression)[] = [
-          clientId,
-          j.newExpression(j.identifier(getCommandName(apiName)), [params]),
-        ];
+          const getSignedUrlArgs: (ClientIdentifier | NewExpression | ObjectExpression)[] = [
+            clientId,
+            j.newExpression(j.identifier(getCommandName(apiName)), [params]),
+          ];
 
-        if (options.properties.length > 0) {
-          getSignedUrlArgs.push(options);
-        }
+          if (options.properties.length > 0) {
+            getSignedUrlArgs.push(options);
+          }
 
-        return j.awaitExpression.from({
-          argument: j.callExpression.from({
+          const outputCallExpression = j.callExpression.from({
             callee: j.identifier("getSignedUrl"),
             arguments: getSignedUrlArgs,
-          }),
+          });
+
+          return getSignedUrlApiName.endsWith("Promise")
+            ? outputCallExpression
+            : j.awaitExpression.from({
+                argument: outputCallExpression,
+              });
         });
-      });
+    }
   }
 };
