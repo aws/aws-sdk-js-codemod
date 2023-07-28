b89e2ca2ff900424d20b80b292620d1152cd842e
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-07-28 10:23:39 -0700

    test: explicitly add expiresIn in getSignedUrl if not defined

--- src/transforms/v2-to-v3/__fixtures__/s3-get-signed-url/putObject.output.js
+++ src/transforms/v2-to-v3/__fixtures__/s3-get-signed-url/putObject.output.js
@@ -4,9 +4,7 @@ import { PutObjectCommand, S3 } from "@aws-sdk/client-s3";
 const s3 = new S3();
 const params = { Bucket: "bucket", Key: "key" };
 
-// If 'Expires' is defined in params, it is ignored in existing transformation.
-// Pass it as the 'expiresIn' value in the third argument of the getSignedUrl call.
-url = await getSignedUrl(s3, new PutObjectCommand(params));
+url = await getSignedUrl(s3, new PutObjectCommand(params), { expiresIn: 900 });
 
 url = await getSignedUrl(s3, new PutObjectCommand({ Bucket: "bucket", Key: "key" }));
 url = await getSignedUrl(s3, new PutObjectCommand({
