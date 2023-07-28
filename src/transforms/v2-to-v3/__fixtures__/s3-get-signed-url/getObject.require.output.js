519cb80841b2a49e6adb7dc359015d66f91fd906
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-07-28 12:38:52 -0700

    test: s3-get-signed-url/getObject.require

---
+++ src/transforms/v2-to-v3/__fixtures__/s3-get-signed-url/getObject.require.output.js
@@ -0,0 +1,17 @@
+const {
+        getSignedUrl
+      } = require("@aws-sdk/s3-request-presigner"),
+      {
+        GetObjectCommand,
+        S3
+      } = require("@aws-sdk/client-s3");
+
+const s3 = new S3();
+
+url = await getSignedUrl(s3, new GetObjectCommand({ Bucket: "bucket", Key: "key" }));
+url = await getSignedUrl(s3, new GetObjectCommand({
+  Bucket: "bucket",
+  Key: "key"
+}), {
+  expiresIn: 60
+});
