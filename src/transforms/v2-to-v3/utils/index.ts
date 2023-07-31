1c687286c36e5d744c6c37ae9e24308c196adceb
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-07-31 11:23:49 -0700

    chore: remove utility getDocClientNewExpression

--- src/transforms/v2-to-v3/utils/index.ts
+++ src/transforms/v2-to-v3/utils/index.ts
@@ -1,5 +1,4 @@
 export * from "./getClientDeepImportPath";
 export * from "./getClientNewExpression";
 export * from "./getDefaultLocalName";
-export * from "./getDocClientNewExpression";
 export * from "./isTypeScriptFile";
