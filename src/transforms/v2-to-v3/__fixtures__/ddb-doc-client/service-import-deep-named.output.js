c42d68fd6d082bcf50fb2fad4fec2ea19267b2ef
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-08-03 08:15:30 -0700

    test: add unsupported comment in ddb-doc-client/service-import-deep-named

--- src/transforms/v2-to-v3/__fixtures__/ddb-doc-client/service-import-deep-named.output.js
+++ src/transforms/v2-to-v3/__fixtures__/ddb-doc-client/service-import-deep-named.output.js
@@ -1,5 +1,6 @@
-import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
-import { DynamoDB } from "@aws-sdk/client-dynamodb";
+// Transformation DocumentClient named import is unsupported in aws-sdk-js-codemod.
+// Please convert to a default import, and rerun codemod.
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
 
-const documentClient = DynamoDBDocument.from(new DynamoDB({ region: "us-west-2" }));
-const response = await documentClient.scan({ TableName: "TABLE_NAME" });
+const documentClient = new DocumentClient({ region: "us-west-2" });
+const response = await documentClient.scan({ TableName: "TABLE_NAME" }).promise();
