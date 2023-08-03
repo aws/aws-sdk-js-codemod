66071de209b24abdd02cc33439e0794279400585
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-08-03 08:48:29 -0700

    test: add ddb-doc-client/service-require-deep-named

---
+++ src/transforms/v2-to-v3/__fixtures__/ddb-doc-client/service-require-deep-named.input.js
@@ -0,0 +1,4 @@
+const { DocumentClient } = require("aws-sdk/clients/dynamodb");
+
+const documentClient = new DocumentClient({ region: "us-west-2" });
+const response = await documentClient.scan({ TableName: "TABLE_NAME" }).promise();
