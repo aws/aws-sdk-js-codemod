b9212a4808156a48b7e9cd8fcfc63e65820e6a8b
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-08-03 17:28:50 -0700

    fix: named import from namespace specifier, if it exists

--- src/transforms/v2-to-v3/modules/importModule/addClientNamedModule.ts
+++ src/transforms/v2-to-v3/modules/importModule/addClientNamedModule.ts
@@ -1,9 +1,19 @@
-import { Collection, ImportSpecifier, JSCodeshift } from "jscodeshift";
+import {
+  Collection,
+  ImportNamespaceSpecifier,
+  ImportSpecifier,
+  JSCodeshift,
+  Property,
+  ObjectProperty,
+} from "jscodeshift";
 
+import { OBJECT_PROPERTY_TYPE_LIST } from "../../config";
 import { getImportDeclaration } from "../getImportDeclaration";
 import { getImportSpecifier } from "../getImportSpecifier";
 import { getImportSpecifiers } from "../getImportSpecifiers";
+import { getRequireProperty } from "../getRequireProperty";
 import { importSpecifierCompareFn } from "../importSpecifierCompareFn";
+import { objectPatternPropertyCompareFn } from "../objectPatternPropertyCompareFn";
 import { ClientModulesOptions, ImportSpecifierOptions } from "../types";
 
 export const addClientNamedModule = (
@@ -19,19 +29,78 @@ export const addClientNamedModule = (
   });
 
   if (importDeclarations.size()) {
-    const allImportSpecifiers = getImportSpecifiers(j, source, v3ClientPackageName).filter(
-      (importSpecifier) => importSpecifier?.type === "ImportSpecifier"
-    ) as ImportSpecifier[];
+    const importSpecifiers = getImportSpecifiers(j, source, v3ClientPackageName);
 
+    // Return if the import specifier already exists.
     if (
-      allImportSpecifiers.find(
-        (specifier) =>
-          specifier?.imported?.name === importedName && specifier?.local?.name === localName
-      )
+      importSpecifiers
+        .filter((importSpecifier) => importSpecifier?.type === "ImportSpecifier")
+        .find(
+          (specifier) =>
+            (specifier as ImportSpecifier)?.imported?.name === importedName &&
+            specifier?.local?.name === localName
+        )
     ) {
       return;
     }
 
+    const importNamespaceSpecifiers = importSpecifiers.filter(
+      (importSpecifier) => importSpecifier?.type === "ImportNamespaceSpecifier"
+    ) as ImportNamespaceSpecifier[];
+
+    // If namespace import exists.
+    if (importNamespaceSpecifiers.length > 0) {
+      const defaultLocalName = importNamespaceSpecifiers[0].local!.name;
+      const namedImportObjectProperty = getRequireProperty(j, { importedName, localName });
+
+      const existingVarDeclarator = source.find(j.VariableDeclarator, {
+        type: "VariableDeclarator",
+        init: { type: "Identifier", name: defaultLocalName },
+      });
+
+      // If variable declarator exists.
+      if (existingVarDeclarator.size()) {
+        // Return if property exists.
+        if (
+          existingVarDeclarator.some((varDeclarator) => {
+            const id = varDeclarator.node.id;
+            if (id.type !== "ObjectPattern") return false;
+            for (const property of id.properties) {
+              if (!OBJECT_PROPERTY_TYPE_LIST.includes(property.type)) continue;
+              const propertyKey = (property as Property | ObjectProperty).key;
+              if (propertyKey.type !== "Identifier") continue;
+              if (propertyKey.name === importedName) return true;
+            }
+            return false;
+          })
+        )
+          return;
+
+        // Add property to the first declarator.
+        const firstDeclaratorProperties = existingVarDeclarator.get(0).node.id.properties;
+        firstDeclaratorProperties.push(namedImportObjectProperty);
+        firstDeclaratorProperties.sort(objectPatternPropertyCompareFn);
+        return;
+      }
+
+      const varDeclaration = j.variableDeclaration("const", [
+        j.variableDeclarator(
+          j.objectPattern([namedImportObjectProperty]),
+          j.identifier(defaultLocalName)
+        ),
+      ]);
+
+      source
+        .find(j.ImportDeclaration, {
+          type: "ImportDeclaration",
+          specifiers: [importNamespaceSpecifiers[0]],
+          source: { value: v3ClientPackageName },
+        })
+        .insertAfter(varDeclaration);
+      return;
+    }
+
+    // Add named import to the first import declaration.
     const firstImportDeclSpecifiers = importDeclarations.nodes()[0].specifiers;
     if (firstImportDeclSpecifiers) {
       firstImportDeclSpecifiers.push(getImportSpecifier(j, { importedName, localName }));
