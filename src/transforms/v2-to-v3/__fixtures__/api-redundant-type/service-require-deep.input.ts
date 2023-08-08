7afafa7b1e57b38271c04e19129d947d3ba93292
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-08-08 08:57:26 -0700

    test: skip transformation of type annotation in api-redundant-type

--- src/transforms/v2-to-v3/__fixtures__/api-redundant-type/service-require-deep.input.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-redundant-type/service-require-deep.input.ts
@@ -10,46 +10,46 @@ const AppIntegrations = require("aws-sdk/clients/appintegrations");
 const SSM = require("aws-sdk/clients/ssm");
 
 // Native types
-const stringType: typeof S3.AccountId = "string";
-const booleanType: typeof S3.BucketKeyEnabled = true;
-const numberType: typeof S3.ContentLength = 123;
+const stringType: S3.AccountId = "string";
+const booleanType: S3.BucketKeyEnabled = true;
+const numberType: S3.ContentLength = 123;
 
 // Date
-const dateType: typeof S3.CreationDate = new Date();
+const dateType: S3.CreationDate = new Date();
 
 // Uint8Array
-const blobType: typeof RDSDataService._Blob = new Uint8Array();
+const blobType: RDSDataService._Blob = new Uint8Array();
 
 // Arrays
-const stringArray: typeof S3.AllowedHeaders = ["string1", "string2"];
-const booleanArray: typeof RDSDataService.BooleanArray = [true, false];
-const numberArray: typeof RDSDataService.LongArray = [123, 456];
-const blobArray: typeof IoTFleetWise.NetworkFilesList = [new Uint8Array()];
-const enumArray: typeof S3.ChecksumAlgorithmList = ["CRC32"];
-const structureArray: typeof S3.Buckets = [{ Name: "bucketName" }];
+const stringArray: S3.AllowedHeaders = ["string1", "string2"];
+const booleanArray: RDSDataService.BooleanArray = [true, false];
+const numberArray: RDSDataService.LongArray = [123, 456];
+const blobArray: IoTFleetWise.NetworkFilesList = [new Uint8Array()];
+const enumArray: S3.ChecksumAlgorithmList = ["CRC32"];
+const structureArray: S3.Buckets = [{ Name: "bucketName" }];
 
 // Maps
-const stringMap: typeof S3.Metadata = { key: "value" };
-const booleanMap: typeof APIGateway.MapOfStringToBoolean = { key: true };
-const numberMap: typeof SSM.AssociationStatusAggregatedCount = { key: 123 };
-const structureMap: typeof APIGateway.MapOfMethodSnapshot = { key: { apiKeyRequired: true } };
+const stringMap: S3.Metadata = { key: "value" };
+const booleanMap: APIGateway.MapOfStringToBoolean = { key: true };
+const numberMap: SSM.AssociationStatusAggregatedCount = { key: 123 };
+const structureMap: APIGateway.MapOfMethodSnapshot = { key: { apiKeyRequired: true } };
 
 // Nested arrays
-const arrayNestedTwice: typeof SageMakerGeospatial.LinearRing = [[1, 2], [3, 4]];
-const arrayNestedThrice: typeof SageMakerGeospatial.LinearRings = [[[1, 2], [3, 4]], [[4, 5], [6, 7]]];
-const arrayNestedFour: typeof SageMakerGeospatial.LinearRingsList = [
+const arrayNestedTwice: SageMakerGeospatial.LinearRing = [[1, 2], [3, 4]];
+const arrayNestedThrice: SageMakerGeospatial.LinearRings = [[[1, 2], [3, 4]], [[4, 5], [6, 7]]];
+const arrayNestedFour: SageMakerGeospatial.LinearRingsList = [
   [[[1], [2]], [[3], [4]]],
   [[[5], [6]], [[7], [8]]]
 ];
 
 // Nested maps
-const mapNestedTwice: typeof LexModelsV2.ConditionMap = { key: stringMap };
-const mapNestedTwiceStruct: typeof APIGateway.PathToMapOfMethodSnapshot = { key: structureMap };
+const mapNestedTwice: LexModelsV2.ConditionMap = { key: stringMap };
+const mapNestedTwiceStruct: APIGateway.PathToMapOfMethodSnapshot = { key: structureMap };
 
 // Nested arrays and maps
-const mapOfArrays: typeof NetworkManager.FilterMap = { key: ["value"] };
-const mapOfMapOfArrays: typeof AppIntegrations.ObjectConfiguration = { key: mapOfArrays };
-const mapOfArrayOfMaps: typeof DynamoDB.BatchGetResponseMap = { key: [{ key: { S:"A" }}] };
-const mapOfArrayOfArrays: typeof APIGateway.MapOfKeyUsages = { key: [[1], [2]] };
-const arrayOfMaps: typeof SSM.InventoryItemEntryList = [stringMap];
-const arrayOfMapOfArrays: typeof SSM.TargetMaps = [mapOfArrays];
+const mapOfArrays: NetworkManager.FilterMap = { key: ["value"] };
+const mapOfMapOfArrays: AppIntegrations.ObjectConfiguration = { key: mapOfArrays };
+const mapOfArrayOfMaps: DynamoDB.BatchGetResponseMap = { key: [{ key: { S:"A" }}] };
+const mapOfArrayOfArrays: APIGateway.MapOfKeyUsages = { key: [[1], [2]] };
+const arrayOfMaps: SSM.InventoryItemEntryList = [stringMap];
+const arrayOfMapOfArrays: SSM.TargetMaps = [mapOfArrays];
