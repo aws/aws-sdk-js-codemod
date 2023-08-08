24d4645633aa33ba77c51f5ef981fcc2391a2fe6
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-08-08 11:30:56 -0700

    test: transformation of type annotation in api-redundant-type

--- src/transforms/v2-to-v3/__fixtures__/api-redundant-type/global-require.input.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-redundant-type/global-require.input.ts
@@ -1,46 +1,46 @@
 const AWS = require("aws-sdk");
 
 // Native types
-const stringType: AWS.S3.AccountId = "string";
-const booleanType: AWS.S3.BucketKeyEnabled = true;
-const numberType: AWS.S3.ContentLength = 123;
+const stringType: typeof AWS.S3.AccountId = "string";
+const booleanType: typeof AWS.S3.BucketKeyEnabled = true;
+const numberType: typeof AWS.S3.ContentLength = 123;
 
 // Date
-const dateType: AWS.S3.CreationDate = new Date();
+const dateType: typeof AWS.S3.CreationDate = new Date();
 
 // Uint8Array
-const blobType: AWS.RDSDataService._Blob = new Uint8Array();
+const blobType: typeof AWS.RDSDataService._Blob = new Uint8Array();
 
 // Arrays
-const stringArray: AWS.S3.AllowedHeaders = ["string1", "string2"];
-const booleanArray: AWS.RDSDataService.BooleanArray = [true, false];
-const numberArray: AWS.RDSDataService.LongArray = [123, 456];
-const blobArray: AWS.IoTFleetWise.NetworkFilesList = [new Uint8Array()];
-const enumArray: AWS.S3.ChecksumAlgorithmList = ["CRC32"];
-const structureArray: AWS.S3.Buckets = [{ Name: "bucketName" }];
+const stringArray: typeof AWS.S3.AllowedHeaders = ["string1", "string2"];
+const booleanArray: typeof AWS.RDSDataService.BooleanArray = [true, false];
+const numberArray: typeof AWS.RDSDataService.LongArray = [123, 456];
+const blobArray: typeof AWS.IoTFleetWise.NetworkFilesList = [new Uint8Array()];
+const enumArray: typeof AWS.S3.ChecksumAlgorithmList = ["CRC32"];
+const structureArray: typeof AWS.S3.Buckets = [{ Name: "bucketName" }];
 
 // Maps
-const stringMap: AWS.S3.Metadata = { key: "value" };
-const booleanMap: AWS.APIGateway.MapOfStringToBoolean = { key: true };
-const numberMap: AWS.SSM.AssociationStatusAggregatedCount = { key: 123 };
-const structureMap: AWS.APIGateway.MapOfMethodSnapshot = { key: { apiKeyRequired: true } };
+const stringMap: typeof AWS.S3.Metadata = { key: "value" };
+const booleanMap: typeof AWS.APIGateway.MapOfStringToBoolean = { key: true };
+const numberMap: typeof AWS.SSM.AssociationStatusAggregatedCount = { key: 123 };
+const structureMap: typeof AWS.APIGateway.MapOfMethodSnapshot = { key: { apiKeyRequired: true } };
 
 // Nested arrays
-const arrayNestedTwice: AWS.SageMakerGeospatial.LinearRing = [[1, 2], [3, 4]];
-const arrayNestedThrice: AWS.SageMakerGeospatial.LinearRings = [[[1, 2], [3, 4]], [[4, 5], [6, 7]]];
-const arrayNestedFour: AWS.SageMakerGeospatial.LinearRingsList = [
+const arrayNestedTwice: typeof AWS.SageMakerGeospatial.LinearRing = [[1, 2], [3, 4]];
+const arrayNestedThrice: typeof AWS.SageMakerGeospatial.LinearRings = [[[1, 2], [3, 4]], [[4, 5], [6, 7]]];
+const arrayNestedFour: typeof AWS.SageMakerGeospatial.LinearRingsList = [
   [[[1], [2]], [[3], [4]]],
   [[[5], [6]], [[7], [8]]]
 ];
 
 // Nested maps
-const mapNestedTwice: AWS.LexModelsV2.ConditionMap = { key: stringMap };
-const mapNestedTwiceStruct: AWS.APIGateway.PathToMapOfMethodSnapshot = { key: structureMap };
+const mapNestedTwice: typeof AWS.LexModelsV2.ConditionMap = { key: stringMap };
+const mapNestedTwiceStruct: typeof AWS.APIGateway.PathToMapOfMethodSnapshot = { key: structureMap };
 
 // Nested arrays and maps
-const mapOfArrays: AWS.NetworkManager.FilterMap = { key: ["value"] };
-const mapOfMapOfArrays: AWS.AppIntegrations.ObjectConfiguration = { key: mapOfArrays };
-const mapOfArrayOfMaps: AWS.DynamoDB.BatchGetResponseMap = { key: [{ key: { S:"A" }}] };
-const mapOfArrayOfArrays: AWS.APIGateway.MapOfKeyUsages = { key: [[1], [2]] };
-const arrayOfMaps: AWS.SSM.InventoryItemEntryList = [stringMap];
-const arrayOfMapOfArrays: AWS.SSM.TargetMaps = [mapOfArrays];
+const mapOfArrays: typeof AWS.NetworkManager.FilterMap = { key: ["value"] };
+const mapOfMapOfArrays: typeof AWS.AppIntegrations.ObjectConfiguration = { key: mapOfArrays };
+const mapOfArrayOfMaps: typeof AWS.DynamoDB.BatchGetResponseMap = { key: [{ key: { S:"A" }}] };
+const mapOfArrayOfArrays: typeof AWS.APIGateway.MapOfKeyUsages = { key: [[1], [2]] };
+const arrayOfMaps: typeof AWS.SSM.InventoryItemEntryList = [stringMap];
+const arrayOfMapOfArrays: typeof AWS.SSM.TargetMaps = [mapOfArrays];
