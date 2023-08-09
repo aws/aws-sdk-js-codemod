725034e9a2ac74d2de37c48d3ef25cff863fbb4e
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-08-08 21:59:21 -0700

    Add type annotation for union of required types (#556)

--- src/transforms/v2-to-v3/ts-type/updateV2ClientType.ts
+++ src/transforms/v2-to-v3/ts-type/updateV2ClientType.ts
@@ -1,8 +1,10 @@
 import { ASTPath, Identifier, JSCodeshift, TSQualifiedName, TSTypeReference } from "jscodeshift";
+import { addTsTypeQueryToRefType } from "./addTsTypeQueryToRefType";
 import { getV3ClientType } from "./getV3ClientType";
 
 const nativeTsRefTypes = ["TSAnyKeyword", "TSStringKeyword", "TSNumberKeyword", "TSBooleanKeyword"];
-const nativeTsIdentifierTypes = ["Date", "Uint8Array", "Array", "Record"];
+const nativeTsUnionTypes = ["Array", "Record"];
+const nativeTsIdentifierTypes = ["Date", "Uint8Array", ...nativeTsUnionTypes];
 
 interface UpdateV2ClientTypeOptions {
   v2ClientName: string;
@@ -17,17 +19,26 @@ export const updateV2ClientType = (
 ) => {
   const v3ClientType = getV3ClientType(j, { v2ClientName, v2ClientTypeName, v2ClientLocalName });
 
-  if (
-    (v2ClientType.parentPath?.value.type === "TSTypeQuery" &&
-      nativeTsRefTypes.includes(v3ClientType.type)) ||
-    (v3ClientType.type === "TSTypeReference" &&
-      (v3ClientType as TSTypeReference).typeName.type === "Identifier" &&
-      nativeTsIdentifierTypes.includes(
-        ((v3ClientType as TSTypeReference).typeName as Identifier).name
-      ))
-  ) {
-    v2ClientType.parentPath?.replace(v3ClientType);
-  } else {
-    v2ClientType.replace(v3ClientType);
+  if (v2ClientType.parentPath?.value.type === "TSTypeQuery") {
+    if (nativeTsRefTypes.includes(v3ClientType.type)) {
+      v2ClientType.parentPath?.replace(v3ClientType);
+      return;
+    }
+
+    if (v3ClientType.type === "TSTypeReference") {
+      const v3ClientTypeRef = v3ClientType as TSTypeReference;
+      if (v3ClientTypeRef.typeName.type === "Identifier") {
+        const v3ClientTypeRefIdentifier = v3ClientTypeRef.typeName as Identifier;
+        if (nativeTsIdentifierTypes.includes(v3ClientTypeRefIdentifier.name)) {
+          if (nativeTsUnionTypes.includes(v3ClientTypeRefIdentifier.name)) {
+            addTsTypeQueryToRefType(j, v3ClientTypeRef);
+          }
+          v2ClientType.parentPath?.replace(v3ClientType);
+          return;
+        }
+      }
+    }
   }
+
+  v2ClientType.replace(v3ClientType);
 };
