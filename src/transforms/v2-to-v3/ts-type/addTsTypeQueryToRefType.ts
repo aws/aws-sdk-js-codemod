25ac71d38565ba5b90128a1bba332210b4450b31
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-08-08 21:37:25 -0700

    fix: recursive call to addTsTypeQueryToRefType for nativeTsUnionTypes

--- src/transforms/v2-to-v3/ts-type/addTsTypeQueryToRefType.ts
+++ src/transforms/v2-to-v3/ts-type/addTsTypeQueryToRefType.ts
@@ -1,6 +1,8 @@
 import { Identifier, TSTypeReference } from "jscodeshift";
 import { getDefaultLocalName } from "../utils";
 
+const nativeTsUnionTypes = ["Array", "Record"];
+
 export const addTsTypeQueryToRefType = (v3ClientType: TSTypeReference) => {
   const { typeParameters } = v3ClientType;
   if (typeParameters?.type === "TSTypeParameterInstantiation") {
@@ -12,6 +14,8 @@ export const addTsTypeQueryToRefType = (v3ClientType: TSTypeReference) => {
           if (typeRefIdentifier.name.startsWith(getDefaultLocalName(""))) {
             // TODO: Add TS Type Query to typeRefIdentifier instead.
             typeRefIdentifier.name = `typeof ${typeRefIdentifier.name}`;
+          } else if (nativeTsUnionTypes.includes(typeRefIdentifier.name)) {
+            addTsTypeQueryToRefType(typeRef);
           }
         }
       }
