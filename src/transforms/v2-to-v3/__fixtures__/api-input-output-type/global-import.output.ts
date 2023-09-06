217f4734ba6970e9d8da4fa76cd63220bf26c460
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-09-06 16:50:16 -0700

    test: lambda Invocation Request/Response

--- src/transforms/v2-to-v3/__fixtures__/api-input-output-type/global-import.output.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-input-output-type/global-import.output.ts
@@ -10,6 +10,12 @@ const {
   STS
 } = AWS_STS;
 
+import * as AWS_Lambda from "@aws-sdk/client-lambda";
+
+const {
+  Lambda
+} = AWS_Lambda;
+
 const ddbClient = new DynamoDB({ region: "us-west-2" });
 const listTablesInput: AWS_DynamoDB.ListTablesCommandInput = { Limit: 10 };
 const listTablesOutput: AWS_DynamoDB.ListTablesCommandOutput = await ddbClient
@@ -19,3 +25,8 @@ const stsClient = new STS({ region: "us-west-2" });
 const getCallerIdentityInput: AWS_STS.GetCallerIdentityCommandInput = {};
 const getCallerIdentityOutput: AWS_STS.GetCallerIdentityCommandOutput = await stsClient
   .getCallerIdentity(getCallerIdentityInput);
+
+const lambdaClient = new Lambda({ region: "us-west-2" });
+const invokeInput: AWS_Lambda.InvokeCommandInput = { FunctionName: "my-function" };
+const invokeOutput: AWS_Lambda.InvokeCommandOutput = await lambdaClient
+  .invoke(invokeInput);
