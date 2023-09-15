bcee6314803f22c9d7640fbc39e088d5e24abf65
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-09-15 10:14:49 -0700

    Transform AWS.util.array functions (#587)

---
+++ src/transforms/v2-to-v3/__fixtures__/util/arrayEach.output.js
@@ -0,0 +1,3 @@
+["one", "two", "three"].forEach((item, index) => {
+  console.log({ index, item });
+});
---
+++ src/transforms/v2-to-v3/aws-util/replaceAwsUtilArrayFunctions.ts
@@ -0,0 +1,24 @@
+import { Collection, FunctionExpression, Identifier, JSCodeshift } from "jscodeshift";
+import { getAwsUtilCallExpression } from "./getAwsUtilCallExpression";
+
+export const replaceAwsUtilArrayFunctions = (
+  j: JSCodeshift,
+  source: Collection<unknown>,
+  v2GlobalName: string
+) => {
+  // replace arrayEach
+  getAwsUtilCallExpression(j, source, { v2GlobalName, functionName: "arrayEach" })
+    .filter(({ node }) => node.arguments.length === 2)
+    .replaceWith(({ node }) => {
+      const array = node.arguments[0] as Identifier;
+      const iteratee = node.arguments[1] as FunctionExpression;
+      return j.callExpression(j.memberExpression(array, j.identifier("forEach")), [iteratee]);
+    });
+
+  // replace arraySliceFn
+  getAwsUtilCallExpression(j, source, { v2GlobalName, functionName: "arraySliceFn" })
+    .filter(({ node }) => node.arguments.length === 1)
+    .replaceWith(({ node }) =>
+      j.memberExpression(node.arguments[0] as Identifier, j.identifier("slice"))
+    );
+};
