4fa1562b662bb52a88b9bc1fbbd86fa0b0991111
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-03 09:23:36 -0700

    fix(scripts): separate getServiceRequireOutput from getGlobalRequireOutput

--- scripts/generateNewClientTests/getServiceRequireOutput.ts
+++ scripts/generateNewClientTests/getServiceRequireOutput.ts
@@ -1,3 +1,13 @@
-import { getGlobalRequireOutput } from "./getGlobalRequireOutput";
+import { CLIENTS_TO_TEST } from "./config";
+import { getClientNamesSortedByPackageName } from "./getClientNamesSortedByPackageName";
+import { getV3ClientsNewExpressionCode } from "./getV3ClientsNewExpressionCode";
+import { getV3PackageRequiresCode } from "./getV3PackageRequiresCode";
 
-export const getServiceRequireOutput = getGlobalRequireOutput;
+export const getServiceRequireOutput = () => {
+  let content = ``;
+
+  content += getV3PackageRequiresCode(getClientNamesSortedByPackageName(CLIENTS_TO_TEST));
+  content += getV3ClientsNewExpressionCode(CLIENTS_TO_TEST);
+
+  return content;
+};
