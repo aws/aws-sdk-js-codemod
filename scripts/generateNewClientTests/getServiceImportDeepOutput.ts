71420ace84d35dd007f3a24d4aa0df4d31dd3bce
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-15 16:57:17 -0700

    chore(scripts): update imports in service import deep

--- scripts/generateNewClientTests/getServiceImportDeepOutput.ts
+++ scripts/generateNewClientTests/getServiceImportDeepOutput.ts
@@ -1,3 +1,4 @@
+import { CLIENT_NAMES_MAP } from "../../src/transforms/v2-to-v3/config";
 import { CLIENTS_TO_TEST } from "./config";
 import { getV3ClientsNewExpressionCode } from "./getV3ClientsNewExpressionCode";
 import { getV3PackageImportsCode } from "./getV3PackageImportsCode";
@@ -5,9 +6,11 @@ import { getV3PackageImportsCode } from "./getV3PackageImportsCode";
 export const getServiceImportDeepOutput = () => {
   let content = ``;
 
-  content += getV3PackageImportsCode(CLIENTS_TO_TEST);
+  content += getV3PackageImportsCode(CLIENTS_TO_TEST, { useV3ClientName: true });
   content += `\n`;
-  content += getV3ClientsNewExpressionCode(CLIENTS_TO_TEST);
+  content += getV3ClientsNewExpressionCode(
+    CLIENTS_TO_TEST.map((clientName) => CLIENT_NAMES_MAP[clientName])
+  );
 
   return content;
 };
