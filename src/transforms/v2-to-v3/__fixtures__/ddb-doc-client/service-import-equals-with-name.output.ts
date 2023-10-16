e3108a70b76d5e0eb30ccc1085999740040c6dd9
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-16 16:08:23 -0700

    test: use DynamoDB client name in ddb-doc-client/service-import-equals-with-name

--- src/transforms/v2-to-v3/__fixtures__/ddb-doc-client/service-import-equals-with-name.output.ts
+++ src/transforms/v2-to-v3/__fixtures__/ddb-doc-client/service-import-equals-with-name.output.ts
@@ -1,5 +1,5 @@
 import AWS_lib_dynamodb = require("@aws-sdk/lib-dynamodb");
 import AWS_DynamoDB = require("@aws-sdk/client-dynamodb");
 
-const documentClient = AWS_lib_dynamodb.DynamoDBDocument.from(new AWS_DynamoDB.DynamoDBClient({ region: "us-west-2" }));
+const documentClient = AWS_lib_dynamodb.DynamoDBDocument.from(new AWS_DynamoDB.DynamoDB({ region: "us-west-2" }));
 const response = await documentClient.scan({ TableName: "TABLE_NAME" });
