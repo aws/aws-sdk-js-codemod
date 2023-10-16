ac9ff85b7f9bbc31884f9e6a317e67b45754aba1
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-16 11:18:29 -0700

    fix: add defaultLocalName in case of import equals import type

--- src/transforms/v2-to-v3/ts-type/getV3ClientType.ts
+++ src/transforms/v2-to-v3/ts-type/getV3ClientType.ts
@@ -2,6 +2,7 @@ import { JSCodeshift, TSType } from "jscodeshift";
 
 import { CLIENT_TYPES_MAP } from "../config";
 import { CLIENT_REQ_RESP_TYPES_MAP } from "../config/CLIENT_REQ_RESP_TYPES_MAP";
+import { ImportType } from "../modules";
 import { getDefaultLocalName } from "../utils";
 import { getTypeForString } from "./getTypeForString";
 
@@ -9,17 +10,33 @@ export interface GetV3ClientTypeOptions {
   v2ClientLocalName: string;
   v2ClientName: string;
   v2ClientTypeName: string;
+  importType: ImportType;
 }
+
+const getV3ClientTypeName = (
+  v3ClientTypeName: string,
+  v2ClientLocalName: string,
+  importType: ImportType
+) => {
+  const v3ClientTypeNameSections = [v3ClientTypeName];
+  if (importType === ImportType.IMPORT_EQUALS) {
+    v3ClientTypeNameSections.unshift(getDefaultLocalName(v2ClientLocalName));
+  }
+  return v3ClientTypeNameSections.join(".");
+};
 
 export const getV3ClientType = (
   j: JSCodeshift,
-  { v2ClientLocalName, v2ClientName, v2ClientTypeName }: GetV3ClientTypeOptions
+  { v2ClientLocalName, v2ClientName, v2ClientTypeName, importType }: GetV3ClientTypeOptions
 ): TSType => {
   const clientReqRespTypesMap = CLIENT_REQ_RESP_TYPES_MAP[v2ClientName];
   const defaultLocalName = getDefaultLocalName(v2ClientLocalName);
 
   if (Object.keys(clientReqRespTypesMap).includes(v2ClientTypeName)) {
-    return j.tsTypeReference(j.identifier(clientReqRespTypesMap[v2ClientTypeName]));
+    const v3ClientTypeName = clientReqRespTypesMap[v2ClientTypeName];
+    return j.tsTypeReference(
+      j.identifier(getV3ClientTypeName(v3ClientTypeName, v2ClientLocalName, importType))
+    );
   }
 
   const clientTypesMap = CLIENT_TYPES_MAP[v2ClientName];
@@ -28,5 +45,7 @@ export const getV3ClientType = (
     return getTypeForString(j, defaultLocalName, clientTypesMap[v2ClientTypeName]);
   }
 
-  return j.tsTypeReference(j.identifier(v2ClientTypeName));
+  return j.tsTypeReference(
+    j.identifier(getV3ClientTypeName(v2ClientTypeName, v2ClientLocalName, importType))
+  );
 };
