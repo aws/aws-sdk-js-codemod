26d945639276a48cb1098edf5fa88918c1af0468
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-16 15:21:09 -0700

    test: update ddb-doc-client/*-import-equals

--- src/transforms/v2-to-v3/__fixtures__/ddb-doc-client/service-import-equals-with-name.output.ts
+++ src/transforms/v2-to-v3/__fixtures__/ddb-doc-client/service-import-equals-with-name.output.ts
@@ -1,14 +1,5 @@
 import AWS_lib_dynamodb = require("@aws-sdk/lib-dynamodb");
-
-const {
-  DynamoDBDocument
-} = AWS_lib_dynamodb;
-
 import AWS_DynamoDB = require("@aws-sdk/client-dynamodb");
 
-const {
-  DynamoDB: DynamoDBClient
-} = AWS_DynamoDB;
-
-const documentClient = DynamoDBDocument.from(new DynamoDBClient({ region: "us-west-2" }));
+const documentClient = AWS_lib_dynamodb.DynamoDBDocument.from(new AWS_DynamoDB.DynamoDBClient({ region: "us-west-2" }));
 const response = await documentClient.scan({ TableName: "TABLE_NAME" });
