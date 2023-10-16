12254be4ec121c374fec601e057d158c2ad4d44b
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-15 19:41:52 -0700

    fix(scripts): use v3 global name in ServiceRequireOutput

--- scripts/generateNewClientTests/getServiceRequireOutput.ts
+++ scripts/generateNewClientTests/getServiceRequireOutput.ts
@@ -1,13 +1,3 @@
-import { CLIENTS_TO_TEST } from "./config";
-import { getClientNamesSortedByPackageName } from "./getClientNamesSortedByPackageName";
-import { getV3ClientsNewExpressionCode } from "./getV3ClientsNewExpressionCode";
-import { getV3PackageRequiresCode } from "./getV3PackageRequiresCode";
+import { getGlobalRequireOutput } from "./getGlobalRequireOutput";
 
-export const getServiceRequireOutput = () => {
-  let content = ``;
-
-  content += getV3PackageRequiresCode(getClientNamesSortedByPackageName(CLIENTS_TO_TEST));
-  content += getV3ClientsNewExpressionCode(CLIENTS_TO_TEST);
-
-  return content;
-};
+export const getServiceRequireOutput = getGlobalRequireOutput;
