85d389381021ea2e35ba863ebdfee9ea38045666
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-16 12:43:28 -0700

    fix: return v3 type in getV3ClientTypes

--- src/transforms/v2-to-v3/ts-type/getV3ClientTypes.ts
+++ src/transforms/v2-to-v3/ts-type/getV3ClientTypes.ts
@@ -1,6 +1,6 @@
 import { Collection, JSCodeshift } from "jscodeshift";
 
-import { CLIENT_TYPES_MAP } from "../config";
+import { CLIENT_TYPES_MAP, CLIENT_REQ_RESP_TYPES_MAP } from "../config";
 import { getClientTypeNames, GetClientTypeNamesOptions } from "./getClientTypeNames";
 
 const arrayBracketRegex = /<([\w]+)>/g;
@@ -22,14 +22,28 @@ export const getV3ClientTypes = (
   const { v2ClientName } = options;
 
   const clientTypeNames = getClientTypeNames(j, source, options);
+
   const clientTypesMap = CLIENT_TYPES_MAP[v2ClientName];
   const v3ClientUnavailableTypes = Object.keys(clientTypesMap);
+  const clientReqRespTypesMap = CLIENT_REQ_RESP_TYPES_MAP[v2ClientName];
 
-  return clientTypeNames.filter((clientTypeName) => {
-    if (!v3ClientUnavailableTypes.includes(clientTypeName)) {
-      return true;
-    }
-    const typesFromString = getTypesFromString(clientTypesMap[clientTypeName]);
-    return typesFromString.some((type) => !nativeTypes.includes(type));
-  });
+  return clientTypeNames
+    .filter((clientTypeName) => {
+      if (!v3ClientUnavailableTypes.includes(clientTypeName)) {
+        return true;
+      }
+      const typesFromString = getTypesFromString(clientTypesMap[clientTypeName]);
+      return typesFromString.some((type) => !nativeTypes.includes(type));
+    })
+    .map((clientTypeName) => {
+      if (Object.keys(clientReqRespTypesMap).includes(clientTypeName)) {
+        return clientReqRespTypesMap[clientTypeName];
+      }
+
+      if (Object.keys(clientTypesMap).includes(clientTypeName)) {
+        return clientTypesMap[clientTypeName];
+      }
+
+      return clientTypeName;
+    });
 };
