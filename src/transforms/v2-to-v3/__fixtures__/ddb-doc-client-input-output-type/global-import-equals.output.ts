2884733c0b9501fdf5400fa41770fa9f9daecd46
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-17 16:03:59 -0700

    test: update ddb-doc-client-input-output-type

--- src/transforms/v2-to-v3/__fixtures__/ddb-doc-client-input-output-type/global-import-equals.output.ts
+++ src/transforms/v2-to-v3/__fixtures__/ddb-doc-client-input-output-type/global-import-equals.output.ts
@@ -1,11 +1,15 @@
 import AWS_lib_dynamodb = require("@aws-sdk/lib-dynamodb");
+import DynamoDBDocument = AWS_lib_dynamodb.DynamoDBDocument;
+import ScanCommandOutput = AWS_lib_dynamodb.ScanCommandOutput;
+import ScanCommandInput = AWS_lib_dynamodb.ScanCommandInput;
 import AWS_DynamoDB = require("@aws-sdk/client-dynamodb");
+import DynamoDB = AWS_DynamoDB.DynamoDB;
 
-const docClient = AWS_lib_dynamodb.DynamoDBDocument.from(new AWS_DynamoDB.DynamoDB({ region: "us-west-2" }));
+const docClient = DynamoDBDocument.from(new DynamoDB({ region: "us-west-2" }));
 
-const docClientScanInput: AWS_lib_dynamodb.ScanCommandInput = {
+const docClientScanInput: ScanCommandInput = {
   TableName: "TableName"
 };
 
-const docClientScanOutput: AWS_lib_dynamodb.ScanCommandOutput = await docClient
+const docClientScanOutput: ScanCommandOutput = await docClient
   .scan(docClientScanInput);
