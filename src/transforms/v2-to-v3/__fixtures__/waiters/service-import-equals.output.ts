70176e807be2c9d30ab09ad303669920cd817c5d
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-16 22:39:36 -0700

    test: update waiters

--- src/transforms/v2-to-v3/__fixtures__/waiters/service-import-equals.output.ts
+++ src/transforms/v2-to-v3/__fixtures__/waiters/service-import-equals.output.ts
@@ -1,16 +1,11 @@
 import AWS_S3 = require("@aws-sdk/client-s3");
 
-const {
-  S3,
-  waitUntilBucketExists
-} = AWS_S3;
-
 const Bucket = "BUCKET_NAME";
-const client = new S3({ region: "REGION" });
+const client = new AWS_S3.S3({ region: "REGION" });
 
 await client.createBucket({ Bucket });
 
-await waitUntilBucketExists({
+await AWS_S3.waitUntilBucketExists({
   client,
   maxWaitTime: 200
 }, { Bucket });
