54b8c7088c7c38154a3afc4406b894ffb5d67d28
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-16 17:55:27 -0700

    test: update api-input-output-type

--- src/transforms/v2-to-v3/__fixtures__/api-input-output-type/global-import-equals.output.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-input-output-type/global-import-equals.output.ts
@@ -1,32 +1,18 @@
 import AWS_DynamoDB = require("@aws-sdk/client-dynamodb");
-
-const {
-  DynamoDB
-} = AWS_DynamoDB;
-
 import AWS_Lambda = require("@aws-sdk/client-lambda");
-
-const {
-  Lambda
-} = AWS_Lambda;
-
 import AWS_STS = require("@aws-sdk/client-sts");
 
-const {
-  STS
-} = AWS_STS;
-
-const ddbClient = new DynamoDB({ region: "us-west-2" });
+const ddbClient = new AWS_DynamoDB.DynamoDB({ region: "us-west-2" });
 const listTablesInput: AWS_DynamoDB.ListTablesCommandInput = { Limit: 10 };
 const listTablesOutput: AWS_DynamoDB.ListTablesCommandOutput = await ddbClient
   .listTables(listTablesInput);
 
-const stsClient = new STS({ region: "us-west-2" });
+const stsClient = new AWS_STS.STS({ region: "us-west-2" });
 const getCallerIdentityInput: AWS_STS.GetCallerIdentityCommandInput = {};
 const getCallerIdentityOutput: AWS_STS.GetCallerIdentityCommandOutput = await stsClient
   .getCallerIdentity(getCallerIdentityInput);
 
-const lambdaClient = new Lambda({ region: "us-west-2" });
+const lambdaClient = new AWS_Lambda.Lambda({ region: "us-west-2" });
 const invokeInput: AWS_Lambda.InvokeCommandInput = { FunctionName: "my-function" };
 const invokeOutput: AWS_Lambda.InvokeCommandOutput = await lambdaClient
   .invoke(invokeInput);
