febcdff5b499b752dd4450ffd0e4e570271065f3
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-16 18:12:03 -0700

    test: revert non importEquals tests in ddb-doc-client-input-output-type

--- src/transforms/v2-to-v3/__fixtures__/ddb-doc-client-input-output-type/global-import.output.ts
+++ src/transforms/v2-to-v3/__fixtures__/ddb-doc-client-input-output-type/global-import.output.ts
@@ -1,11 +1,16 @@
-import { DynamoDBDocument, ScanCommandInput, ScanCommandOutput } from "@aws-sdk/lib-dynamodb";
+import * as AWS_DynamoDBDocumentClient from "@aws-sdk/lib-dynamodb";
+
+const {
+  DynamoDBDocument
+} = AWS_DynamoDBDocumentClient;
+
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
 
 const docClient = DynamoDBDocument.from(new DynamoDB({ region: "us-west-2" }));
 
-const docClientScanInput: ScanCommandInput = {
+const docClientScanInput: AWS_DynamoDBDocumentClient.ScanCommandInput = {
   TableName: "TableName"
 };
 
-const docClientScanOutput: ScanCommandOutput = await docClient
+const docClientScanOutput: AWS_DynamoDBDocumentClient.ScanCommandOutput = await docClient
   .scan(docClientScanInput);
--- src/transforms/v2-to-v3/__fixtures__/ddb-doc-client-input-output-type/service-import.output.ts
+++ src/transforms/v2-to-v3/__fixtures__/ddb-doc-client-input-output-type/service-import.output.ts
@@ -1,11 +1,16 @@
-import { DynamoDBDocument, ScanCommandInput, ScanCommandOutput } from "@aws-sdk/lib-dynamodb";
+import * as AWS_DynDBDocumentClient from "@aws-sdk/lib-dynamodb";
+
+const {
+  DynamoDBDocument
+} = AWS_DynDBDocumentClient;
+
 import { DynamoDB as DynDB } from "@aws-sdk/client-dynamodb";
 
 const docClient = DynamoDBDocument.from(new DynDB({ region: "us-west-2" }));
 
-const docClientScanInput: ScanCommandInput = {
+const docClientScanInput: AWS_DynDBDocumentClient.ScanCommandInput = {
   TableName: "TableName"
 };
 
-const docClientScanOutput: ScanCommandOutput = await docClient
+const docClientScanOutput: AWS_DynDBDocumentClient.ScanCommandOutput = await docClient
   .scan(docClientScanInput);
