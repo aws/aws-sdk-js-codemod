907dc1a390d0d465495919ae726121c92ac150ae
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-17 15:57:30 -0700

    test: update api-input-output-type

--- src/transforms/v2-to-v3/__fixtures__/api-input-output-type/service-import-equals.output.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-input-output-type/service-import-equals.output.ts
@@ -1,18 +1,27 @@
 import AWS_DynamoDB = require("@aws-sdk/client-dynamodb");
+import DynamoDB = AWS_DynamoDB.DynamoDB;
+import ListTablesCommandOutput = AWS_DynamoDB.ListTablesCommandOutput;
+import ListTablesCommandInput = AWS_DynamoDB.ListTablesCommandInput;
 import AWS_Lambda = require("@aws-sdk/client-lambda");
+import Lambda = AWS_Lambda.Lambda;
+import InvokeCommandOutput = AWS_Lambda.InvokeCommandOutput;
+import InvokeCommandInput = AWS_Lambda.InvokeCommandInput;
 import AWS_STS = require("@aws-sdk/client-sts");
+import STS = AWS_STS.STS;
+import GetCallerIdentityCommandOutput = AWS_STS.GetCallerIdentityCommandOutput;
+import GetCallerIdentityCommandInput = AWS_STS.GetCallerIdentityCommandInput;
 
-const ddbClient = new AWS_DynamoDB.DynamoDB({ region: "us-west-2" });
-const listTablesInput: AWS_DynamoDB.ListTablesCommandInput = { Limit: 10 };
-const listTablesOutput: AWS_DynamoDB.ListTablesCommandOutput = await ddbClient
+const ddbClient = new DynamoDB({ region: "us-west-2" });
+const listTablesInput: ListTablesCommandInput = { Limit: 10 };
+const listTablesOutput: ListTablesCommandOutput = await ddbClient
   .listTables(listTablesInput);
 
-const stsClient = new AWS_STS.STS({ region: "us-west-2" });
-const getCallerIdentityInput: AWS_STS.GetCallerIdentityCommandInput = {};
-const getCallerIdentityOutput: AWS_STS.GetCallerIdentityCommandOutput = await stsClient
+const stsClient = new STS({ region: "us-west-2" });
+const getCallerIdentityInput: GetCallerIdentityCommandInput = {};
+const getCallerIdentityOutput: GetCallerIdentityCommandOutput = await stsClient
   .getCallerIdentity(getCallerIdentityInput);
 
-const lambdaClient = new AWS_Lambda.Lambda({ region: "us-west-2" });
-const invokeInput: AWS_Lambda.InvokeCommandInput = { FunctionName: "my-function" };
-const invokeOutput: AWS_Lambda.InvokeCommandOutput = await lambdaClient
+const lambdaClient = new Lambda({ region: "us-west-2" });
+const invokeInput: InvokeCommandInput = { FunctionName: "my-function" };
+const invokeOutput: InvokeCommandOutput = await lambdaClient
   .invoke(invokeInput);
