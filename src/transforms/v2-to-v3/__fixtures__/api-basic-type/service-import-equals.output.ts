689a2b33f8b88ad9ac7788edd4038ab275f9c7a1
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-17 15:54:47 -0700

    test: update api-basic-type

--- src/transforms/v2-to-v3/__fixtures__/api-basic-type/service-import-equals.output.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-basic-type/service-import-equals.output.ts
@@ -1,3 +1,4 @@
 import AWS_S3 = require("@aws-sdk/client-s3");
+import Tag = AWS_S3.Tag;
 
-const testTags: AWS_S3.Tag[] = [{ Key: "key", Value: "value" }];
+const testTags: Tag[] = [{ Key: "key", Value: "value" }];
