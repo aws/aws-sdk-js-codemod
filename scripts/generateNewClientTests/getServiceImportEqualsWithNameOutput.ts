b2742c1842d683528b7e1b9e7f9aeca29071c28a
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-16 17:27:53 -0700

    chore(scripts): use default import in new client import equals

--- scripts/generateNewClientTests/getServiceImportEqualsWithNameOutput.ts
+++ scripts/generateNewClientTests/getServiceImportEqualsWithNameOutput.ts
@@ -1,13 +1,3 @@
-import { CLIENTS_TO_TEST } from "./config";
-import { getClientNameWithLocalSuffix } from "./getClientNameWithLocalSuffix";
-import { getV3ClientsNewExpressionCode } from "./getV3ClientsNewExpressionCode";
-import { getV3PackageImportEqualsCode } from "./getV3PackageImportEqualsCode";
+import { getServiceImportEqualsOutput } from "./getServiceImportEqualsOutput";
 
-export const getServiceImportEqualsWithNameOutput = () => {
-  let content = ``;
-
-  content += getV3PackageImportEqualsCode(CLIENTS_TO_TEST, { useLocalSuffix: true });
-  content += getV3ClientsNewExpressionCode(CLIENTS_TO_TEST.map(getClientNameWithLocalSuffix));
-
-  return content;
-};
+export const getServiceImportEqualsWithNameOutput = getServiceImportEqualsOutput;
