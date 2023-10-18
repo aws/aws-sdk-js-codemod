051b2d2d3353aa85fe3cb1be87fe3082bac5edd7
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-18 15:10:32 -0700

    Remove require property declarator from declaration

--- src/transforms/v2-to-v3/modules/removeRequireProperty.ts
+++ src/transforms/v2-to-v3/modules/removeRequireProperty.ts
@@ -1,4 +1,4 @@
-import { Collection, JSCodeshift } from "jscodeshift";
+import { Collection, Identifier, JSCodeshift, VariableDeclarator } from "jscodeshift";
 
 import { getRequireDeclaratorsWithProperty } from "./getRequireDeclaratorsWithProperty";
 
@@ -20,15 +20,44 @@ export const removeRequireProperty = (
   });
 
   requireDeclarators.forEach((varDeclarator) => {
-    const varDeclarationCollection = j(varDeclarator).closest(j.VariableDeclaration);
+    const varDeclaration = varDeclarator.parentPath.parentPath;
 
-    // Remove VariableDeclarator as it contains the only identifier.
-    j(varDeclarator).remove();
+    // Removes variable declarator from the declarations.
+    varDeclaration.value.declarations = varDeclaration.value.declarations.filter(
+      (declaration: VariableDeclarator | Identifier) => {
+        if (declaration.type === "Identifier") return true;
+
+        const id = declaration.id;
+        if (id.type !== "Identifier") return true;
+        if (id.name !== localName) return true;
+
+        const init = declaration.init;
+        if (!init) return true;
+        if (init.type !== "MemberExpression") return true;
+
+        const object = init.object;
+        if (object.type !== "CallExpression") return true;
+
+        const callee = object.callee;
+        if (callee.type !== "Identifier") return true;
+        if (callee.name !== "require") return true;
+
+        const args = object.arguments;
+        if (args.length !== 1) return true;
+        if (args[0].type !== "Literal") return true;
+        if (args[0].value !== sourceValue) return true;
+
+        const property = init.property;
+        if (property.type !== "Identifier") return true;
+        if (property.name !== propertyName) return true;
+
+        return false;
+      }
+    );
 
     // Remove VariableDeclaration if there are no declarations.
-    const varDeclaration = varDeclarationCollection.nodes()[0];
-    if (varDeclaration && varDeclaration.declarations?.length === 0) {
-      varDeclarationCollection.remove();
+    if (varDeclaration.value.declarations?.length === 0) {
+      j(varDeclaration).remove();
     }
   });
 };
