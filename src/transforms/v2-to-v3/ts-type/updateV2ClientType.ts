6e23c4b8d7587544e4e68c6a5f0f5a3a0bc3e82b
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-17 17:31:17 -0700

    Remove type annotation for union of required types

--- src/transforms/v2-to-v3/ts-type/updateV2ClientType.ts
+++ src/transforms/v2-to-v3/ts-type/updateV2ClientType.ts
@@ -1,10 +1,8 @@
 import { ASTPath, Identifier, JSCodeshift, TSQualifiedName, TSTypeReference } from "jscodeshift";
-import { addTsTypeQueryToRefType } from "./addTsTypeQueryToRefType";
 import { getV3ClientType } from "./getV3ClientType";
 
 const nativeTsRefTypes = ["TSAnyKeyword", "TSStringKeyword", "TSNumberKeyword", "TSBooleanKeyword"];
-const nativeTsUnionTypes = ["Array", "Record"];
-const nativeTsIdentifierTypes = ["Date", "Uint8Array", ...nativeTsUnionTypes];
+const nativeTsIdentifierTypes = ["Date", "Uint8Array", "Array", "Record"];
 
 interface UpdateV2ClientTypeOptions {
   v2ClientName: string;
@@ -19,26 +17,17 @@ export const updateV2ClientType = (
 ) => {
   const v3ClientType = getV3ClientType(j, options);
 
-  if (v2ClientType.parentPath?.value.type === "TSTypeQuery") {
-    if (nativeTsRefTypes.includes(v3ClientType.type)) {
-      v2ClientType.parentPath?.replace(v3ClientType);
-      return;
-    }
-
-    if (v3ClientType.type === "TSTypeReference") {
-      const v3ClientTypeRef = v3ClientType as TSTypeReference;
-      if (v3ClientTypeRef.typeName.type === "Identifier") {
-        const v3ClientTypeRefIdentifier = v3ClientTypeRef.typeName as Identifier;
-        if (nativeTsIdentifierTypes.includes(v3ClientTypeRefIdentifier.name)) {
-          if (nativeTsUnionTypes.includes(v3ClientTypeRefIdentifier.name)) {
-            addTsTypeQueryToRefType(j, v3ClientTypeRef);
-          }
-          v2ClientType.parentPath?.replace(v3ClientType);
-          return;
-        }
-      }
-    }
+  if (
+    (v2ClientType.parentPath?.value.type === "TSTypeQuery" &&
+      nativeTsRefTypes.includes(v3ClientType.type)) ||
+    (v3ClientType.type === "TSTypeReference" &&
+      (v3ClientType as TSTypeReference).typeName.type === "Identifier" &&
+      nativeTsIdentifierTypes.includes(
+        ((v3ClientType as TSTypeReference).typeName as Identifier).name
+      ))
+  ) {
+    v2ClientType.parentPath?.replace(v3ClientType);
+  } else {
+    v2ClientType.replace(v3ClientType);
   }
-
-  v2ClientType.replace(v3ClientType);
 };
