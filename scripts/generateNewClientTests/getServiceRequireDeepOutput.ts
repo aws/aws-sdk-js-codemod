16164cd799eb6e927e410528a2a94120d28a67fe
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-19 09:09:16 -0700

    chore(scripts): add newline for new-client require

--- scripts/generateNewClientTests/getServiceRequireDeepOutput.ts
+++ scripts/generateNewClientTests/getServiceRequireDeepOutput.ts
@@ -3,7 +3,7 @@ import { getV3ClientsNewExpressionCode } from "./getV3ClientsNewExpressionCode";
 import { getV3PackageRequiresCode } from "./getV3PackageRequiresCode";
 
 export const getServiceRequireDeepOutput = () => {
-  let content = ``;
+  let content = `\n\n`;
 
   content += getV3PackageRequiresCode(CLIENTS_TO_TEST);
   content += getV3ClientsNewExpressionCode(CLIENTS_TO_TEST);
--- scripts/generateNewClientTests/getServiceRequireDeepWithNameOutput.ts
+++ scripts/generateNewClientTests/getServiceRequireDeepWithNameOutput.ts
@@ -4,7 +4,7 @@ import { getV3ClientsNewExpressionCode } from "./getV3ClientsNewExpressionCode";
 import { getV3PackageRequiresCode } from "./getV3PackageRequiresCode";
 
 export const getServiceRequireDeepWithNameOutput = () => {
-  let content = ``;
+  let content = `\n\n`;
 
   content += getV3PackageRequiresCode(CLIENTS_TO_TEST, { useLocalSuffix: true });
   content += getV3ClientsNewExpressionCode(CLIENTS_TO_TEST.map(getClientNameWithLocalSuffix));
