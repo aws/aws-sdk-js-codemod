577fc33d1ff72130286e0bd55f171a418617ecd3
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-10-20 08:07:16 -0700

    Add generic utility addNamedModule (#630)

---
+++ src/transforms/v2-to-v3/modules/addNamedModule.ts
@@ -0,0 +1,23 @@
+import { Collection, JSCodeshift } from "jscodeshift";
+
+import * as importEqualsModule from "./importEqualsModule";
+import * as importModule from "./importModule";
+import * as requireModule from "./requireModule";
+import { ImportType, ModulesOptions } from "./types";
+
+export const addNamedModule = (
+  j: JSCodeshift,
+  source: Collection<unknown>,
+  options: ModulesOptions & { importType: ImportType }
+) => {
+  const { importType, ...addNamedModuleOptions } = options;
+
+  const { addNamedModule } =
+    importType === ImportType.REQUIRE
+      ? requireModule
+      : importType === ImportType.IMPORT_EQUALS
+      ? importEqualsModule
+      : importModule;
+
+  addNamedModule(j, source, addNamedModuleOptions);
+};
