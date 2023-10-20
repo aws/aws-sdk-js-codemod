2d47c9ab6ef5c65b5fcdd0ab354f7c1e599fbf6b
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-20 08:39:47 -0700

    Support RemoteCredentials

--- src/transforms/v2-to-v3/config/AWS_CREDENTIALS_MAP.ts
+++ src/transforms/v2-to-v3/config/AWS_CREDENTIALS_MAP.ts
@@ -2,8 +2,9 @@
  * Maps the AWS credentials class name in v2 to the v3 equivalent provider.
  */
 export const AWS_CREDENTIALS_MAP: Record<string, string> = {
+  ChainableTemporaryCredentials: "fromTemporaryCredentials",
   EC2MetadataCredentials: "fromInstanceMetadata",
   ECSCredentials: "fromContainerMetadata",
   EnvironmentCredentials: "fromEnv",
-  ChainableTemporaryCredentials: "fromTemporaryCredentials",
+  RemoteCredentials: "fromContainerMetadata",
 };
