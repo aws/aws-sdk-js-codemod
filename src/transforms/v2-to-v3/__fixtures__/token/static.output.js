29bc3a292acebd378a67fd9e7701448195a2e06c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-23 17:00:26 -0700

    test: token providers

---
+++ src/transforms/v2-to-v3/__fixtures__/token/static.output.js
@@ -0,0 +1,6 @@
+import { fromStatic } from "@aws-sdk/token-providers";
+
+// JS SDK v3 switched token providers to functions instead of objects.
+// This is the closest approximation from codemod of what your application needs.
+// Reference: https://www.npmjs.com/package/@aws-sdk/token-providers
+fromStatic(options);
