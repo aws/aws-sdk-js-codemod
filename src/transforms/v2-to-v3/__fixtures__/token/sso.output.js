9ce703affbf97dc637bfd152821331d3cd995b38
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-23 17:18:01 -0700

    test: simplify comment in token

--- src/transforms/v2-to-v3/__fixtures__/token/sso.output.js
+++ src/transforms/v2-to-v3/__fixtures__/token/sso.output.js
@@ -1,6 +1,6 @@
 import { fromSso } from "@aws-sdk/token-providers";
 
-// JS SDK v3 switched token providers to functions instead of objects.
+// JS SDK v3 switched token providers from classes to functions.
 // This is the closest approximation from codemod of what your application needs.
 // Reference: https://www.npmjs.com/package/@aws-sdk/token-providers
 fromSso(options);
