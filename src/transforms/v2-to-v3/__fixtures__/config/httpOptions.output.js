73a65eb8a2337073eb589307830f0a063028ef05
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-25 10:44:38 -0700

    Add comment for httpOptions

---
+++ src/transforms/v2-to-v3/__fixtures__/config/httpOptions.output.js
@@ -0,0 +1,8 @@
+const config = {
+  // The transformation for httpOptions is not implemented.
+  // Refer to UPGRADING.md on aws-sdk-js-v3 for changes needed.
+  // Please create/upvote feature request on aws-sdk-js-codemod for httpOptions.
+  httpOptions: {
+    connectTimeout: 1000,
+  }
+};
