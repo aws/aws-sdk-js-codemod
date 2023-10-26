92360bf55ae04a846c54d036892524559ca656dd
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-26 15:12:03 -0700

    Remove redundant chekcs from hasImport

--- src/transforms/v2-to-v3/modules/hasImport.ts
+++ src/transforms/v2-to-v3/modules/hasImport.ts
@@ -1,14 +1,11 @@
 import { Collection, JSCodeshift } from "jscodeshift";
+import { PACKAGE_NAME } from "../config";
 
 export const hasImport = (j: JSCodeshift, source: Collection<unknown>) =>
   source
     .find(j.ImportDeclaration)
     .filter((importDeclaration) => {
-      const { source: importSource } = importDeclaration.value;
-      return (
-        (importSource.type === "Literal" || importSource.type === "StringLiteral") &&
-        typeof importSource.value === "string" &&
-        importSource.value.startsWith("aws-sdk")
-      );
+      const { value: sourceValue } = importDeclaration.value.source;
+      return typeof sourceValue === "string" && sourceValue.startsWith(PACKAGE_NAME);
     })
     .size() > 0;
