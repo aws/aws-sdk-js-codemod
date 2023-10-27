121d0f53361d624da18355dcb181e50e96cd886b
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-10-27 10:33:28 -0700

    test: add api-gateway Stage in api-basic-type/global-import

--- src/transforms/v2-to-v3/__fixtures__/api-basic-type/global-import.input.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-basic-type/global-import.input.ts
@@ -1,3 +1,4 @@
 import AWS from "aws-sdk";
 
 const testTags: AWS.S3.Tag[] = [{ Key: "key", Value: "value" }];
+const testStages: AWS.APIGateway.Stage[] = [];
--- src/transforms/v2-to-v3/__fixtures__/api-basic-type/global-import.output.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-basic-type/global-import.output.ts
@@ -1,3 +1,5 @@
 import { Tag } from "@aws-sdk/client-s3";
+import { Stage } from "@aws-sdk/client-api-gateway";
 
 const testTags: Tag[] = [{ Key: "key", Value: "value" }];
+const testStages: Stage[] = [];
