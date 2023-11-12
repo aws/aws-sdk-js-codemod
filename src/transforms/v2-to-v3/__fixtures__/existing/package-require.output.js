bc4386501247ad1a0ab077d4995c4584c742e025
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-11-12 02:23:06 -0800

    test: existing

--- src/transforms/v2-to-v3/__fixtures__/existing/package-require.output.js
+++ src/transforms/v2-to-v3/__fixtures__/existing/package-require.output.js
@@ -1,7 +1,4 @@
-const {
-  DynamoDB,
-  DynamoDBClient
-} = require("@aws-sdk/client-dynamodb");
+const { DynamoDB, DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 
 const client1 = new DynamoDB();
 const client2 = new DynamoDBClient();
