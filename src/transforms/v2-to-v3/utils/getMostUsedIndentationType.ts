ddfedd923a78c7a29f71459d2305359dea4244b9
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-11-11 17:09:21 -0800

    Replace mix of tabs and spaces with tabs

--- src/transforms/v2-to-v3/utils/getMostUsedIndentationType.ts
+++ src/transforms/v2-to-v3/utils/getMostUsedIndentationType.ts
@@ -1,11 +1,21 @@
+import { EOL } from "os";
+
 export enum IndentationType {
   TAB = "tab",
   SPACE = "space",
 }
 
 export const getMostUsedIndentationType = (source: string) => {
-  const tabCount = (source.match(/\n\t/g) || []).length;
-  const spaceCount = (source.match(/\n {2}/g) || []).length;
+  let tabCount = 0;
+  let spaceCount = 0;
+
+  for (const line of source.split(EOL)) {
+    if (line.startsWith(" ")) {
+      spaceCount++;
+    } else if (line.startsWith("\t")) {
+      tabCount++;
+    }
+  }
 
   if (tabCount > spaceCount) {
     return IndentationType.TAB;
--- src/transforms/v2-to-v3/utils/index.ts
+++ src/transforms/v2-to-v3/utils/index.ts
@@ -2,4 +2,5 @@ export * from "./getClientDeepImportPath";
 export * from "./getClientNewExpression";
 export * from "./getMostUsedStringLiteralQuote";
 export * from "./getMostUsedIndentationType";
+export * from "./getValueIndentedWithTabs";
 export * from "./isTypeScriptFile";
