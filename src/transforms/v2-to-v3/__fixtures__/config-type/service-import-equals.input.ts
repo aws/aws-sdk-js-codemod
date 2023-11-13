eeb843d62a74f1b1ab5d816620748a449844fa0d
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-11-13 09:44:38 -0800

    test: config-type

---
+++ src/transforms/v2-to-v3/__fixtures__/config-type/service-import-equals.input.ts
@@ -0,0 +1,3 @@
+import S3 = require("aws-sdk/clients/s3");
+
+const config: S3.ClientConfiguration = {};
---
+++ src/transforms/v2-to-v3/__fixtures__/config-type/service-import.output.ts
@@ -0,0 +1,3 @@
+import { S3ClientConfig } from "@aws-sdk/client-s3";
+
+const config: S3ClientConfig = {};
