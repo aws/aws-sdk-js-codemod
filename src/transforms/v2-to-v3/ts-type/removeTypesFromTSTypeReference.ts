ee5570f2ce7830a7225ecc835b3d2796b1e58ff7
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-11-13 08:35:29 -0800

    Remove redundant Types from TSQualifiedName before replacing

---
+++ src/transforms/v2-to-v3/ts-type/removeTypesFromTSTypeReference.ts
@@ -0,0 +1,19 @@
+import { Collection, JSCodeshift } from "jscodeshift";
+
+export const removeTypesFromTSTypeReference = (
+  j: JSCodeshift,
+  source: Collection<unknown>,
+  v2ClientName: string
+) => {
+  // Support for DynamoDB.DocumentClient
+  const [, clientNameSuffix] = v2ClientName.split(".");
+  const clientName = clientNameSuffix ? clientNameSuffix : v2ClientName;
+
+  source
+    .find(j.TSQualifiedName, {
+      // ToDo: Check for left being a TSQualifiedName
+      left: { type: "Identifier", name: clientName },
+      right: { type: "Identifier", name: "Types" },
+    })
+    .replaceWith((nodePath) => nodePath.node.left);
+};
