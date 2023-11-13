1893b542afbdf3c42dc193483fb5cae49a77e297
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-11-13 08:58:18 -0800

    test: fix api-basic-types/*-import-equals

--- src/transforms/v2-to-v3/__fixtures__/api-basic-types/global-import-equals.output.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-basic-types/global-import-equals.output.ts
@@ -1,4 +1,4 @@
 import AWS_client_s3 = require("@aws-sdk/client-s3");
-import Tag = AWS_client_S3.Types.Tag;
+import Tag = AWS_client_s3.Tag;
 
 const testTags: Tag[] = [{ Key: "key", Value: "value" }];
