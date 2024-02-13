a712ac62d2257a06729a9023163182ca0055b4f3
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-02-13 13:31:42 -0800

    Add files in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config({
+  files: ["src/**/*.ts"],
   plugins: {
     "@typescript-eslint": tseslint.plugin,
   },
