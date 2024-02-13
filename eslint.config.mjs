8fdf546df5b5345b96ce21109ab7ab56de769412
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-02-13 08:01:57 -0800

    WIP use eslint flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config({
+  plugins: {
+    "@typescript-eslint": tseslint.plugin,
+  },
+  languageOptions: {
+    parser: tseslint.parser,
+    parserOptions: {
+      project: true,
+    },
+  },
+  rules: {
+    "@typescript-eslint/no-unsafe-argument": "error",
+    "@typescript-eslint/no-unsafe-assignment": "error",
+    "@typescript-eslint/no-unsafe-call": "error",
+    "@typescript-eslint/no-unsafe-member-access": "error",
+    "@typescript-eslint/no-unsafe-return": "error",
+  },
+});
