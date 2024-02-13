ad553edc8eeaa86d6c67cfa1672e7f42fd59e200
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-02-13 13:33:34 -0800

    Extend eslint and tseslint recommended configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config({
   files: ["src/**/*.ts"],
+  extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
   plugins: {
     "@typescript-eslint": tseslint.plugin,
   },
