6cf332a3e3c477bf9e6ace246a7b99a0dbc9e67f
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-02-26 21:09:14 -0800

    chore: use Node.remove() in removeRequireObjectPattern

--- src/transforms/v2-to-v3/modules/removeRequireObjectPattern.ts
+++ src/transforms/v2-to-v3/modules/removeRequireObjectPattern.ts
@@ -1,16 +1,7 @@
-import {
-  Collection,
-  Identifier,
-  JSCodeshift,
-  ObjectPattern,
-  ObjectProperty,
-  Property,
-  VariableDeclarator,
-} from "jscodeshift";
+import { Collection, JSCodeshift } from "jscodeshift";
 
-import { OBJECT_PROPERTY_TYPE_LIST } from "../config";
+import { addTopLevelComments } from "./addTopLevelComments";
 import { getRequireDeclaratorsWithObjectPattern } from "./getRequireDeclaratorsWithObjectPattern";
-import { removeDeclaration } from "./removeDeclaration";
 
 export interface RemoveRequireObjectPropertyOptions {
   localName: string;
@@ -27,26 +18,7 @@ export const removeRequireObjectPattern = (
     sourceValue,
   });
 
-  requireDeclarators.forEach((varDeclarator) => {
-    // Remove ObjectProperty from Variable Declarator.
-    const varDeclaratorId = varDeclarator.value.id as ObjectPattern;
-    varDeclaratorId.properties = varDeclaratorId.properties.filter((property) => {
-      if (!OBJECT_PROPERTY_TYPE_LIST.includes(property.type)) return true;
-      const propertyValue = (property as Property | ObjectProperty).value;
-      return propertyValue.type !== "Identifier" || propertyValue.name !== localName;
-    });
-
-    // Remove VariableDeclarator if there are no properties.
-    if (varDeclaratorId.properties.length === 0) {
-      const varDeclaration = varDeclarator.parentPath.parentPath;
-      varDeclaration.value.declarations = varDeclaration.value.declarations.filter(
-        (declaration: VariableDeclarator | Identifier) => declaration !== varDeclarator.value
-      );
-
-      // Remove VariableDeclaration if there are no declarations.
-      if (varDeclaration.value.declarations?.length === 0) {
-        removeDeclaration(j, source, varDeclaration);
-      }
-    }
-  });
+  const comments = source.find(j.Program).get("body", 0).node.comments;
+  requireDeclarators.remove();
+  addTopLevelComments(j, source, comments);
 };
