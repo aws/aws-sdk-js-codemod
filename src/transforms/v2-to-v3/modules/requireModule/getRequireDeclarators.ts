9633f120ad93624b6e1e98d881b7748fd64cebd2
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-02-28 12:40:24 -0800

    chore: update getRequireDeclarators to process require property

--- src/transforms/v2-to-v3/modules/requireModule/getRequireDeclarators.ts
+++ src/transforms/v2-to-v3/modules/requireModule/getRequireDeclarators.ts
@@ -1,37 +1,45 @@
 import { CallExpression, Collection, JSCodeshift, Literal, VariableDeclarator } from "jscodeshift";
 import { PACKAGE_NAME } from "../../config";
 
+const isValidRequireCallExpression = (callExpression: CallExpression, path?: string) => {
+  if (callExpression.arguments.length !== 1) {
+    return false;
+  }
+
+  const { value: sourceValue } = callExpression.arguments[0] as Literal;
+  if (typeof sourceValue !== "string") {
+    return false;
+  }
+
+  if (path) {
+    return sourceValue === path;
+  }
+
+  return sourceValue.startsWith(PACKAGE_NAME);
+};
+
 export const getRequireDeclarators = (
   j: JSCodeshift,
   source: Collection<unknown>,
   path?: string
 ): Collection<VariableDeclarator> =>
-  source
-    .find(j.VariableDeclarator, {
-      init: {
-        type: "CallExpression",
-        callee: { type: "Identifier", name: "require" },
-      },
-    })
-    .filter((varDeclarator) => {
-      const declaratorInit = varDeclarator.value.init;
-      if (!declaratorInit || declaratorInit.type !== "CallExpression") {
+  source.find(j.VariableDeclarator).filter((varDeclarator) => {
+    const declaratorInit = varDeclarator.value.init;
+    if (!declaratorInit) {
+      return false;
+    }
+
+    switch (declaratorInit.type) {
+      case "CallExpression":
+        return isValidRequireCallExpression(declaratorInit, path);
+      case "MemberExpression": {
+        const declaratorInitObject = declaratorInit.object;
+        if (declaratorInitObject.type !== "CallExpression") {
+          return false;
+        }
+        return isValidRequireCallExpression(declaratorInitObject, path);
+      }
+      default:
         return false;
-      }
-
-      const callExpression = declaratorInit as CallExpression;
-      if (callExpression.arguments.length !== 1) {
-        return false;
-      }
-
-      const { value: sourceValue } = callExpression.arguments[0] as Literal;
-      if (typeof sourceValue !== "string") {
-        return false;
-      }
-
-      if (path) {
-        return sourceValue === path;
-      }
-
-      return sourceValue.startsWith(PACKAGE_NAME);
-    });
+    }
+  });
