59a8b7a88c291f3a03bb149892c51b369570d2f6
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2024-02-29 14:49:35 -0800

    Simplify getClientNamedRecord using import specifiers utilities (#792)

--- src/transforms/v2-to-v3/client-names/getClientNamesRecord.ts
+++ src/transforms/v2-to-v3/client-names/getClientNamesRecord.ts
@@ -1,21 +1,49 @@
 import { Collection, JSCodeshift } from "jscodeshift";
 
+import { CLIENT_NAMES, PACKAGE_NAME } from "../config";
 import { ImportType } from "../modules";
+import * as importEqualsModule from "../modules/importEqualsModule";
+import * as importModule from "../modules/importModule";
+import * as requireModule from "../modules/requireModule";
+import { getClientDeepImportPath } from "../utils";
 import { getClientNamesFromDeepImport } from "./getClientNamesFromDeepImport";
-import { getClientNamesRecordFromImport } from "./getClientNamesRecordFromImport";
-import { getClientNamesRecordFromRequire } from "./getClientNamesRecordFromRequire";
 
 export const getClientNamesRecord = (
   j: JSCodeshift,
   source: Collection<unknown>,
   importType: ImportType
 ) => {
-  const clientNamesFromDeepImport = getClientNamesFromDeepImport(source.toSource());
+  const clientNamesRecord: Record<string, string> = {};
 
-  const clientNamesRecord =
+  const { getImportSpecifiers } =
     importType === ImportType.REQUIRE
-      ? getClientNamesRecordFromRequire(j, source, clientNamesFromDeepImport)
-      : getClientNamesRecordFromImport(j, source, clientNamesFromDeepImport);
+      ? requireModule
+      : importType === ImportType.IMPORT_EQUALS
+      ? importEqualsModule
+      : importModule;
+
+  const specifiersFromNamedImport = getImportSpecifiers(j, source, PACKAGE_NAME).filter(
+    (importSpecifier) => importSpecifier.importedName
+  );
+
+  for (const { importedName, localName } of specifiersFromNamedImport) {
+    const clientName = importedName ?? localName;
+    if (CLIENT_NAMES.includes(clientName)) {
+      clientNamesRecord[clientName] = localName;
+    }
+  }
+
+  const clientNamesFromDeepImport = getClientNamesFromDeepImport(source.toSource());
+  for (const clientName of clientNamesFromDeepImport) {
+    const deepImportPath = getClientDeepImportPath(clientName);
+
+    const specifiersFromDeepImport = getImportSpecifiers(j, source, deepImportPath).filter(
+      (importSpecifier) => !importSpecifier.importedName
+    );
+    if (specifiersFromDeepImport.length > 0) {
+      clientNamesRecord[clientName] = specifiersFromDeepImport[0].localName;
+    }
+  }
 
   // Populate client names for type transformations
   // Ref: https://github.com/aws/aws-sdk-js-codemod/issues/663
