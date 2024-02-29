10ba4654d5996a29c26bd15511fd62ca3d505af2
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2024-02-28 19:59:57 -0800

    Clean up adding import as object pattern (#787)

--- src/transforms/v2-to-v3/modules/importModule/addNamedModule.ts
+++ src/transforms/v2-to-v3/modules/importModule/addNamedModule.ts
@@ -1,17 +1,8 @@
-import {
-  Collection,
-  ImportNamespaceSpecifier,
-  ImportSpecifier,
-  JSCodeshift,
-  Property,
-  ObjectProperty,
-} from "jscodeshift";
+import { Collection, ImportSpecifier, JSCodeshift } from "jscodeshift";
 
-import { OBJECT_PROPERTY_TYPE_LIST, PACKAGE_NAME } from "../../config";
+import { PACKAGE_NAME } from "../../config";
 import { getImportSpecifiers } from "../getImportSpecifiers";
-import { getRequireProperty } from "../getRequireProperty";
 import { importSpecifierCompareFn } from "../importSpecifierCompareFn";
-import { objectPatternPropertyCompareFn } from "../objectPatternPropertyCompareFn";
 import { ModulesOptions } from "../types";
 
 export const addNamedModule = (
@@ -38,62 +29,6 @@ export const addNamedModule = (
             specifier?.local?.name === localName
         )
     ) {
-      return;
-    }
-
-    const importNamespaceSpecifiers = importSpecifiers.filter(
-      (importSpecifier) => importSpecifier?.type === "ImportNamespaceSpecifier"
-    ) as ImportNamespaceSpecifier[];
-
-    // If namespace import exists.
-    if (importNamespaceSpecifiers.length > 0) {
-      const defaultLocalName = importNamespaceSpecifiers[0].local!.name;
-      const namedImportObjectProperty = getRequireProperty(j, { importedName, localName });
-
-      const existingVarDeclarator = source.find(j.VariableDeclarator, {
-        type: "VariableDeclarator",
-        init: { type: "Identifier", name: defaultLocalName },
-      });
-
-      // If variable declarator exists.
-      if (existingVarDeclarator.size()) {
-        // Return if property exists.
-        if (
-          existingVarDeclarator.some((varDeclarator) => {
-            const id = varDeclarator.node.id;
-            if (id.type !== "ObjectPattern") return false;
-            for (const property of id.properties) {
-              if (!OBJECT_PROPERTY_TYPE_LIST.includes(property.type)) continue;
-              const propertyKey = (property as Property | ObjectProperty).key;
-              if (propertyKey.type !== "Identifier") continue;
-              if (propertyKey.name === importedName) return true;
-            }
-            return false;
-          })
-        )
-          return;
-
-        // Add property to the first declarator.
-        const firstDeclaratorProperties = existingVarDeclarator.get(0).node.id.properties;
-        firstDeclaratorProperties.push(namedImportObjectProperty);
-        firstDeclaratorProperties.sort(objectPatternPropertyCompareFn);
-        return;
-      }
-
-      const varDeclaration = j.variableDeclaration("const", [
-        j.variableDeclarator(
-          j.objectPattern([namedImportObjectProperty]),
-          j.identifier(defaultLocalName)
-        ),
-      ]);
-
-      source
-        .find(j.ImportDeclaration, {
-          type: "ImportDeclaration",
-          specifiers: [importNamespaceSpecifiers[0]],
-          source: { value: packageName },
-        })
-        .insertAfter(varDeclaration);
       return;
     }
 
