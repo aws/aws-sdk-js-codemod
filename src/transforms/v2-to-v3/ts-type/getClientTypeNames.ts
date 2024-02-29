ef3596914621040d9459fa3ba9f2945b3645002a
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-02-29 10:28:32 -0800

    chore: call getImportSpecifiers from getClientTypeNames

--- src/transforms/v2-to-v3/ts-type/getClientTypeNames.ts
+++ src/transforms/v2-to-v3/ts-type/getClientTypeNames.ts
@@ -1,6 +1,7 @@
 import { Collection, Identifier, JSCodeshift, TSQualifiedName, TSTypeReference } from "jscodeshift";
 
-import { getImportSpecifiers } from "../modules";
+import { ImportSpecifierDefault } from "../modules";
+import { getImportSpecifiers } from "../modules/importModule";
 import { getClientDeepImportPath } from "../utils";
 
 export interface GetClientTypeNamesOptions {
@@ -75,14 +76,9 @@ export const getClientTypeNames = (
   );
 
   clientTypeNames.push(
-    ...getImportSpecifiers(j, source, getClientDeepImportPath(v2ClientName))
-      .filter(
-        (importSpecifier) =>
-          importSpecifier.type === "ImportSpecifier" &&
-          importSpecifier.local &&
-          importSpecifier.local.type === "Identifier"
-      )
-      .map((importSpecifier) => (importSpecifier.local as Identifier).name)
+    ...(getImportSpecifiers(j, source, getClientDeepImportPath(v2ClientName)).filter(
+      (importSpecifier) => typeof importSpecifier === "string"
+    ) as ImportSpecifierDefault[])
   );
 
   return [...new Set(clientTypeNames)];
