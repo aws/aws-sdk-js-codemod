0d0eda5dd59c748876b1979ae8e463ba3793f6f2
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-02-29 14:45:49 -0800

    fix: process client names with deep import

--- src/transforms/v2-to-v3/client-names/getClientNamesRecord.ts
+++ src/transforms/v2-to-v3/client-names/getClientNamesRecord.ts
@@ -1,10 +1,11 @@
 import { Collection, JSCodeshift } from "jscodeshift";
 
-import { CLIENT_NAMES } from "../config";
+import { CLIENT_NAMES, PACKAGE_NAME } from "../config";
 import { ImportType } from "../modules";
 import * as importEqualsModule from "../modules/importEqualsModule";
 import * as importModule from "../modules/importModule";
 import * as requireModule from "../modules/requireModule";
+import { getClientDeepImportPath } from "../utils";
 import { getClientNamesFromDeepImport } from "./getClientNamesFromDeepImport";
 
 export const getClientNamesRecord = (
@@ -21,16 +22,32 @@ export const getClientNamesRecord = (
       ? importEqualsModule
       : importModule;
 
-  for (const { importedName, localName } of getImportSpecifiers(j, source)) {
+  const specifiersFromNamedImport = getImportSpecifiers(j, source, PACKAGE_NAME).filter(
+    (importSpecifier) => importSpecifier.importedName
+  );
+
+  for (const { importedName, localName } of specifiersFromNamedImport) {
     const clientName = importedName ?? localName;
     if (CLIENT_NAMES.includes(clientName)) {
       clientNamesRecord[clientName] = localName;
     }
   }
 
+  const clientNamesFromDeepImport = getClientNamesFromDeepImport(source.toSource());
+  for (const clientName of clientNamesFromDeepImport) {
+    const deepImportPath = getClientDeepImportPath(clientName);
+
+    const specifiersFromDeepImport = getImportSpecifiers(j, source, deepImportPath).filter(
+      (importSpecifier) => !importSpecifier.importedName
+    );
+    if (specifiersFromDeepImport.length > 0) {
+      clientNamesRecord[clientName] = specifiersFromDeepImport[0].localName;
+    }
+  }
+
   // Populate client names for type transformations
   // Ref: https://github.com/aws/aws-sdk-js-codemod/issues/663
-  for (const clientName of getClientNamesFromDeepImport(source.toSource())) {
+  for (const clientName of clientNamesFromDeepImport) {
     if (!(clientName in clientNamesRecord)) {
       clientNamesRecord[clientName] = clientName;
     }
