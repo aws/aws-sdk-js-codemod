3566aae12ad4a84ea6cb8765f73439dec604516d
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-02-29 14:38:32 -0800

    Simplify getClientNamedRecord using import specifiers utilities

--- src/transforms/v2-to-v3/client-names/getClientNamesRecord.ts
+++ src/transforms/v2-to-v3/client-names/getClientNamesRecord.ts
@@ -1,25 +1,36 @@
 import { Collection, JSCodeshift } from "jscodeshift";
 
+import { CLIENT_NAMES } from "../config";
 import { ImportType } from "../modules";
+import * as importEqualsModule from "../modules/importEqualsModule";
+import * as importModule from "../modules/importModule";
+import * as requireModule from "../modules/requireModule";
 import { getClientNamesFromDeepImport } from "./getClientNamesFromDeepImport";
-import { getClientNamesRecordFromImport } from "./getClientNamesRecordFromImport";
-import { getClientNamesRecordFromRequire } from "./getClientNamesRecordFromRequire";
 
 export const getClientNamesRecord = (
   j: JSCodeshift,
   source: Collection<unknown>,
   importType: ImportType
 ) => {
-  const clientNamesFromDeepImport = getClientNamesFromDeepImport(source.toSource());
+  const clientNamesRecord: Record<string, string> = {};
 
-  const clientNamesRecord =
+  const { getImportSpecifiers } =
     importType === ImportType.REQUIRE
-      ? getClientNamesRecordFromRequire(j, source, clientNamesFromDeepImport)
-      : getClientNamesRecordFromImport(j, source, clientNamesFromDeepImport);
+      ? requireModule
+      : importType === ImportType.IMPORT_EQUALS
+      ? importEqualsModule
+      : importModule;
+
+  for (const { importedName, localName } of getImportSpecifiers(j, source)) {
+    const clientName = importedName ?? localName;
+    if (CLIENT_NAMES.includes(clientName)) {
+      clientNamesRecord[clientName] = localName;
+    }
+  }
 
   // Populate client names for type transformations
   // Ref: https://github.com/aws/aws-sdk-js-codemod/issues/663
-  for (const clientName of clientNamesFromDeepImport) {
+  for (const clientName of getClientNamesFromDeepImport(source.toSource())) {
     if (!(clientName in clientNamesRecord)) {
       clientNamesRecord[clientName] = clientName;
     }
