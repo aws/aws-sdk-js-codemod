fdbf2e48bc3c3ab81eea11804085ac2c3712a010
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-03-01 10:03:50 -0800

    chore: check for another importEquals when removing

--- src/transforms/v2-to-v3/modules/importEqualsModule/removeImportEquals.ts
+++ src/transforms/v2-to-v3/modules/importEqualsModule/removeImportEquals.ts
@@ -2,13 +2,15 @@ import { Collection, JSCodeshift } from "jscodeshift";
 import { removeDeclaration } from "../removeDeclaration";
 import { getImportEqualsDeclarations } from "./getImportEqualsDeclarations";
 
+const isAnotherSpecifier = (j: JSCodeshift, source: Collection<unknown>, localName: string) =>
+  source.find(j.TSImportEqualsDeclaration, { id: { name: localName } }).size() > 1;
+
 export const removeImportEquals = (j: JSCodeshift, source: Collection<unknown>) =>
   getImportEqualsDeclarations(j, source).forEach((importEqualsDeclaration) => {
     const localName = importEqualsDeclaration.value.id.name;
     const identifiers = source.find(j.Identifier, { name: localName });
     // One occurrence: local identifier.
-    // ToDo: Check if there're another import equals from `@aws-sdk/`.
-    if (identifiers.size() === 1) {
+    if (identifiers.size() === 1 || isAnotherSpecifier(j, source, localName)) {
       removeDeclaration(j, source, importEqualsDeclaration.get());
     }
   });
