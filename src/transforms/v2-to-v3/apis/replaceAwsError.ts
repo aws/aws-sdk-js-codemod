f76d4dd62d37ec813fb494f41d0b1eefbd75cd8f
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-03-06 22:20:34 -0800

    chore: replace AWSError with ServiceException

--- src/transforms/v2-to-v3/apis/replaceAwsError.ts
+++ src/transforms/v2-to-v3/apis/replaceAwsError.ts
@@ -1,5 +1,5 @@
 import { Collection, JSCodeshift } from "jscodeshift";
-import { ImportType } from "../modules";
+import { ImportType, addNamedModule } from "../modules";
 
 export interface ReplaceAwsErrorOptions {
   v2GlobalName?: string;
@@ -10,4 +10,39 @@ export const replaceAwsError = (
   j: JSCodeshift,
   source: Collection<unknown>,
   { v2GlobalName, importType }: ReplaceAwsErrorOptions
-) => {};
+) => {
+  const v2AwsErrorName = "AWSError";
+  const v3AwsErrorName = "ServiceException";
+
+  const namedModuleParams = {
+    importType,
+    localName: v3AwsErrorName,
+    packageName: "@smithy/smithy-client",
+  };
+
+  if (v2GlobalName) {
+    source
+      .find(j.TSTypeReference, {
+        typeName: {
+          left: { type: "Identifier", name: v2GlobalName },
+          right: { type: "Identifier", name: v2AwsErrorName },
+        },
+      })
+      .replaceWith((v2ErrorType) => {
+        addNamedModule(j, source, namedModuleParams);
+        return j.tsTypeReference(j.identifier(v3AwsErrorName), v2ErrorType.node.typeParameters);
+      });
+  }
+
+  source
+    .find(j.TSTypeReference, {
+      typeName: {
+        type: "Identifier",
+        name: v2AwsErrorName,
+      },
+    })
+    .replaceWith((v2ErrorType) => {
+      addNamedModule(j, source, namedModuleParams);
+      return j.tsTypeReference(j.identifier(v3AwsErrorName), v2ErrorType.node.typeParameters);
+    });
+};
