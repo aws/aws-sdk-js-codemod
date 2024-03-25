ea5ce9d4aed2aca44e4fb867b7b4c8f186fc1715
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-03-25 12:43:48 -0700

    test: udpate aws-error-name/global-import.ts

--- src/transforms/v2-to-v3/__fixtures__/aws-error-name/global-import.input.ts
+++ src/transforms/v2-to-v3/__fixtures__/aws-error-name/global-import.input.ts
@@ -1,10 +1,10 @@
 import AWS from "aws-sdk";
 
-export const func = async (client: AWS.S3) => {
+const Bucket = "bucket-name";
+
+export const funcTryCatch = async (client: AWS.S3) => {
   try {
-    await client.createBucket({
-      Bucket: "bucket"
-    }).promise();
+    await client.createBucket({ Bucket }).promise();
   } catch (error) {
     if (error.code === "BucketAlreadyExists") {
       // Handle BucketAlreadyExists error
@@ -12,4 +12,36 @@ export const func = async (client: AWS.S3) => {
       throw error;
     }
   }
+}
+
+export const funcPromiseCatchArrowFn = async (client: AWS.S3) => {
+  client
+    .createBucket({ Bucket })
+    .promise()
+    .then((response) => {
+      // Consume the response
+    })
+    .catch((error) => {
+      if (error.code === "BucketAlreadyExists") {
+        // Handle BucketAlreadyExists error
+      } else {
+        // Handle other error.
+      }
+    });
+}
+
+export const funcPromiseCatchFn = async (client: AWS.S3) => {
+  client
+    .createBucket({ Bucket })
+    .promise()
+    .then((response) => {
+      // Consume the response
+    })
+    .catch(function (error) {
+      if (error.code === "BucketAlreadyExists") {
+        // Handle BucketAlreadyExists error
+      } else {
+        // Handle other error.
+      }
+    });
 }
