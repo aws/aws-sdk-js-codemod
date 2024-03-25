5fb11bb463a45fc1cbab6b532cfdf21c6182563e
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2024-03-25 12:48:14 -0700

    Transform checks for error.code to error.name in promise catch (#810)

--- src/transforms/v2-to-v3/__fixtures__/aws-error-name/global-import.output.js
+++ src/transforms/v2-to-v3/__fixtures__/aws-error-name/global-import.output.js
@@ -1,11 +1,10 @@
 import { S3 } from "@aws-sdk/client-s3";
 
 const client = new S3();
+const Bucket = "bucket-name";
 
 try {
-  await client.createBucket({
-    Bucket: "bucket"
-  });
+  await client.createBucket({ Bucket });
 } catch (error) {
   if (error.name === "BucketAlreadyExists") {
     // Handle BucketAlreadyExists error
@@ -13,3 +12,29 @@ try {
     throw error;
   }
 }
+
+client
+  .createBucket({ Bucket })
+  .then((response) => {
+    // Consume the response
+  })
+  .catch((error) => {
+    if (error.name === "BucketAlreadyExists") {
+      // Handle BucketAlreadyExists error
+    } else {
+      // Handle other error.
+    }
+  });
+
+client
+  .createBucket({ Bucket })
+  .then((response) => {
+    // Consume the response
+  })
+  .catch(function (error) {
+    if (error.name === "BucketAlreadyExists") {
+      // Handle BucketAlreadyExists error
+    } else {
+      // Handle other error.
+    }
+  });
--- src/transforms/v2-to-v3/__fixtures__/aws-error-name/global-import.output.ts
+++ src/transforms/v2-to-v3/__fixtures__/aws-error-name/global-import.output.ts
@@ -1,10 +1,10 @@
 import { S3 } from "@aws-sdk/client-s3";
 
-export const func = async (client: S3) => {
+const Bucket = "bucket-name";
+
+export const funcTryCatch = async (client: S3) => {
   try {
-    await client.createBucket({
-      Bucket: "bucket"
-    });
+    await client.createBucket({ Bucket });
   } catch (error) {
     if (error.name === "BucketAlreadyExists") {
       // Handle BucketAlreadyExists error
@@ -12,4 +12,34 @@ export const func = async (client: S3) => {
       throw error;
     }
   }
+}
+
+export const funcPromiseCatchArrowFn = async (client: S3) => {
+  client
+    .createBucket({ Bucket })
+    .then((response) => {
+      // Consume the response
+    })
+    .catch((error) => {
+      if (error.name === "BucketAlreadyExists") {
+        // Handle BucketAlreadyExists error
+      } else {
+        // Handle other error.
+      }
+    });
+}
+
+export const funcPromiseCatchFn = async (client: S3) => {
+  client
+    .createBucket({ Bucket })
+    .then((response) => {
+      // Consume the response
+    })
+    .catch(function (error) {
+      if (error.name === "BucketAlreadyExists") {
+        // Handle BucketAlreadyExists error
+      } else {
+        // Handle other error.
+      }
+    });
 }
