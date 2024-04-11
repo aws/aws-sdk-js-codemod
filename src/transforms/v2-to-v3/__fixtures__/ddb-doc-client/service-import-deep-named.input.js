d9499f65509142e50234b79dfb837a74dc3dc971
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-04-11 10:21:14 -0700

    test: remove ddb-doc-client/service-*-deep-named

--- src/transforms/v2-to-v3/__fixtures__/ddb-doc-client/service-import-deep-named.input.js
+++
@@ -1,4 +0,0 @@
-import { DocumentClient } from "aws-sdk/clients/dynamodb";
-
-const documentClient = new DocumentClient({ region: "us-west-2" });
-const response = await documentClient.scan({ TableName: "TABLE_NAME" }).promise();
--- src/transforms/v2-to-v3/__fixtures__/ddb-doc-client/service-require-deep-named.output.js
+++
@@ -1,6 +0,0 @@
-// Transformation of DocumentClient named import from deep path is unsupported in aws-sdk-js-codemod.
-// Please convert to a default import, and re-run aws-sdk-js-codemod.
-const { DocumentClient } = require("aws-sdk/clients/dynamodb");
-
-const documentClient = new DocumentClient({ region: "us-west-2" });
-const response = await documentClient.scan({ TableName: "TABLE_NAME" }).promise();
