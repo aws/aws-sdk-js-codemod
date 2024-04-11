b70009dd40f360ca42e1b9ba7423114543aa8344
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-04-11 15:26:38 -0700

    Remove .promise() calls from APIs called from new client

--- src/transforms/v2-to-v3/apis/removePromiseCalls.ts
+++ src/transforms/v2-to-v3/apis/removePromiseCalls.ts
@@ -3,12 +3,69 @@ import { Collection, Identifier, JSCodeshift } from "jscodeshift";
 import { ClientIdentifier } from "../types";
 import { removePromiseForCallExpression } from "./removePromiseForCallExpression";
 
+export interface RemovePromiseCallsOptions {
+  v2GlobalName?: string;
+  v2ClientName: string;
+  v2ClientLocalName: string;
+  clientIdentifiers: ClientIdentifier[];
+}
+
 // Removes .promise() from client API calls.
 export const removePromiseCalls = (
   j: JSCodeshift,
   source: Collection<unknown>,
-  clientIdentifiers: ClientIdentifier[]
+  { v2GlobalName, v2ClientName, v2ClientLocalName, clientIdentifiers }: RemovePromiseCallsOptions
 ): void => {
+  // Remove .promise() for API calls from client creation from global name.
+  if (v2GlobalName) {
+    source
+      .find(j.CallExpression, {
+        callee: {
+          type: "MemberExpression",
+          object: {
+            type: "CallExpression",
+            callee: {
+              type: "MemberExpression",
+              object: {
+                type: "NewExpression",
+                callee: {
+                  type: "MemberExpression",
+                  object: { type: "Identifier", name: v2GlobalName },
+                  property: { type: "Identifier", name: v2ClientName },
+                },
+              },
+            },
+          },
+          property: { type: "Identifier", name: "promise" },
+        },
+      })
+      .forEach((callExpression) => {
+        removePromiseForCallExpression(j, callExpression);
+      });
+  }
+
+  // Remove .promise() for API calls client creation from local name.
+  source
+    .find(j.CallExpression, {
+      callee: {
+        type: "MemberExpression",
+        object: {
+          type: "CallExpression",
+          callee: {
+            type: "MemberExpression",
+            object: {
+              type: "NewExpression",
+              callee: { type: "Identifier", name: v2ClientLocalName },
+            },
+          },
+        },
+        property: { type: "Identifier", name: "promise" },
+      },
+    })
+    .forEach((callExpression) => {
+      removePromiseForCallExpression(j, callExpression);
+    });
+
   for (const clientId of clientIdentifiers) {
     // Remove .promise() from client API calls.
     source
