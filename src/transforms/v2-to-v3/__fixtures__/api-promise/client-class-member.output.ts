c24322a5f0725a7aa8c366beb6d65f0d02df7ba7
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-04-10 19:20:12 -0700

    test: api-promise/client-class-member

--- src/transforms/v2-to-v3/__fixtures__/api-promise/client-class-member.output.ts
+++ src/transforms/v2-to-v3/__fixtures__/api-promise/client-class-member.output.ts
@@ -16,3 +16,20 @@ class ClientClassMember {
     return this.clientInClass.listTagsOfResource({ ResourceArn: "STRING_VALUE" });
   }
 }
+
+// Client as class member with creation inside constructor
+class ClientClassMemberConstructor {
+  private clientInClassCtr: DynamoDB;
+  
+  constructor() {
+    this.clientInClassCtr = new DynamoDB();
+  }
+
+  async listTables() {
+    return this.clientInClassCtr.listTables();
+  }
+
+  async listTagsOfResource() {
+    return this.clientInClassCtr.listTagsOfResource({ ResourceArn: "STRING_VALUE" });
+  }
+}
