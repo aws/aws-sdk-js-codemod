59d2e57dc082da71df93cc5bfc5d2488491208a4
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2024-04-11 14:39:31 -0700

    Add comment about unsupported config.params (#856)

---
+++ src/transforms/v2-to-v3/__fixtures__/config/params.output.js
@@ -0,0 +1,5 @@
+const config = {
+  // The key params is no longer supported in v3, and can be removed.
+  // @deprecated The object needs to be passed to individual operations where it's intended.
+  params: { TableName: "TableName" }
+};
