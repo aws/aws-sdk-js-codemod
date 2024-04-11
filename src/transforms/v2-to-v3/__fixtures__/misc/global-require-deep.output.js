45e1e46c47176c3f91d6261bbd2a72a28c2498f6
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-04-11 12:42:46 -0700

    test: fix misc/global-require-deep.output.js

--- src/transforms/v2-to-v3/__fixtures__/misc/global-require-deep.output.js
+++ src/transforms/v2-to-v3/__fixtures__/misc/global-require-deep.output.js
@@ -1,4 +1,4 @@
-import { DynamoDB } from "@aws-sdk/client-dynamodb";
+const { DynamoDB } = require("@aws-sdk/client-dynamodb");
 
 const client = new DynamoDB();
 const data = await client.listTables();
