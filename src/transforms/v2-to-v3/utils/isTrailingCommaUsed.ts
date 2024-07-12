e66c64b17130872531cb58817036001ee72cf305
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2024-07-12 07:49:53 -0700

    Use optional chain instead of chained logical expressions (#896)

--- src/transforms/v2-to-v3/utils/isTrailingCommaUsed.ts
+++ src/transforms/v2-to-v3/utils/isTrailingCommaUsed.ts
@@ -3,7 +3,7 @@ import type { Collection, JSCodeshift } from "jscodeshift";
 export const isTrailingCommaUsed = (j: JSCodeshift, source: Collection<unknown>) => {
   for (const node of source.find(j.ObjectExpression).nodes()) {
     // @ts-ignore Property 'extra' does not exist on type 'ObjectExpression'.
-    if (node.extra && node.extra.trailingComma) {
+    if (node.extra?.trailingComma) {
       return true;
     }
   }
