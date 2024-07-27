5894deed5b6980016c7bfd069deac7833449e0e7
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-07-27 14:49:17 -0700

    fix(scripts): lint/style/useNodejsImportProtocol

--- scripts/generateClientTypesMap/index.ts
+++ scripts/generateClientTypesMap/index.ts
@@ -1,5 +1,5 @@
-import { join } from "path";
-import { writeFile } from "fs/promises";
+import { writeFile } from "node:fs/promises";
+import { join } from "node:path";
 import { format } from "prettier";
 
 import {
